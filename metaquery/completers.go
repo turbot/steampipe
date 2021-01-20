6dca3eaf0914d1932b5ac1509f89878b6d285de1
Binaek Sarkar <binaek@turbot.com>
2021-01-20 18:51:46 +0530

    adding completer for output and inspect

--- metaquery/completers.go
+++ metaquery/completers.go
@@ -43,3 +43,41 @@ func booleanCompleter(input *CompleterInput) []prompt.Suggest {
 
 	return suggestions
 }
+
+func outputCompleter(input *CompleterInput) []prompt.Suggest {
+	suggestions := make([]prompt.Suggest, 3)
+
+	suggestions[0] = prompt.Suggest{Text: "json", Description: "Set output to JSON"}
+	suggestions[1] = prompt.Suggest{Text: "csv", Description: "Set output to CSV"}
+	suggestions[1] = prompt.Suggest{Text: "table", Description: "Set output to TABLE (default)"}
+
+	return suggestions
+}
+
+func inspectCompleter(input *CompleterInput) []prompt.Suggest {
+	suggestions := []prompt.Suggest{}
+
+	if len(input.args()) == 0 {
+		for _, schema := range input.Schema.GetSchemas() {
+			suggestions = append(suggestions, prompt.Suggest{Text: schema, Description: "Schema"})
+		}
+		return suggestions
+	}
+
+	// fully qualified table names
+	qualifiedTablesToAdd := []string{}
+
+	for schemaName, schemaDetails := range input.Schema.Schemas {
+		for tableName := range schemaDetails {
+			qualifiedTablesToAdd = append(qualifiedTablesToAdd, fmt.Sprintf("%s.%s", schemaName, tableName))
+		}
+	}
+
+	sort.Strings(qualifiedTablesToAdd)
+
+	for _, table := range qualifiedTablesToAdd {
+		suggestions = append(suggestions, prompt.Suggest{Text: table, Description: "Table"})
+	}
+
+	return suggestions
+}
