be46a91712eb952ed844bb638d715a7ff9e58b40
tyagiparth <parth.tyagi@turbot.com>
2021-01-21 16:18:14 +0530

    Clear postgres logs older than 7 days. closes #16

---
+++ db/logs.go
@@ -0,0 +1,29 @@
+package db
+
+import (
+	"io/ioutil"
+	"log"
+	"os"
+	"time"
+)
+
+const logRetentionDays = 7
+
+func TrimLogs() {
+	fileLocation := getDatabaseLogDirectory()
+	files, err := ioutil.ReadDir(fileLocation)
+	if err != nil {
+		log.Fatal(err)
+	}
+	for _, file := range files {
+		fileName := file.Name()
+		diff := time.Now().Sub(file.ModTime()).Hours()
+		if diff > logRetentionDays*24 {
+			logPath := fileLocation + "/" + fileName
+			err := os.Remove(logPath)
+			if err != nil {
+				log.Printf("[DEBUG] failed to delete log file %s\n", logPath)
+			}
+		}
+	}
+}
