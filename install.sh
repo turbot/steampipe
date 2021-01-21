1b22713faefa8f7ffa92406e908137eeca71d476
Binaek Sarkar <binaek@turbot.com>
2021-01-21 20:26:21 +0530

    fixing installer

--- install.sh
+++ install.sh
@@ -4,7 +4,12 @@
 set -e
 
 if ! command -v unzip >/dev/null; then
-	echo "Error: unzip is required to install Steampipe." 1>&2
+	echo "Error: `unzip` is required to install Steampipe." 1>&2
+	exit 1
+fi
+
+if ! command -v install >/dev/null; then
+	echo "Error: `install` is required to install Steampipe." 1>&2
 	exit 1
 fi
 
@@ -25,30 +30,31 @@ else
 	steampipe_uri="https://github.com/denoland/deno/releases/download/${1}/steampipe_${target}.zip"
 fi
 
-steampipe_install="/usr/local"
-bin_dir="$steampipe_install/bin"
+bin_dir="/usr/local/bin"
 exe="$bin_dir/steampipe"
 
-if [ ! -d "$bin_dir" ]; then
-	mkdir -p "$bin_dir"
-fi
+test -z "$tmp_dir" && tmp_dir="$(mktemp -d)"
+mkdir -p "${tmp_dir}"
+tmp_dir="${tmp_dir%/}"
+zip_location="$tmp_dir/steampipe.zip"
 
 echo "Downloading from $steampipe_uri"
 if command -v wget >/dev/null; then
-	wget -q --show-progress -O "$exe.zip" "$steampipe_uri"
+	wget -q --show-progress -O "$zip_location" "$steampipe_uri"
 elif command -v curl >/dev/null; then
-    curl --fail --location --progress-bar --output "$exe.zip" "$steampipe_uri"
+    curl --fail --location --progress-bar --output "$zip_location" "$steampipe_uri"
 else
     echo "Unable to find wget or curl. Cannot download."
     exit 1
 fi
 
 echo "Deflating downloaded archive"
-unzip -d "$bin_dir" -o "$exe.zip"
+unzip -d "$tmp_dir" -o "$zip_location"
+echo "Installing"
+install -d "$bin_dir"
+install "$tmp_dir/steampipe" "$bin_dir"
 echo "Removing downloaded archive"
-rm "$exe.zip"
-echo "Setting necessary permissions"
-chmod +x "$exe"
+rm "$zip_location"
 
 echo "Steampipe was installed successfully to $exe"
 if command -v steampipe >/dev/null; then
