ddb6b6d3b0f6b3cff11631ab04d449f97d14ec2f
Binaek Sarkar <binaek@turbot.com>
2021-01-28 18:23:47 +0530

    adding constant for col width

--- constants/app.go
+++ constants/app.go
@@ -1,10 +1,6 @@
 package constants
 
-import "time"
-
 // Application constants
 const (
 	APPNAME = "steampipe"
-	// SpinnerShowTimeout :: duration after which spinner should be shown
-	SpinnerShowTimeout = 100 * time.Millisecond
 )
---
+++ constants/display.go
@@ -0,0 +1,12 @@
+package constants
+
+import "time"
+
+// Display constants
+const (
+	// SpinnerShowTimeout :: duration after which spinner should be shown
+	SpinnerShowTimeout = 100 * time.Millisecond
+
+	// Max Column Width
+	MaxColumnWidth = 1024
+)
