a1d69dc863b79be05c922229a8406ac3bc70ff22
Binaek Sarkar <binaek@turbot.com>
2021-02-16 18:25:02 +0530

    sending WSL in useragent

--- utils/useragent.go
+++ utils/useragent.go
@@ -7,13 +7,22 @@ import (
 	"github.com/containerd/containerd/version"
 )
 
+// ConstructUserAgent :: constructs an user-agent string for the installation and env
 func ConstructUserAgent(installationID string) string {
 	const format = "TURBOT(STEAMPIPE/%s)(%s/%s)(%s/%s)(%s)"
+
+	wsl := "non-wsl"
+	isWSL, err := IsWSL()
+	if err != nil {
+		wsl = "wsl-unknown"
+	} else if isWSL {
+		wsl = "wsl"
+	}
 
 	return fmt.Sprintf(format,
 		version.Version,
 		runtime.GOOS,
-		"",
+		wsl,
 		runtime.GOARCH,
 		"",
 		installationID)
