036af78c96cfe8da762ba7afab92d1f46d8c2fdc
Binaek Sarkar <binaek@turbot.com>
2021-02-17 14:26:07 +0530

    check if steampipe is running in `windows subsystem for Linux`. Closes #171 (#175)

---
+++ utils/wsl.go
@@ -0,0 +1,22 @@
+package utils
+
+import (
+	"io/ioutil"
+	"runtime"
+	"strings"
+)
+
+// IsWSL :: detects whether app is running in WSL environment
+// refer to: https://github.com/Microsoft/WSL/issues/423#issuecomment-679190758
+func IsWSL() (bool, error) {
+	if runtime.GOOS != "linux" {
+		return false, nil
+	}
+	// https://github.com/Microsoft/WSL/issues/2299#issuecomment-361366982
+	osReleaseContent, err := ioutil.ReadFile("/proc/version")
+	if err != nil {
+		return false, err
+	}
+	osRelease := strings.ToLower(string(osReleaseContent))
+	return (strings.Contains(osRelease, "microsoft") || strings.Contains(osRelease, "wsl")), nil
+}
