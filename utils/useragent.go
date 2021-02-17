9429c13d635a4339ea02bc5c6d31a83a2f66560d
Binaek Sarkar <binaek@turbot.com>
2021-02-17 18:48:27 +0530

    include WSL in user-agent

--- utils/useragent.go
+++ utils/useragent.go
@@ -8,13 +8,29 @@ import (
 )
 
 func ConstructUserAgent(installationID string) string {
-	const format = "TURBOT(STEAMPIPE/%s)(%s/%s)(%s/%s)(%s)"
+
+	wslString := ""
+	if runtime.GOOS != "linux" {
+		wslString = "wsl-na"
+	} else {
+		wsl, err := IsWSL()
+		if err != nil {
+			wslString = "wsl-unknown"
+		} else if wsl {
+			wslString = "wsl-win"
+		} else {
+			wslString = "wsl-nil"
+		}
+
+	}
+
+	// TURBOT(STEAMPIPE/1.4.1+unknown)(linux/wsl-nil)(amd64)(wsl-test)
+	const format = "TURBOT(STEAMPIPE/%s)(%s/%s)(%s)(%s)"
 
 	return fmt.Sprintf(format,
 		version.Version,
 		runtime.GOOS,
-		"",
+		wslString,
 		runtime.GOARCH,
-		"",
 		installationID)
 }
