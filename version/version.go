dd5b14678f61ff304bd5b8a766b5debe6e07bd86
Binaek Sarkar <binaek@turbot.com>
2021-02-19 16:24:04 +0530

    version evaluation consistency with SDK

--- version/version.go
+++ version/version.go
@@ -1,7 +1,8 @@
-// The version package provides a location to set the release versions for all
+// Package version :: The version package provides a location to set the release versions for all
 // packages to consume, without creating import cycles.
 //
 // This package should not import any other steampipe packages.
+//
 package version
 
 import (
@@ -9,6 +10,13 @@ import (
 
 	goVersion "github.com/hashicorp/go-version"
 )
+
+/**
+We should fill in the `steampipeVersion` and `prerelease` variables using ldflags during build
+
+See https://blog.alexellis.io/inject-build-time-vars-golang/
+Also https://www.digitalocean.com/community/tutorials/using-ldflags-to-set-version-information-for-go-applications
+**/
 
 // The main version number that is being run at the moment.
 var steampipeVersion = "0.2.0"
@@ -18,18 +26,17 @@ var steampipeVersion = "0.2.0"
 // such as "dev" (in development), "beta", "rc1", etc.
 var prerelease = ""
 
-// The git commit that was compiled. This will be filled in by the compiler.
-// See https://blog.alexellis.io/inject-build-time-vars-golang/
-// Also https://www.digitalocean.com/community/tutorials/using-ldflags-to-set-version-information-for-go-applications
-var gitCommit = "gitcommit"
-
 // semVer is an instance of version.Version. This has the secondary
 // benefit of verifying during tests and init time that our version is a
 // proper semantic version, which should always be the case.
 var semVer *goVersion.Version
 
 func init() {
-	semVer = goVersion.Must(goVersion.NewVersion(steampipeVersion))
+	versionString := steampipeVersion
+	if prerelease != "" {
+		versionString = fmt.Sprintf("%s-%s", steampipeVersion, prerelease)
+	}
+	semVer = goVersion.Must(goVersion.NewVersion(versionString))
 }
 
 // header is the header name used to send the current steampipe version
@@ -38,13 +45,5 @@ const header = "Steampipe-Version"
 
 // String returns the complete version string, including prerelease
 func String() string {
-	versionString := steampipeVersion
-	if prerelease != "" {
-		versionString = fmt.Sprintf("%s-%s", versionString, prerelease)
-		if gitCommit != "" {
-			// we should not include git commit if this is not prerelease
-			versionString = fmt.Sprintf("%s+%s", versionString, gitCommit)
-		}
-	}
-	return versionString
+	return semVer.String()
 }
