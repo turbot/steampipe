6691ab4d111245fb99d73f03b9c1ffedea55dd08
Binaek Sarkar <binaek@turbot.com>
2021-02-19 20:37:56 +0530

    error if no args are given in `install` or `update`

--- cmd/plugin.go
+++ cmd/plugin.go
@@ -205,6 +205,11 @@ func runPluginInstallCmd(cmd *cobra.Command, args []string) {
 	plugins := append([]string{}, args...)
 	installSkipped := []string{}
 
+	if len(plugins) == 0 {
+		utils.ShowError(fmt.Errorf("you need to provide at least one plugin to install. Refer to %s for available plugins", constants.Bold("https://hub.steampipe.io")))
+		return
+	}
+
 	if len(plugins) > 1 {
 		fmt.Println()
 	}
@@ -251,7 +256,7 @@ func runPluginInstallCmd(cmd *cobra.Command, args []string) {
 		fmt.Printf("\nTo update these plugins, please run: %s %s\n", constants.Bold("steampipe plugin update"), constants.Bold(strings.Join(installSkipped, " ")))
 	}
 
-	if len(args) > 1 {
+	if len(plugins) > 1 {
 		fmt.Println("")
 	}
 
@@ -272,6 +277,11 @@ func runPluginUpdateCmd(cmd *cobra.Command, args []string) {
 	// These can be simple names ('aws') for "standard" plugins, or
 	// full refs to the OCI image (us-docker.pkg.dev/steampipe/plugin/turbot/aws:1.0.0)
 	plugins := append([]string{}, args...)
+
+	if len(plugins) == 0 && !cmdconfig.Viper().GetBool("all") {
+		utils.ShowError(fmt.Errorf("you need to provide at least one plugin to update or to update all plugins, use %s", constants.Bold("steampipe plugin update --all")))
+		return
+	}
 
 	// we can't allow update and install at the same time
 	if cmdconfig.Viper().GetBool("all") {
@@ -345,7 +355,7 @@ func runPluginUpdateCmd(cmd *cobra.Command, args []string) {
 		}
 		fmt.Printf("\nTo install these plugins, please run: %s %s\n", constants.Bold("steampipe plugin install"), constants.Bold(strings.Join(updateSkipped, " ")))
 	}
-	if len(args) > 1 {
+	if len(plugins) > 1 {
 		fmt.Println("")
 	}
 
