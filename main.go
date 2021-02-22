c694d66eb2b7dac518622f4001f9c60c1b1be191
Binaek Sarkar <binaek@turbot.com>
2021-02-22 22:48:44 +0530

    update messaging

--- main.go
+++ main.go
@@ -18,7 +18,6 @@ var Logger hclog.Logger
 
 func main() {
 
-	// make sure that we are not running as root
 	checkRoot()
 
 	/// setup logging
@@ -57,10 +56,9 @@ func createLogger() {
 func checkRoot() {
 	// return (os.Getuid() == 0)
 	if os.Geteuid() == 0 {
-		utils.ShowError(fmt.Errorf(`"root" execution of %s is not permitted. 
-%s must be started under an unprivileged user ID to prevent possible system security compromise`,
-			constants.Bold("steampipe"),
-			constants.Bold("steampipe"),
+		utils.ShowError(fmt.Errorf(`%s cannot be run as the "root" user.
+To reduce security risk, use an unprivileged user account instead.`,
+			constants.Bold("Steampipe"),
 		))
 
 		os.Exit(-1)
@@ -77,7 +75,46 @@ func checkRoot() {
 
 	if os.Geteuid() != os.Getuid() {
 		utils.ShowError(fmt.Errorf(`%s: real and effective user IDs must match`, constants.Bold("steampipe")))
-
 		os.Exit(-1)
 	}
 }
+
+// static void
+// check_root(const char *progname)
+// {
+// #ifndef WIN32
+// 	if (geteuid() == 0)
+// 	{
+// write_stderr("\"root\" execution of the PostgreSQL server is not permitted.\n"
+// 			 "The server must be started under an unprivileged user ID to prevent\n"
+// 			 "possible system security compromise.  See the documentation for\n"
+// 			 "more information on how to properly start the server.\n");
+// 		exit(1);
+// 	}
+
+/*
+ * Also make sure that real and effective uids are the same. Executing as
+ * a setuid program from a root shell is a security hole, since on many
+ * platforms a nefarious subroutine could setuid back to root if real uid
+ * is root.  (Since nobody actually uses postgres as a setuid program,
+ * trying to actively fix this situation seems more trouble than it's
+ * worth; we'll just expend the effort to check for it.)
+ */
+// if (getuid() != geteuid())
+// {
+// 	write_stderr("%s: real and effective user IDs must match\n",
+// 				 progname);
+// 	exit(1);
+// }
+// #else							/* WIN32 */
+// 	if (pgwin32_is_admin())
+// 	{
+// 		write_stderr("Execution of PostgreSQL by a user with administrative permissions is not\n"
+// 					 "permitted.\n"
+// 					 "The server must be started under an unprivileged user ID to prevent\n"
+// 					 "possible system security compromises.  See the documentation for\n"
+// 					 "more information on how to properly start the server.\n");
+// 		exit(1);
+// 	}
+// #endif							/* WIN32 */
+// }
