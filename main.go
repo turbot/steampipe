f45cde649a8e79f75eaa7e5791d0c41b7ed9e5c3
Binaek Sarkar <binaek@turbot.com>
2021-02-24 00:44:05 +0530

    adding relevant comment and removing irrelevant ones

--- main.go
+++ main.go
@@ -53,8 +53,9 @@ func createLogger() {
 	log.SetFlags(0)
 }
 
+// this is to replicate the user security mechanism of out underlying
+// postgresql engine.
 func checkRoot() {
-	// return (os.Getuid() == 0)
 	if os.Geteuid() == 0 {
 		utils.ShowError(fmt.Errorf(`%s cannot be run as the "root" user.
 To reduce security risk, use an unprivileged user account instead.`,
@@ -78,43 +79,3 @@ To reduce security risk, use an unprivileged user account instead.`,
 		os.Exit(-1)
 	}
 }
-
-// static void
-// check_root(const char *progname)
-// {
-// #ifndef WIN32
-// 	if (geteuid() == 0)
-// 	{
-// write_stderr("\"root\" execution of the PostgreSQL server is not permitted.\n"
-// 			 "The server must be started under an unprivileged user ID to prevent\n"
-// 			 "possible system security compromise.  See the documentation for\n"
-// 			 "more information on how to properly start the server.\n");
-// 		exit(1);
-// 	}
-
-/*
- * Also make sure that real and effective uids are the same. Executing as
- * a setuid program from a root shell is a security hole, since on many
- * platforms a nefarious subroutine could setuid back to root if real uid
- * is root.  (Since nobody actually uses postgres as a setuid program,
- * trying to actively fix this situation seems more trouble than it's
- * worth; we'll just expend the effort to check for it.)
- */
-// if (getuid() != geteuid())
-// {
-// 	write_stderr("%s: real and effective user IDs must match\n",
-// 				 progname);
-// 	exit(1);
-// }
-// #else							/* WIN32 */
-// 	if (pgwin32_is_admin())
-// 	{
-// 		write_stderr("Execution of PostgreSQL by a user with administrative permissions is not\n"
-// 					 "permitted.\n"
-// 					 "The server must be started under an unprivileged user ID to prevent\n"
-// 					 "possible system security compromises.  See the documentation for\n"
-// 					 "more information on how to properly start the server.\n");
-// 		exit(1);
-// 	}
-// #endif							/* WIN32 */
-// }
