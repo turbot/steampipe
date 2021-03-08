91c26e1ce83db75653e353830b1d0541426b722c
Binaek Sarkar <binaek@turbot.com>
2021-03-08 22:37:23 +0530

    using global viper instance

--- cmdconfig/cmd_flags.go
+++ cmdconfig/cmd_flags.go
@@ -5,11 +5,12 @@ import (
 
 	"github.com/fatih/color"
 	"github.com/spf13/cobra"
+	"github.com/spf13/viper"
 )
 
 var requiredColor = color.New(color.Bold).SprintfFunc()
 
-type flagOpt func(c *cobra.Command, name string, key string, v *ViperWrapper)
+type flagOpt func(c *cobra.Command, name string, key string)
 
 // FlagOptions :: shortcut for common flag options
 var FlagOptions = struct {
@@ -22,17 +23,17 @@ var FlagOptions = struct {
 
 // Helper function to mark a flag as required
 func requiredOpt() flagOpt {
-	return func(c *cobra.Command, name, key string, w *ViperWrapper) {
+	return func(c *cobra.Command, name, key string) {
 		c.MarkFlagRequired(key)
 		key = fmt.Sprintf("required.%s", key)
-		w.v.Set(key, true)
+		viper.GetViper().Set(key, true)
 		u := c.Flag(name).Usage
 		c.Flag(name).Usage = fmt.Sprintf("%s %s", u, requiredColor("(required)"))
 	}
 }
 
 func hiddenOpt() flagOpt {
-	return func(c *cobra.Command, name, key string, v *ViperWrapper) {
+	return func(c *cobra.Command, name, key string) {
 		c.Flag(name).Hidden = true
 	}
 }
--- cmdconfig/viper.go
+++ cmdconfig/viper.go
@@ -1,71 +1,25 @@
 package cmdconfig
 
 import (
-	"fmt"
 	"strings"
 
-	"github.com/spf13/cobra"
-	"github.com/spf13/pflag"
 	"github.com/spf13/viper"
 	"github.com/turbot/steampipe/constants"
 )
 
-var viperWrapper *ViperWrapper
+var viperWrapper *viper.Viper
 
 // InitViper :: initializes and configures an instance of viper
-func InitViper(w *ViperWrapper) {
-	w.v.SetEnvPrefix("STEAMPIPE")
-	w.v.AutomaticEnv()
-	w.v.SetEnvKeyReplacer(strings.NewReplacer("-", "_"))
+func InitViper() {
+	viper.GetViper().SetEnvPrefix("STEAMPIPE")
+	viper.GetViper().AutomaticEnv()
+	viper.GetViper().SetEnvKeyReplacer(strings.NewReplacer("-", "_"))
 
 	// set defaults
-	w.Set(constants.ShowInteractiveOutputConfigKey, true)
-}
-
-// sets a global viper instance
-func setConfig(v *ViperWrapper) {
-	viperWrapper = v
+	viper.GetViper().Set(constants.ShowInteractiveOutputConfigKey, true)
 }
 
 // Viper :: fetches the global viper instance
-func Viper() *ViperWrapper {
-	return viperWrapper
-}
-
-type ViperWrapper struct {
-	v      *viper.Viper
-	prefix string
-}
-
-func NewViperWrapper(cmd *cobra.Command) *ViperWrapper {
-	w := new(ViperWrapper)
-	w.v = viper.GetViper()
-	w.prefix = fmt.Sprintf("%s_%p", cmd.Use, cmd)
-	return w
-}
-func (w *ViperWrapper) GetString(key string) string {
-	return w.v.GetString(w.buildGetKey(key))
-}
-func (w *ViperWrapper) GetBool(key string) bool {
-	return w.v.GetBool(w.buildGetKey(key))
-}
-func (w *ViperWrapper) GetInt(key string) int {
-	return w.v.GetInt(w.buildGetKey(key))
-}
-func (w *ViperWrapper) Set(key string, value interface{}) {
-	w.v.Set(w.buildSetKey(key), value)
-}
-func (w *ViperWrapper) BindPFlag(key string, flag *pflag.Flag) error {
-	newKey := w.buildGetKey(key)
-	return w.v.BindPFlag(newKey, flag)
-}
-func (w *ViperWrapper) buildSetKey(key string) string {
-	return fmt.Sprintf("%s_%s", w.prefix, key)
-}
-func (w *ViperWrapper) buildGetKey(key string) string {
-	prefixedKey := fmt.Sprintf("%s_%s", w.prefix, key)
-	if w.v.IsSet(prefixedKey) {
-		return prefixedKey
-	}
-	return key
+func Viper() *viper.Viper {
+	return viper.GetViper()
 }
