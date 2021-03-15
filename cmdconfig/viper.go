8826c621c2a066d42bb30b6b0e650df333d5aaaf
Binaek Sarkar <binaek@turbot.com>
2021-03-15 19:12:59 +0530

    `install-dir` in viper. give up auto env extraction

--- cmdconfig/viper.go
+++ cmdconfig/viper.go
@@ -1,21 +1,24 @@
 package cmdconfig
 
 import (
-	"strings"
+	"os"
 
 	"github.com/spf13/viper"
 	"github.com/turbot/steampipe/constants"
 )
 
 // InitViper :: initializes and configures an instance of viper
-func InitViper() {
-	v := viper.GetViper()
-	v.SetEnvPrefix("STEAMPIPE")
-	v.AutomaticEnv()
-	v.SetEnvKeyReplacer(strings.NewReplacer("-", "_"))
-
+func InitViper(v *viper.Viper) {
 	// set defaults
 	v.Set(constants.ShowInteractiveOutputConfigKey, true)
+
+	installDir, isSet := os.LookupEnv("STEAMPIPE_INSTALL_DIR")
+
+	if isSet {
+		v.SetDefault(constants.ArgInstallDir, installDir)
+	} else {
+		v.SetDefault(constants.ArgInstallDir, "~/.steampipe")
+	}
 }
 
 // Viper :: fetches the global viper instance
