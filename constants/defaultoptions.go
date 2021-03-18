c31018ef83f55e5d7b29cc3d7a0db8d89078a13f
kai <kai@turbot.com>
2021-03-18 16:36:17 +0000

    Update formatting of default options

--- constants/defaultoptions.go
+++ constants/defaultoptions.go
@@ -2,25 +2,24 @@ package constants
 
 const DefaultSPCContent = `
 # options "connection" {
-#     cache           = true   # true, false
-#     cache_ttl       = 300    # int = time in seconds
+#   cache     = true    # true, false
+#   cache_ttl = 300     # int = time in seconds
 # }
 
 # options "database" {
-#     port   = 9193
-#     listen = "local"
+#   port   = 9193
+#   listen = "local"
 # }
 
 # options "terminal" {
-#     multi       = false      # true, false
-#     output      = "table"    # json, csv, table, line
-#     header      = false      # true, false
-#     separator   = ","        # any single char
-#     timing      = false      # true, false
+#   multi     = false      # true, false
+#   output    = "table"    # json, csv, table, line
+#   header    = false      # true, false
+#   separator = ","        # any single char
+#   timing    = false      # true, false
 # }
 
 # options "general" {
-#     update_check  = true     # true, false
+#   update_check = true    # true, false
 # }
-
 `
