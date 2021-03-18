91bfe54e66b2f11ee0da672bcefd44ad7a5eac4a
kai <kai@turbot.com>
2021-03-18 13:57:21 +0000

    Update fdw version to 0.0.27. update version to v0.3.0-rc2 #309

--- version/version.go
+++ version/version.go
@@ -24,7 +24,7 @@ var steampipeVersion = "0.3.0"
 // A pre-release marker for the version. If this is "" (empty string)
 // then it means that it is a final release. Otherwise, this is a pre-release
 // such as "dev" (in development), "beta", "rc1", etc.
-var prerelease = "rc.1"
+var prerelease = "rc.2"
 
 // semVer is an instance of version.Version. This has the secondary
 // benefit of verifying during tests and init time that our version is a
