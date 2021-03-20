23c2ac9e0413f0608fdf1237c7dd419d24d7626c
Nathan Wallace <nathan@turbot.com>
2021-03-20 14:46:34 -0400

    v0.3.2

--- version/version.go
+++ version/version.go
@@ -19,7 +19,7 @@ Also https://www.digitalocean.com/community/tutorials/using-ldflags-to-set-versi
 **/
 
 // The main version number that is being run at the moment.
-var steampipeVersion = "0.3.1"
+var steampipeVersion = "0.3.2"
 
 // A pre-release marker for the version. If this is "" (empty string)
 // then it means that it is a final release. Otherwise, this is a pre-release
