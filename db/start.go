45b6b692d9392d3ba45c5c4ca9ccc3379cb6115a
Binaek Sarkar <binaek@turbot.com>
2021-03-22 20:27:41 +0530

    review corrections

--- db/start.go
+++ db/start.go
@@ -206,7 +206,9 @@ func StartDB(port int, listen StartListenType, invoker Invoker) (StartResult, er
 
 	err = ensureSteampipeServer()
 	if err != nil {
-		return ServiceStarted, err
+		// there was a problem with the installation
+		StopDB(true, invoker)
+		return ServiceFailedToStart, err
 	}
 
 	// refresh plugin connections - ensure db schemas are in sync with connection config
@@ -223,14 +225,16 @@ func StartDB(port int, listen StartListenType, invoker Invoker) (StartResult, er
 	return ServiceStarted, nil
 }
 
+// ensures that the `steampipe` fdw server exists
+// checks for it - (re)installs FDW and creates server if it doesn't
 func ensureSteampipeServer() error {
 	rawClient, err := createSteampipeRootDbClient()
 	if err != nil {
 		return err
 	}
 	out := rawClient.QueryRow("select srvname from pg_catalog.pg_foreign_server where srvname='steampipe'")
-	var srvname string
-	err = out.Scan(&srvname)
+	var serverName string
+	err = out.Scan(&serverName)
 	rawClient.Close()
 	if err != nil {
 		return installSteampipeHub()
