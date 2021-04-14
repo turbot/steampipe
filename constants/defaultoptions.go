541683584ea5320f46b5df8dfb2c862ba7d65092
kai <kai@turbot.com>
2021-04-14 10:26:17 +0100

    update default options

--- constants/defaultoptions.go
+++ constants/defaultoptions.go
@@ -7,16 +7,19 @@ const DefaultSPCContent = `
 # }
 
 # options "database" {
-#   port   = 9193    # any valid, open port number
-#   listen = "local" # local, network
+#   port        = 9193    # any valid, open port number
+#   listen      = "local" # local, network
+#   search_path =  ""     # comma-separated string; an exact search_path
 # }
 
 # options "terminal" {
-#   multi     = false   # true, false
-#   output    = "table" # json, csv, table, line
-#   header    = true    # true, false
-#   separator = ","     # any single char
-#   timing    = false   # true, false
+#   multi               = false   # true, false
+#   output              = "table" # json, csv, table, line
+#   header              = true    # true, false
+#   separator           = ","     # any single char
+#   timing      	    = false   # true, false
+#   search_path         =  ""     # comma-separated string; an exact search_path
+#   search_path_prefix  =  ""     # comma-separated string; a search_path_prefix to prepend to the search_path
 # }
 
 # options "general" {
