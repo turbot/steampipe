a25b1806dd5c92af48d69e9389639485393ee8c8
Binaek Sarkar <binaek@turbot.com>
2021-04-15 15:04:04 +0530

    Save namedQuery name in history, instead of the SQL. Move `watch` argument to query command. Closes #383

--- cmd/query.go
+++ cmd/query.go
@@ -49,6 +49,7 @@ Examples:
 		AddStringFlag(constants.ArgSeparator, "", ",", "Separator string for csv output").
 		AddStringFlag(constants.ArgOutput, "", "table", "Output format: line, csv, json or table").
 		AddBoolFlag(constants.ArgTimer, "", false, "Turn on the timer which reports query time.").
+		AddBoolFlag(constants.ArgWatch, "", false, "Watch SQL files in the current workspace (works only in interactive mode)").
 		AddStringSliceFlag(constants.ArgSearchPath, "", []string{}, "Set a custom search_path for the steampipe user for a query session (comma-separated)").
 		AddStringSliceFlag(constants.ArgSearchPathPrefix, "", []string{}, "Set a prefix to the current search path for a query session (comma-separated)")
 
