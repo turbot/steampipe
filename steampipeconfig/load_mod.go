979a5452a5739230da7c02fb99cf9c354d3d4f68
kaidaguerre <kai@turbot.com>
2021-04-15 14:40:58 +0100

    Update default options. And Command arg descriptions. Remove error for empty query file, and just log instead.  Close #382

--- steampipeconfig/load_mod.go
+++ steampipeconfig/load_mod.go
@@ -227,8 +227,10 @@ func createPseudoResources(modPath string, parseResults *modParseResult, opts *L
 			errors = append(errors, err)
 			continue
 		}
-		if err := addResourceIfUnique(resource, parseResults, path); err != nil {
-			errors = append(errors, err)
+		if resource != nil {
+			if err := addResourceIfUnique(resource, parseResults, path); err != nil {
+				errors = append(errors, err)
+			}
 		}
 	}
 
