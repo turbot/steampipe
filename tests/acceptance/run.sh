b0d241572d44b17f25fa7c0a6acb11e98fe45602
Binaek Sarkar <binaek@turbot.com>
2021-06-07 23:36:30 +0530

    sample workspace location

--- tests/acceptance/run.sh
+++ tests/acceptance/run.sh
@@ -12,6 +12,7 @@ export LIB_BATS_ASSERT=$MY_PATH/lib/bats-assert
 export LIB_BATS_SUPPORT=$MY_PATH/lib/bats-support
 export TEST_DATA_DIR=$MY_PATH/test_data/templates
 export SRC_DATA_DIR=$MY_PATH/test_data/source_files
+export WORKSPACE_DIR=$MY_PATH/test_data/sample_workspace
 
 # Must have these commands for the test suite to run
 declare -a required_commands=("jq" "sed" "steampipe" "rm" "mv" "cp" "mkdir" "cd" "head" "wc" "find" "basename" "dirname" "touch")
