a01e96fdc0a5275be24040a8e7f2166ba2c628b2
Binaek Sarkar <binaek@turbot.com>
2021-07-14 15:44:28 +0530

    update

--- db/install.go
+++ db/install.go
@@ -206,7 +206,7 @@ func doInit(firstInstall bool, spinner *spinner.Spinner) error {
 	}
 	// force stop
 	display.UpdateSpinnerMessage(spinner, "Completing configuration")
-	_, err = StopDB(false, InvokerInstaller, nil)
+	_, err = StopDB(false, InvokerInstaller, spinner)
 
 	return err
 }
