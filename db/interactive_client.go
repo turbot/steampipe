413d10a5f3d8efd2a6959e74f79dc7c2cd31e4cd
kai <kai@turbot.com>
2021-07-19 13:27:05 +0100

    error formatting

--- db/interactive_client.go
+++ db/interactive_client.go
@@ -140,8 +140,10 @@ func (c *InteractiveClient) handleInitResult(ctx context.Context, initResult *In
 
 	// if there is an error, shutdown
 	if initResult.Error != nil {
-		utils.ShowError(initResult.Error)
 		c.ClosePrompt(AfterPromptCloseExit)
+		// add newline to ensure error is not printed at end of current prompt line
+		fmt.Println()
+		utils.ShowError(initResult.Error)
 		return initResult.Error
 	}
 	if initResult.HasMessages() {
