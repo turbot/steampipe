6f97ee32dc7fc9aaab676fd75eed6ad8d43b52e9
kai <kai@turbot.com>
2021-07-26 17:05:16 +0100

    Update CHANGELOG for v0.7.1 #697

--- version/version.go
+++ version/version.go
@@ -19,12 +19,12 @@ Also https://www.digitalocean.com/community/tutorials/using-ldflags-to-set-versi
 **/
 
 // The main version number that is being run at the moment.
-var steampipeVersion = "0.7.0"
+var steampipeVersion = "0.7.1"
 
 // A pre-release marker for the version. If this is "" (empty string)
 // then it means that it is a final release. Otherwise, this is a pre-release
 // such as "dev" (in development), "beta", "rc1", etc.
-var prerelease = ""
+var prerelease = "rc.0"
 
 // semVer is an instance of version.Version. This has the secondary
 // benefit of verifying during tests and init time that our version is a
