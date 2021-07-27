77ca6b1899c70e993257ab361903d0d83a303dc7
Binaek Sarkar <binaek@turbot.com>
2021-07-27 18:08:44 +0530

    adding interactive cache command

--- constants/commands.go
+++ constants/commands.go
@@ -18,6 +18,7 @@ const (
 	CmdHelp             = ".help"               // list all meta commands
 	CmdSearchPath       = ".search_path"        // Set or show search-path
 	CmdSearchPathPrefix = ".search_path_prefix" // set search path prefix
+	CmdCache            = ".cache"              // cache control
 )
 
 // ArgFromMetaquery converts a metaquery of form '.header' into the config argument used to set the mode, i.e. 'header'
