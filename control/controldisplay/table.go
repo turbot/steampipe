dc213cd6238d0a5b86c53bc8981aa0b6bb5f8298
Binaek Sarkar <binaek@turbot.com>
2021-08-04 14:31:05 +0530

    first cut

--- control/controldisplay/table.go
+++ control/controldisplay/table.go
@@ -1,6 +1,9 @@
 package controldisplay
 
 import (
+	"bytes"
+
+	"github.com/jedib0t/go-pretty/v6/table"
 	"github.com/turbot/steampipe/control/controlexecute"
 )
 
@@ -23,6 +26,46 @@ func NewTableRenderer(resultTree *controlexecute.ExecutionTree, width int) *Tabl
 }
 
 func (r TableRenderer) Render() string {
-	// leading newline
+	// the buffer to put the output data in
+	outbuf := bytes.NewBufferString("")
+
+	outbuf.WriteString(r.renderSummary())
+	outbuf.WriteString("\n")
+	outbuf.WriteString(r.renderResult())
+
+	return outbuf.String()
+}
+
+func (r TableRenderer) renderSummary() string {
+	// the table
+	t := table.NewWriter()
+	t.SetStyle(table.StyleDefault)
+
+	colConfigs := []table.ColumnConfig{}
+	headers := make(table.Row, 5)
+
+	for idx, column := range []string{"Alarm", "Ok", "Info", "Skip", "Error"} {
+		headers[idx] = column
+		colConfigs = append(colConfigs, table.ColumnConfig{
+			Name:     column,
+			Number:   idx + 1,
+			WidthMin: (r.width / 5),
+			WidthMax: (r.width / 5),
+		})
+	}
+	t.SetColumnConfigs(colConfigs)
+	t.AppendHeader(headers)
+
+	t.AppendRow(table.Row{
+		r.resultTree.Root.Summary.Status.Alarm,
+		r.resultTree.Root.Summary.Status.Ok,
+		r.resultTree.Root.Summary.Status.Info,
+		r.resultTree.Root.Summary.Status.Skip,
+		r.resultTree.Root.Summary.Status.Error,
+	})
+	return t.Render()
+}
+
+func (r TableRenderer) renderResult() string {
 	return NewGroupRenderer(r.resultTree.Root, nil, r.maxFailedControls, r.maxTotalControls, r.resultTree, r.width).Render()
 }
