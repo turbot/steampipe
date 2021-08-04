e912c9da18305d11596908b03944d7ed11d59fc0
Puskar Basu <puskar@turbot.com>
2021-08-04 16:58:30 +0530

    Add acceptance tests for "search path not being honored by steampipe check". Closes #721

---
+++ tests/acceptance/test_data/sample_workspace/query/search_path_1.sql
@@ -0,0 +1,7 @@
+WITH s_path AS (select setting from pg_settings where name='search_path') 
+SELECT s_path.setting as resource, 
+CASE 
+    WHEN s_path.setting LIKE 'aws%' THEN 'ok' 
+    ELSE 'alarm' 
+END as status, '' as reason
+FROM s_path
