ca4d65599920cd6aa5a1d623ab30cdc9afec6fbb
kai <kai@turbot.com>
2021-08-18 18:39:52 +0100

    revert acc tests

--- tests/acceptance/test_data/sample_workspace/query/check_plugincrash_normalquery2.sql
+++
@@ -1,8 +0,0 @@
-select 
-    case
-        when mod(id,2)=0 then 'alarm'
-        when mod(id,2)=1 then 'ok'
-    end status,
-    fatal_error as resource,
-    retryable_error as reason
-from chaos_get_errors limit 10
