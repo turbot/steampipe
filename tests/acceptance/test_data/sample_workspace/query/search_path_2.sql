3da1c4b5ceb60c1dd483406d68be032d59bedee6
Puskar Basu <45908484+pskrbasu@users.noreply.github.com>
2021-08-24 14:31:29 +0530

    Update search-path acceptance tests to modify the queries. Closes #793

--- tests/acceptance/test_data/sample_workspace/query/search_path_2.sql
+++ tests/acceptance/test_data/sample_workspace/query/search_path_2.sql
@@ -3,5 +3,9 @@ SELECT s_path.setting as resource,
 CASE 
     WHEN s_path.setting LIKE 'a, b, c%' THEN 'ok' 
     ELSE 'alarm' 
-END as status, '' as reason
+END as status,
+CASE
+    WHEN s_path.setting LIKE 'aws%' THEN 'Starts with "a, b, c"'
+    ELSE 'Does not start with "a, b, c"'
+END as reason
 FROM s_path
