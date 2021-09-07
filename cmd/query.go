a9ac60509cada6444a8ba0e90d0cde7daef9ca52
kai <kai@turbot.com>
2021-09-07 10:30:14 +0100

    tidy

--- cmd/query.go
+++ cmd/query.go
@@ -209,15 +209,19 @@ func getQueryInitDataAsync(ctx context.Context, workspace *workspace.Workspace, 
 
 		utils.LogTime("cmd.getQueryInitDataAsync CreatePreparedStatements")
 
-		createPreparedStatements := true
-		if createPreparedStatementsEnv, exists := os.LookupEnv("SP_CREATE_PREPARED_STATEMENTS"); exists {
-			createPreparedStatements = strings.ToUpper(createPreparedStatementsEnv) == "TRUE"
-		}
-
-		if createPreparedStatements {
+		if shouldCreatePreparedStatements() {
 			initData.Result.Error = db_common.CreatePreparedStatements(ctx, workspace.GetResourceMaps(), client)
 		}
 	}()
+}
+
+// check env var to see if prepared statement creation has been disabled
+func shouldCreatePreparedStatements() bool {
+	createPreparedStatements := true
+	if createPreparedStatementsEnv, exists := os.LookupEnv("SP_CREATE_PREPARED_STATEMENTS"); exists {
+		createPreparedStatements = strings.ToUpper(createPreparedStatementsEnv) == "TRUE"
+	}
+	return createPreparedStatements
 }
 
 func startCancelHandler(cancel context.CancelFunc) {
