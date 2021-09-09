1cf5a82b7d4f9b7b455b7445a921ee061cfb8d6c
Binaek Sarkar <binaek@turbot.com>
2021-09-09 19:35:39 +0530

    Update completion cmd help text

--- cmd/completion.go
+++ cmd/completion.go
@@ -3,53 +3,25 @@ package cmd
 import (
 	"fmt"
 	"os"
+	"runtime"
 
 	"github.com/spf13/cobra"
 	"github.com/turbot/steampipe/cmdconfig"
-	"github.com/turbot/steampipe/utils"
 )
 
 func generateCompletionScriptsCmd() *cobra.Command {
 	var cmd = &cobra.Command{
 		Use:                   "completion [bash|zsh|fish]",
-		Args:                  cobra.ExactValidArgs(1),
+		Args:                  cobra.ArbitraryArgs,
 		DisableFlagsInUseLine: true,
 		ValidArgs:             []string{"bash", "zsh", "fish"},
 		Run:                   runGenCompletionScriptsCmd,
-		Short:                 "Generate completion script",
-		Long: `
-To load completions:
-    Bash:
-        # To load for the current session
-        $ source <(steampipe completion bash)
-        
-        # To load completions for every session, execute once:
-        # Linux:
-        $ steampipe completion bash > /etc/bash_completion.d/steampipe
-        
-        # macOS:
-        $ steampipe completion bash > /usr/local/etc/bash_completion.d/steampipe
-    
-    Zsh:
-        # If shell completion is not already enabled in your environment,
-        # you will need to enable it.  You can execute the following once:
-        $ echo "autoload -U compinit; compinit" >> ~/.zshrc
-        
-        # To load completions for each session, execute once:
-        $ steampipe completion zsh > "${fpath[1]}/steampipe"
-        
-        # You will need to start a new shell for this setup to take effect.
-        
-    fish:
-        
-        $ steampipe completion fish | source
-        
-        # To load completions for each session, execute once:
-        $ steampipe completion fish > ~/.config/fish/completions/steampipe.fish
-`,
+		Short:                 "Generate completion scripts",
 	}
 
 	cmd.ResetFlags()
+
+	cmd.SetHelpFunc(completionHelp)
 
 	cmdconfig.
 		OnCmd(cmd)
@@ -57,17 +29,100 @@ To load completions:
 	return cmd
 }
 
+func includeBashHelp(base string) string {
+	buildUp := base
+	buildUp = fmt.Sprintf(`%s
+  Bash:`, buildUp)
+
+	if runtime.GOOS == "darwin" {
+		buildUp = fmt.Sprintf(`%s
+    # Load for the current session:
+    $ source <(steampipe completion bash)
+		
+    # Load for every session (requires shell restart):
+    $ steampipe completion bash > $(brew --prefix)/etc/bash_completion.d/steampipe
+`, buildUp)
+	} else if runtime.GOOS == "linux" {
+		buildUp = fmt.Sprintf(`%s
+    # Load for the current session:
+    $ source <(steampipe completion bash)
+
+    # Load for every session (requires shell restart):
+    $ steampipe completion bash > /etc/bash_completion.d/steampipe
+	`, buildUp)
+	}
+
+	return buildUp
+}
+
+func includeZshHelp(base string) string {
+	buildUp := base
+
+	if runtime.GOOS == "darwin" {
+		buildUp = fmt.Sprintf(`%s
+  Zsh:
+    # Load for every session (requires shell restart):
+    $ steampipe completion zsh > "${fpath[1]}/steampipe"
+`, buildUp)
+	}
+
+	return buildUp
+}
+
+func includeFishHelp(base string) string {
+	buildUp := base
+
+	buildUp = fmt.Sprintf(`%s
+  fish:
+    # Load for the current session:
+    $ steampipe completion fish | source
+    
+    # Load for every session (requires shell restart):
+    $ steampipe completion fish > ~/.config/fish/completions/steampipe.fish
+	`, buildUp)
+
+	return buildUp
+}
+
+func completionHelp(cmd *cobra.Command, args []string) {
+	helpString := ""
+
+	if runtime.GOOS == "darwin" {
+		helpString = `
+Note: Completions must be enabled in your environment. Please refer to: https://steampipe.io/docs/reference/cli-args#steampipe-completion
+	
+To load completions:
+`
+	} else if runtime.GOOS == "linux" {
+		helpString = `
+To load completions:
+`
+	}
+
+	helpString = includeBashHelp(helpString)
+	helpString = includeZshHelp(helpString)
+	helpString = includeFishHelp(helpString)
+
+	fmt.Println(helpString)
+	fmt.Println(cmd.UsageString())
+}
+
 func runGenCompletionScriptsCmd(cmd *cobra.Command, args []string) {
+	if len(args) != 1 {
+		completionHelp(cmd, args)
+		return
+	}
+
 	completionFor := args[0]
 
 	switch completionFor {
 	case "bash":
-		cmd.Root().GenBashCompletionV2(os.Stdout, true)
+		cmd.Root().GenBashCompletionV2(os.Stdout, false)
 	case "zsh":
-		cmd.Root().GenZshCompletion(os.Stdout)
+		cmd.Root().GenZshCompletionNoDesc(os.Stdout)
 	case "fish":
-		cmd.Root().GenFishCompletion(os.Stdout, true)
+		cmd.Root().GenFishCompletion(os.Stdout, false)
 	default:
-		utils.ShowError(fmt.Errorf("completion for '%s' is not supported yet", completionFor))
+		completionHelp(cmd, args)
 	}
 }
