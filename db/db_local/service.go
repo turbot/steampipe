db2c1dd285fb9de9f24e9d8a33b04ba41308b1bb
kai <kai@turbot.com>
2021-09-13 15:59:35 +0100

    fix after rebase

--- db/db_local/service.go
+++ db/db_local/service.go
@@ -1,9 +1,14 @@
 package db_local
 
 import (
+	"fmt"
+	"io/ioutil"
 	"log"
 	"os"
+	"strconv"
+	"strings"
 
+	"github.com/turbot/go-kit/helpers"
 	"github.com/turbot/steampipe/constants"
 
 	"github.com/turbot/steampipe/utils"
@@ -43,7 +48,7 @@ func EnsureDbAndStartService(invoker constants.Invoker) error {
 	return nil
 }
 
-// GetStatus checks that the db instance is running and returns its details
+// GetStatus :: check that the db instance is running and returns it's details
 func GetStatus() (*RunningDBInstanceInfo, error) {
 	utils.LogTime("db.GetStatus start")
 	defer utils.LogTime("db.GetStatus end")
@@ -56,7 +61,7 @@ func GetStatus() (*RunningDBInstanceInfo, error) {
 	if info == nil {
 		log.Println("[TRACE] GetRunStatus - loadRunningInstanceInfo returned nil ")
 		// we do not have a info file
-		return nil, nil
+		return nil, errorIfUnknownService()
 	}
 
 	pidExists, err := PidExists(info.Pid)
@@ -71,4 +76,59 @@ func GetStatus() (*RunningDBInstanceInfo, error) {
 	}
 
 	return info, nil
+}
+
+// errorIfUnknownService returns an error if it can find a `postmaster.pid` in the `INSTALL_DIR`
+// and the PID recorded in the found `postmaster.pid` is running - nil otherwise.
+//
+// This is because, this function is called when we cannot find the steampipe service state file.
+//
+// No steampipe state file indicates that the service is not running, so, if the service
+// is running without us knowing about it, then it's an irrecoverable state
+//
+func errorIfUnknownService() error {
+	// no postmaster.pid, we are good
+	if !helpers.FileExists(getPostmasterPidLocation()) {
+		return nil
+	}
+
+	// read the content of the postmaster.pid file
+	fileContent, err := ioutil.ReadFile(getPostmasterPidLocation())
+	if err != nil {
+		return err
+	}
+
+	// the first line contains the PID
+	lines := strings.FieldsFunc(string(fileContent), func(r rune) bool {
+		return r == '\n'
+	})
+
+	// make sure that there's split up content
+	if len(lines) == 0 {
+		return nil
+	}
+
+	// extract it
+	pid, err := strconv.ParseInt(lines[0], 10, 64)
+	if err != nil {
+		return err
+	}
+
+	// check if a process with that PID exists
+	exists, err := PidExists(int(pid))
+	if err != nil {
+		return err
+	}
+	if exists {
+		// if it does, then somehow we don't know about it. Error out
+		return fmt.Errorf("service is running in an unknown state [PID: %d] - try killing it with %s", pid, constants.Bold("steampipe service stop --force"))
+	}
+
+	// the pid does not exist
+	// this can confuse postgres as per https://postgresapp.com/documentation/troubleshooting.html
+	// delete it
+	os.Remove(getPostmasterPidLocation())
+
+	// this must be a stale file left over by PG. Ignore
+	return nil
 }
