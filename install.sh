a489051b7027b2f3c5cadd83162ff674e90fdd19
Binaek Sarkar <binaek@turbot.com>
2021-09-21 16:47:59 +0530

    explanation for preferring IPv4 in wget

--- install.sh
+++ install.sh
@@ -58,6 +58,8 @@ echo "Downloading from $steampipe_uri"
 if command -v wget >/dev/null; then
 	# because --show-progress was introduced in 1.16.
 	wget --help | grep -q '\--showprogress' && _FORCE_PROGRESS_BAR="--no-verbose --show-progress" || _FORCE_PROGRESS_BAR=""
+	# prefer an IPv4 connection, since github.com does not handle IPv6 connections properly.
+	# Refer: https://github.com/turbot/steampipe/issues/861
 	if ! wget --prefer-family=IPv4 --progress=bar:force:noscroll $_FORCE_PROGRESS_BAR -O "$zip_location" "$steampipe_uri"; then
         echo "Could not find version $1"
         exit 1
