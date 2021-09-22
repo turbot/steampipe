00d6dafffa78182b811420d2a24cc1b8563efaf0
Puskar Basu <45908484+pskrbasu@users.noreply.github.com>
2021-09-22 20:01:11 +0530

    Add acceptance test for testing time required to query a chaos table. Closes #913

--- tests/acceptance/run-local.sh
+++ tests/acceptance/run-local.sh
@@ -4,6 +4,8 @@ MY_PATH="`dirname \"$0\"`"              # relative
 MY_PATH="`( cd \"$MY_PATH\" && pwd )`"  # absolutized and normalized
 
 export STEAMPIPE_INSTALL_DIR=$(mktemp -d)
+export TIME_TO_QUERY=3                  # overriding since it takes more than 2secs to run locally
+
 trap "code=$?;rm -rf $STEAMPIPE_INSTALL_DIR; exit $code" EXIT
 
 source $MY_PATH/run.sh
--- tests/acceptance/run.sh
+++ tests/acceptance/run.sh
@@ -6,6 +6,11 @@ then
   MY_PATH="`( cd \"$MY_PATH\" && pwd )`"  # absolutized and normalized  
 fi
 
+if [[ ! ${TIME_TO_QUERY} ]];
+then
+  TIME_TO_QUERY=2
+fi
+
 # set this to the source file for development
 export BATS_PATH=$MY_PATH/lib/bats/bin/bats
 export LIB_BATS_ASSERT=$MY_PATH/lib/bats-assert
@@ -14,6 +19,7 @@ export TEST_DATA_DIR=$MY_PATH/test_data/templates
 export SRC_DATA_DIR=$MY_PATH/test_data/source_files
 export WORKSPACE_DIR=$MY_PATH/test_data/sample_workspace
 export BAD_TEST_MOD_DIR=$MY_PATH/test_data/failure_test_mod
+export TIME_TO_QUERY=$TIME_TO_QUERY
 
 # Must have these commands for the test suite to run
 declare -a required_commands=("jq" "sed" "steampipe" "rm" "mv" "cp" "mkdir" "cd" "head" "wc" "find" "basename" "dirname" "touch")
