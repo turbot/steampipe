5356ce1e5f51f4639504e4d5434310967f25c33d
Puskar Basu <puskar@turbot.com>
2021-09-22 19:56:30 +0530

    Added comment

--- tests/acceptance/run-local.sh
+++ tests/acceptance/run-local.sh
@@ -4,7 +4,7 @@ MY_PATH="`dirname \"$0\"`"              # relative
 MY_PATH="`( cd \"$MY_PATH\" && pwd )`"  # absolutized and normalized
 
 export STEAMPIPE_INSTALL_DIR=$(mktemp -d)
-export TIME_TO_QUERY=3
+export TIME_TO_QUERY=3                  # overriding since it takes more than 2secs to run locally
 
 trap "code=$?;rm -rf $STEAMPIPE_INSTALL_DIR; exit $code" EXIT
 
