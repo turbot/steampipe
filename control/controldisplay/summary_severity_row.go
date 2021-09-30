e1b9bcc8de7682bd03320680873217c9c556ea43
Binaek Sarkar <binaek@turbot.com>
2021-09-30 15:59:12 +0530

    renderers for summary

---
+++ control/controldisplay/summary_severity_row.go
@@ -0,0 +1,84 @@
+package controldisplay
+
+import (
+	"fmt"
+	"strings"
+
+	"github.com/turbot/go-kit/helpers"
+	"github.com/turbot/steampipe/control/controlexecute"
+)
+
+type SummarySeverityRowRenderer struct {
+	resultTree *controlexecute.ExecutionTree
+	width      int
+	severity   string
+}
+
+func NewSummarySeverityRowRenderer(resultTree *controlexecute.ExecutionTree, width int, severity string) *SummarySeverityRowRenderer {
+	fmt.Println("NewSummarySeverityRowRenderer", width, severity)
+	return &SummarySeverityRowRenderer{
+		resultTree: resultTree,
+		width:      width,
+		severity:   severity,
+	}
+}
+
+func (r *SummarySeverityRowRenderer) countWithSeverity(group *controlexecute.ResultGroup) (int, int) {
+	runsWithThisSeverity := 0
+	runsWithSeverityValue := 0
+
+	for _, subGroup := range group.Groups {
+		v, any := r.countWithSeverity(subGroup)
+		runsWithThisSeverity += v
+		runsWithSeverityValue += any
+	}
+
+	for _, run := range group.ControlRuns {
+		if len(run.Severity) == 0 {
+			continue
+		}
+
+		runsWithSeverityValue += 1
+		if strings.EqualFold(run.Severity, r.severity) {
+			runsWithThisSeverity += 1
+		}
+	}
+
+	return runsWithThisSeverity, runsWithSeverityValue
+}
+
+func (r *SummarySeverityRowRenderer) Render() string {
+	colorFunc := ControlColors.Severity
+
+	severityStr := fmt.Sprintf("%s ", colorFunc(strings.ToUpper(r.severity)))
+	sevCount, withSevValueCount := r.countWithSeverity(r.resultTree.Root)
+
+	count := NewCounterRenderer(
+		sevCount,
+		withSevValueCount,
+		r.resultTree.Root.Summary.Status.FailedCount(), // not sure what this is
+		r.resultTree.Root.Summary.Status.TotalCount(),
+	).Render()
+
+	graph := NewCounterGraphRenderer(
+		sevCount,
+		withSevValueCount,
+		r.resultTree.Root.Summary.Status.TotalCount(),
+	).Render()
+
+	spaceAvailable := r.width - (helpers.PrintableLength(severityStr) + helpers.PrintableLength(count) + helpers.PrintableLength(graph))
+	space := ""
+	if r.severity == "critical" {
+		space = NewSpacerRenderer(4).Render()
+	} else {
+		space = NewSpacerRenderer(spaceAvailable).Render()
+	}
+
+	return fmt.Sprintf(
+		"%s%s%s%s",
+		severityStr,
+		space,
+		colorFunc(count),
+		colorFunc(graph),
+	)
+}
---
+++ control/controldisplay/summary_status_row.go
@@ -0,0 +1,67 @@
+package controldisplay
+
+import (
+	"fmt"
+	"strings"
+
+	"github.com/turbot/go-kit/helpers"
+	"github.com/turbot/steampipe/control/controlexecute"
+	"golang.org/x/text/language"
+	"golang.org/x/text/message"
+)
+
+type SummaryStatusRowRenderer struct {
+	resultTree *controlexecute.ExecutionTree
+	width      int
+	status     string
+}
+
+func NewSummaryStatusRowRenderer(resultTree *controlexecute.ExecutionTree, width int, status string) *SummaryStatusRowRenderer {
+	return &SummaryStatusRowRenderer{
+		resultTree: resultTree,
+		width:      width,
+		status:     status,
+	}
+}
+
+func (r *SummaryStatusRowRenderer) Render() string {
+
+	colorFunction := ControlColors.StatusColors[strings.ToLower(r.status)]
+	count := -1
+	switch r.status {
+	case "ok":
+		count = r.resultTree.Root.Summary.Status.Ok
+	case "skip":
+		count = r.resultTree.Root.Summary.Status.Skip
+	case "info":
+		count = r.resultTree.Root.Summary.Status.Info
+	case "alarm":
+		count = r.resultTree.Root.Summary.Status.Alarm
+	case "error":
+		count = r.resultTree.Root.Summary.Status.Error
+	}
+	countString := r.getPrintableNumber(count, colorFunction)
+
+	graph := NewCounterGraphRenderer(
+		0,
+		count,
+		r.resultTree.Root.Summary.Status.TotalCount(),
+	).Render()
+
+	statusStr := fmt.Sprintf("%s ", colorFunction(strings.ToUpper(r.status)))
+	spaceAvailableForSpacer := r.width - (helpers.PrintableLength(statusStr) + helpers.PrintableLength(countString) + helpers.PrintableLength(graph))
+	spacer := NewSpacerRenderer(spaceAvailableForSpacer)
+
+	return fmt.Sprintf(
+		"%s%s%s%s",
+		statusStr,
+		colorFunction(spacer.Render()),
+		countString,
+		graph,
+	)
+}
+func (r *SummaryStatusRowRenderer) getPrintableNumber(number int, cf colorFunc) string {
+	p := message.NewPrinter(language.English)
+	s := p.Sprintf("%d", number)
+	return fmt.Sprintf("%s ", cf(s))
+}
