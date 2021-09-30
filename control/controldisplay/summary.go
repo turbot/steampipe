1fa49c734aaaca16339f5863de6566de8f030f4c
Binaek Sarkar <binaek@turbot.com>
2021-09-30 16:00:44 +0530

    using summary renderers

--- control/controldisplay/summary.go
+++ control/controldisplay/summary.go
@@ -1,72 +1,79 @@
 package controldisplay
 
 import (
-	"bytes"
 	"fmt"
-	"strings"
 
-	"github.com/olekukonko/tablewriter"
+	"github.com/turbot/go-kit/helpers"
 	"github.com/turbot/steampipe/control/controlexecute"
 )
 
 type SummaryRenderer struct {
 	resultTree *controlexecute.ExecutionTree
-
-	// screen width
-	width             int
-	maxFailedControls int
-	maxTotalControls  int
+	width      int
 }
 
 func NewSummaryRenderer(resultTree *controlexecute.ExecutionTree, width int) *SummaryRenderer {
+	fmt.Println("Width:", width)
 	return &SummaryRenderer{
-		resultTree:        resultTree,
-		width:             width,
-		maxFailedControls: resultTree.Root.Summary.Status.FailedCount(),
-		maxTotalControls:  resultTree.Root.Summary.Status.TotalCount(),
+		resultTree: resultTree,
+		width:      width,
 	}
 }
 
 func (r SummaryRenderer) Render() string {
-	outbuf := bytes.NewBufferString("")
-	table := tablewriter.NewWriter(outbuf)
-	table.SetHeader([]string{})                // no headers please
-	table.SetAlignment(tablewriter.ALIGN_LEFT) // we align to the left
-	table.SetAutoWrapText(false)               // let's not wrap the text
-	table.SetBorder(true)                      // there needs to be a border to give the dialog feel
-	table.AppendBulk([][]string{
-		{r.renderStatus("Alarm", r.resultTree.Root.Summary.Status.Alarm, r.resultTree.Root.Summary.Status.TotalCount())},
-		{r.renderStatus("Ok", r.resultTree.Root.Summary.Status.Ok, r.resultTree.Root.Summary.Status.TotalCount())},
-		{r.renderStatus("Info", r.resultTree.Root.Summary.Status.Info, r.resultTree.Root.Summary.Status.TotalCount())},
-		{r.renderStatus("Skip", r.resultTree.Root.Summary.Status.Skip, r.resultTree.Root.Summary.Status.TotalCount())},
-		{r.renderStatus("Error", r.resultTree.Root.Summary.Status.Error, r.resultTree.Root.Summary.Status.TotalCount())},
-	}) // Add Bulk Data
 
-	table.Render()
+	criticalSeverityRow := NewSummarySeverityRowRenderer(r.resultTree, r.width, "critical").Render()
+	derivedWidth := helpers.PrintableLength(criticalSeverityRow) ///- 3 // why the magic number?
 
-	return outbuf.String()
-}
+	highSeverityRow := NewSummarySeverityRowRenderer(r.resultTree, derivedWidth, "high").Render()
+	fmt.Println("high:", helpers.PrintableLength(highSeverityRow))
 
-func (r SummaryRenderer) renderStatus(status string, count int, total int) string {
-	statusColorFunction := ControlColors.StatusColors[strings.ToLower(status)]
-	countColorFunction := ControlColors.ReasonColors[strings.ToLower(status)]
+	okStatusRow := NewSummaryStatusRowRenderer(r.resultTree, derivedWidth, "ok").Render()
+	skipStatusRow := NewSummaryStatusRowRenderer(r.resultTree, derivedWidth, "skip").Render()
+	infoStatusRow := NewSummaryStatusRowRenderer(r.resultTree, derivedWidth, "info").Render()
+	alarmStatusRow := NewSummaryStatusRowRenderer(r.resultTree, derivedWidth, "alarm").Render()
+	errorStatusRow := NewSummaryStatusRowRenderer(r.resultTree, derivedWidth, "error").Render()
 
-	countString := fmt.Sprintf("%d", count)
+	fmt.Println(r.shouldRenderSeverities(r.resultTree.Root))
 
 	return fmt.Sprintf(
-		"%s %s%s %s / %d",
-		statusColorFunction(status),
-		countColorFunction(padLeft(status, (r.width/2)-6)),
-		countColorFunction(padLeft(countString, (r.width/2)-6)),
-		countColorFunction(countString),
-		total,
+		`
+ %s
+ 
+ %s
+ %s
+ %s
+ %s
+ %s
+ 
+ %s
+ %s
+ 
+ %s
+		`,
+		ControlColors.GroupTitle("Summary"),
+
+		okStatusRow,
+		skipStatusRow,
+		infoStatusRow,
+		alarmStatusRow,
+		errorStatusRow,
+
+		highSeverityRow,
+		criticalSeverityRow,
+
+		NewSummaryTotalRowRenderer(r.resultTree, derivedWidth).Render(),
 	)
 }
 
-func padLeft(str string, width int) string {
-	if len(str) > width {
-		return str
+func (r SummaryRenderer) shouldRenderSeverities(group *controlexecute.ResultGroup) bool {
+	for _, subGroup := range group.Groups {
+		return r.shouldRenderSeverities(subGroup)
 	}
-
-	return strings.Repeat(".", width-len(str))
+	for _, run := range group.ControlRuns {
+		if len(run.Severity) != 0 {
+			return true
+		}
+	}
+	return false
 }
