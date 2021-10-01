5a5794bb0d60337bd1dda791f8bc90d26984f868
Binaek Sarkar <binaek@turbot.com>
2021-10-01 20:24:19 +0530

    comment

--- control/controldisplay/control.go
+++ control/controldisplay/control.go
@@ -56,7 +56,7 @@ func (r ControlRenderer) parentIndent() string {
 
 // indent before first result
 func (r ControlRenderer) preResultIndent() string {
-	// for dry run we show no results so do not add a '|' to indent
+	// when we do not have any rows, do not add a '|' to indent
 	if viper.GetBool(constants.ArgDryRun) || len(r.run.Rows) == 0 {
 		return r.parentIndent()
 	}
