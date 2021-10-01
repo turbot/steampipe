018daf9cc2e17c2cad9a6506afb664b61a0aff25
Binaek Sarkar <binaek@turbot.com>
2021-10-01 17:24:04 +0530

    rendering severity in summary

--- control/controldisplay/summary_severity_row.go
+++ control/controldisplay/summary_severity_row.go
@@ -22,47 +22,26 @@ func NewSummarySeverityRowRenderer(resultTree *controlexecute.ExecutionTree, wid
 	}
 }
 
-func (r *SummarySeverityRowRenderer) countWithSeverity(group *controlexecute.ResultGroup) (int, int) {
-	runsWithThisSeverity := 0
-	runsWithSeverityValue := 0
-
-	for _, subGroup := range group.Groups {
-		v, any := r.countWithSeverity(subGroup)
-		runsWithThisSeverity += v
-		runsWithSeverityValue += any
-	}
-
-	for _, run := range group.ControlRuns {
-		if len(run.Severity) == 0 {
-			continue
-		}
-
-		runsWithSeverityValue += 1
-		if strings.EqualFold(run.Severity, r.severity) {
-			runsWithThisSeverity += 1
-		}
-	}
-
-	return runsWithThisSeverity, runsWithSeverityValue
-}
-
 func (r *SummarySeverityRowRenderer) Render() string {
-	colorFunc := ControlColors.Severity
+	var severitySummary controlexecute.StatusSummary
+	if val, exists := r.resultTree.Root.Severity[r.severity]; exists {
+		severitySummary = val
+	}
 
+	colorFunc := ControlColors.Severity
 	severityStr := fmt.Sprintf("%s ", colorFunc(strings.ToUpper(r.severity)))
-	sevCount, withSevValueCount := r.countWithSeverity(r.resultTree.Root)
 
 	count := NewCounterRenderer(
-		sevCount,
-		withSevValueCount,
+		severitySummary.FailedCount(),
+		severitySummary.TotalCount(),
 		r.resultTree.Root.Summary.Status.FailedCount(), // not sure what this is
 		r.resultTree.Root.Summary.Status.TotalCount(),
 		false,
 	).Render()
 
 	graph := NewCounterGraphRenderer(
-		sevCount,
-		withSevValueCount,
+		severitySummary.FailedCount(),
+		severitySummary.TotalCount(),
 		r.resultTree.Root.Summary.Status.TotalCount(),
 	).Render()
 
