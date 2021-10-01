92dbae4559e2cebc03f50f6e24dba196670315b1
Binaek Sarkar <binaek@turbot.com>
2021-10-01 17:23:23 +0530

    removing trailing | for controls with 0 rows

--- control/controldisplay/control.go
+++ control/controldisplay/control.go
@@ -57,7 +57,7 @@ func (r ControlRenderer) parentIndent() string {
 // indent before first result
 func (r ControlRenderer) preResultIndent() string {
 	// for dry run we show no results so do not add a '|' to indent
-	if viper.GetBool(constants.ArgDryRun) {
+	if viper.GetBool(constants.ArgDryRun) || len(r.run.Rows) == 0 {
 		return r.parentIndent()
 	}
 	return r.parentIndent() + "| "
