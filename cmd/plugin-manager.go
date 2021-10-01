51a49af369494ed4538abe9765c7e0e18f6fe3e4
kai <kai@turbot.com>
2021-10-01 16:45:37 +0100

    working about to move to sp command

---
+++ cmd/plugin-manager.go
@@ -0,0 +1,24 @@
+package cmd
+
+//
+//import (
+//	"github.com/spf13/cobra"
+//	"github.com/turbot/steampipe/cmdconfig"
+//	"github.com/turbot/steampipe/plugin_manager"
+//)
+//
+//// pluginManagerCmd :: represents the pluginManager command
+//func pluginManagerCmd() *cobra.Command {
+//	cmd := &cobra.Command{
+//		Use:    "plugin-manager",
+//		Run:    runPluginManagerCmd,
+//		Hidden: true,
+//	}
+//	cmdconfig.OnCmd(cmd)
+//
+//	return cmd
+//}
+//
+//func runPluginManagerCmd(cmd *cobra.Command, args []string) {
+//	plugin_manager.PluginManager{}.Serve()
+//}
--- plugin_manager/main.go
+++ plugin_manager/main.go
@@ -1,15 +1,5 @@
 package main
 
-import (
-	"github.com/hashicorp/go-plugin"
-	pluginshared "github.com/turbot/steampipe/plugin_manager/grpc/shared"
-)
-
 func main() {
-	plugin.Serve(&plugin.ServeConfig{
-		HandshakeConfig: pluginshared.Handshake,
-		Plugins:         pluginshared.PluginMap,
-		//  enable gRPC serving for this plugin...
-		GRPCServer: plugin.DefaultGRPCServer,
-	})
+	PluginManager{}.Serve()
 }
