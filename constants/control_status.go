d54acb6c374fcb29755bce9307f4309d52cb9624
Binaek Sarkar <binaek@turbot.com>
2021-10-04 15:50:10 +0530

    ControlRowStatus constants

---
+++ constants/control_status.go
@@ -0,0 +1,15 @@
+package constants
+
+var ControlRowStatus = struct {
+	OK    string
+	SKIP  string
+	INFO  string
+	ALARM string
+	ERROR string
+}{
+	OK:    "ok",
+	SKIP:  "skip",
+	INFO:  "info",
+	ALARM: "alarm",
+	ERROR: "error",
+}
