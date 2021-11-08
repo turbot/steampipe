da0db0f86711b5fb925090e96d300895ed668a62
Puskar Basu <puskar@turbot.com>
2021-11-08 17:34:44 +0530

    update run.sh to set-output

--- tests/acceptance/run.sh
+++ tests/acceptance/run.sh
@@ -54,6 +54,6 @@ fi
 
 echo "Running with STEAMPIPE_INSTALL_DIR set to $STEAMPIPE_INSTALL_DIR"
 
-# bats --tap $MY_PATH/test_files/000*
+bats --tap $MY_PATH/test_files/000*
 
-echo "::set-output name=test::abcd"
+echo "::set-output name=exit_code::$(echo $?)"
