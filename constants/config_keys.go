a5685d28419cb95ccca51d136a40e08aaca12c83
kaidaguerre <kai@turbot.com>
2021-11-10 12:09:00 +0000

    Update ConnectionWatcher to set viper defaults on file events to ensure search path changes in database options are respected. Closes #1096 (#1097)

--- constants/config_keys.go
+++ constants/config_keys.go
@@ -3,9 +3,11 @@ package constants
 // viper config keys
 const (
 	ConfigKeyShowInteractiveOutput = "show-interactive-output"
-	ConfigKeyDatabaseSearchPath    = "database.search-path"
-	ConfigKeyInteractive           = "interactive"
-	ConfigKeyActiveCommand         = "cmd"
-	ConfigKeyActiveCommandArgs     = "cmd_args"
-	ConfigInteractiveVariables     = "interactive_var"
+	// ConfigKeyDatabaseSearchPath is used to store the search path set in the database config in viper
+	// the viper value will be set via via a call to getScopedKey in steampipeconfig/steampipeconfig.go
+	ConfigKeyDatabaseSearchPath = "database.search-path"
+	ConfigKeyInteractive        = "interactive"
+	ConfigKeyActiveCommand      = "cmd"
+	ConfigKeyActiveCommandArgs  = "cmd_args"
+	ConfigInteractiveVariables  = "interactive_var"
 )
