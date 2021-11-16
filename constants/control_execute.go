a82c59afa6f44b8789f9144ca8e5cb2c40d6d7cd
kaidaguerre <kai@turbot.com>
2021-11-16 21:01:40 +0000

    Update control_execute.go

--- constants/control_execute.go
+++ constants/control_execute.go
@@ -1,6 +1,5 @@
 package constants
 
-// the number of goroutines to start.
-// this is a multiplier to the max parallel input
-// essentially, twice as many go routines wait for DBSession
+// ParallelControlMultiplier is used to determine the nbumber of goroutines to start for the control run
+// this is a multiplier for the max db connections which are configred
 const ParallelControlMultiplier = 3
