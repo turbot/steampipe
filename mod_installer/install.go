bd7e9b114b9dafa6f36c01292e731eaf3b0eb511
kai <kai@turbot.com>
2021-12-07 16:40:43 +0000

    updating specific mod works

--- mod_installer/install.go
+++ mod_installer/install.go
@@ -4,16 +4,8 @@ import (
 	"github.com/spf13/viper"
 	"github.com/turbot/go-kit/helpers"
 	"github.com/turbot/steampipe/constants"
-	"github.com/turbot/steampipe/steampipeconfig/modconfig"
-	"github.com/turbot/steampipe/steampipeconfig/parse"
 	"github.com/turbot/steampipe/utils"
 )
-
-type InstallOpts struct {
-	ShouldUpdate bool
-	GetMods      []*modconfig.ModVersionConstraint
-	UpdateMods   []*modconfig.ModVersionConstraint
-}
 
 func InstallModDependencies(opts *InstallOpts) (string, error) {
 	utils.LogTime("cmd.runModInstallCmd")
@@ -24,37 +16,15 @@ func InstallModDependencies(opts *InstallOpts) (string, error) {
 		}
 	}()
 
-	workspacePath := viper.GetString(constants.ArgWorkspaceChDir)
+	opts.WorkspacePath = viper.GetString(constants.ArgWorkspaceChDir)
 
 	// install workspace dependencies
-	var mod *modconfig.Mod
-	if !parse.ModfileExists(workspacePath) {
-		if len(opts.GetMods) == 0 {
-			return "No mod file found, so there are no dependencies to install", nil
-		}
-		// so there is no mod file, but we are adding mod dependencies - create a default mod
-		mod = modconfig.CreateDefaultMod(workspacePath)
-	} else {
-		// load the modfile only
-		var err error
-		mod, err = parse.ParseModDefinition(workspacePath)
-		if err != nil {
-			return "", err
-		}
-	}
-
-	installer, err := NewModInstaller(workspacePath)
+	installer, err := NewModInstaller(opts)
 	if err != nil {
 		return "", err
 	}
 
-	// set update flag
-	installer.ShouldUpdate = opts.ShouldUpdate
-	installer.GetMods = opts.GetMods
-	installer.UpdateMods = opts.UpdateMods
-
-	err = installer.InstallModDependencies(mod)
-	if err != nil {
+	if err := installer.InstallWorkspaceDependencies(); err != nil {
 		return "", err
 	}
 
