05f8b94c8c009a55aba2eb668dbf8bd8fbdd2126
kai <kai@turbot.com>
2021-12-08 22:19:41 +0000

    some get bugs, broadly works, about to refactor commands

--- mod_installer/mod_installer_tidy.go
+++ mod_installer/mod_installer_tidy.go
@@ -1,18 +1,12 @@
 package mod_installer
 
 import (
-	"fmt"
 	"os"
 
 	"github.com/turbot/steampipe/steampipeconfig/modconfig"
 )
 
 func (i *ModInstaller) Tidy() (modconfig.VersionListMap, error) {
-	// install first if necessary
-	if len(i.installData.Lock) == 0 {
-		fmt.Println("no installation cache - run 'steampipe mod install'")
-		return nil, nil
-	}
 	unusedMods := i.installData.getUnusedMods()
 	// now delete any mod folders which are not in the lock file
 	for name, versions := range unusedMods {
@@ -23,5 +17,6 @@ func (i *ModInstaller) Tidy() (modconfig.VersionListMap, error) {
 			}
 		}
 	}
+	// TODO remove empty folders
 	return unusedMods, nil
 }
