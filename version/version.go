42112d5b06c10b909dc78533947ee06fa4f8ab46
kai <kai@turbot.com>
2021-12-10 15:00:39 +0000

    Update version to v0.11.0-dev.0

--- version/version.go
+++ version/version.go
@@ -19,12 +19,12 @@ Also https://www.digitalocean.com/community/tutorials/using-ldflags-to-set-versi
 **/
 
 // The main version number that is being run at the moment.
-var steampipeVersion = "0.10.0"
+var steampipeVersion = "0.11.0"
 
 // A pre-release marker for the version. If this is "" (empty string)
 // then it means that it is a final release. Otherwise, this is a pre-release
 // such as "dev" (in development), "beta", "rc1", etc.
-var prerelease = ""
+var prerelease = "dev.0"
 
 // Version is an instance of version.Version. This has the secondary
 // benefit of verifying during tests and init time that our version is a
