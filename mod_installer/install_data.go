6e2455b5b1f8f4f1018b72c0a74bc427ffa29956
kai <kai@turbot.com>
2021-12-15 21:44:29 +0000

    all mod commands now show tree results

--- mod_installer/install_data.go
+++ mod_installer/install_data.go
@@ -3,6 +3,8 @@ package mod_installer
 import (
 	"fmt"
 
+	"github.com/xlab/treeprint"
+
 	"github.com/Masterminds/semver"
 	"github.com/turbot/steampipe/steampipeconfig/modconfig"
 	"github.com/turbot/steampipe/steampipeconfig/version_map"
@@ -18,22 +20,23 @@ type InstallData struct {
 	allAvailable version_map.VersionListMap
 
 	// list of dependencies installed by recent install operation
-	RecentlyInstalled version_map.ResolvedVersionListMap
-	// list of dependencies which were already installed
-	AlreadyInstalled version_map.ResolvedVersionListMap
+	Installed version_map.DependencyVersionMap
 	// list of dependencies which have been updated
-	Updated version_map.ResolvedVersionListMap
+	Updated version_map.DependencyVersionMap
 	// list of dependencies which have been uninstalled
-	Uninstalled version_map.ResolvedVersionListMap
+	Uninstalled  version_map.DependencyVersionMap
+	WorkspaceMod *modconfig.Mod
 }
 
-func NewInstallData(workspaceLock *version_map.WorkspaceLock) *InstallData {
+func NewInstallData(workspaceLock *version_map.WorkspaceLock, workspaceMod *modconfig.Mod) *InstallData {
 	return &InstallData{
-		Lock:              workspaceLock,
-		NewLock:           version_map.EmptyWorkspaceLock(workspaceLock),
-		allAvailable:      make(version_map.VersionListMap),
-		RecentlyInstalled: make(version_map.ResolvedVersionListMap),
-		AlreadyInstalled:  make(version_map.ResolvedVersionListMap),
+		Lock:         workspaceLock,
+		WorkspaceMod: workspaceMod,
+		NewLock:      version_map.EmptyWorkspaceLock(workspaceLock),
+		allAvailable: make(version_map.VersionListMap),
+		Installed:    make(version_map.DependencyVersionMap),
+		Updated:      make(version_map.DependencyVersionMap),
+		Uninstalled:  make(version_map.DependencyVersionMap),
 	}
 }
 
@@ -59,30 +62,18 @@ func (d *InstallData) GetAvailableUpdates() (version_map.DependencyVersionMap, e
 
 // onModInstalled is called when a dependency is satisfied by installing a mod version
 func (d *InstallData) onModInstalled(dependency *ResolvedModRef, parent *modconfig.Mod) {
+	parentPath := parent.GetModDependencyPath()
 	// get the constraint from the parent (it must be there)
 	modVersion := parent.Require.GetModDependency(dependency.Name)
 	// update lock
-	d.NewLock.InstallCache.Add(dependency.Name, dependency.Version, modVersion.Constraint.Original, parent.GetModDependencyPath())
-	// update list items installed by this installer
-	d.RecentlyInstalled.Add(dependency.Name, &version_map.ResolvedVersionConstraint{
-		Name:       dependency.Name,
-		Version:    dependency.Version,
-		Constraint: dependency.Constraint.Original,
-	})
+	d.NewLock.InstallCache.Add(dependency.Name, dependency.Version, modVersion.Constraint.Original, parentPath)
 }
 
 // addExisting is called when a dependency is satisfied by a mod which is already installed
 func (d *InstallData) addExisting(name string, version *semver.Version, constraint *version.Constraints, parent *modconfig.Mod) {
 	// update lock
-	d.NewLock.InstallCache.Add(name, version, constraint.Original, parent.GetModDependencyPath())
-
-	modVersion := parent.Require.GetModDependency(name)
-	// update list of already installed items
-	d.AlreadyInstalled.Add(name, &version_map.ResolvedVersionConstraint{
-		Name:       name,
-		Version:    version,
-		Constraint: modVersion.Constraint.Original,
-	})
+	parentPath := parent.GetModDependencyPath()
+	d.NewLock.InstallCache.Add(name, version, constraint.Original, parentPath)
 }
 
 // retrieve all available mod versions from our cache, or from Git if not yet cached
@@ -106,19 +97,20 @@ func (d *InstallData) getAvailableModVersions(modName string, includePrerelease 
 
 // update the lock with the NewLock and dtermine if any mods have been uninstalled
 func (d *InstallData) onInstallComplete() {
-	d.Uninstalled = d.getUninstalled(d.Lock, d.NewLock)
+	d.Installed = d.NewLock.InstallCache.GetMissingFromOther(d.Lock.InstallCache)
+	d.Uninstalled = d.Lock.InstallCache.GetMissingFromOther(d.NewLock.InstallCache)
+	d.Updated = d.Lock.InstallCache.GetUpdatedInOther(d.NewLock.InstallCache)
 	d.Lock = d.NewLock
 }
 
-// determine which dependencies have been uninstalled by comparing old and new lock data
-func (d *InstallData) getUninstalled(oldLock *version_map.WorkspaceLock, newLock *version_map.WorkspaceLock) version_map.ResolvedVersionListMap {
-	res := make(version_map.ResolvedVersionListMap)
-	oldFlat := oldLock.InstallCache.FlatMap()
-	newFlat := newLock.InstallCache.FlatMap()
-	for fullName, oldDep := range oldFlat {
-		if _, ok := newFlat[fullName]; !ok {
-			res.Add(oldDep.Name, oldDep)
-		}
-	}
-	return res
+func (d *InstallData) GetUpdatedTree() treeprint.Tree {
+	return d.Updated.GetDependencyTree(d.WorkspaceMod.GetModDependencyPath())
+}
+
+func (d *InstallData) GetInstalledTree() treeprint.Tree {
+	return d.Installed.GetDependencyTree(d.WorkspaceMod.GetModDependencyPath())
+}
+
+func (d *InstallData) GetUninstalledTree() treeprint.Tree {
+	return d.Uninstalled.GetDependencyTree(d.WorkspaceMod.GetModDependencyPath())
 }
