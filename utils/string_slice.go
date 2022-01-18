4a2639dc64359f26af0402c8edff06a8443906e7
Binaek Sarkar <binaek@turbot.com>
2022-01-18 15:16:34 +0530

    util fn

---
+++ utils/string_slice.go
@@ -0,0 +1,13 @@
+package utils
+
+func StringSliceDistinct(slice []string) []string {
+	var res []string
+	occurenceMap := make(map[string]struct{})
+	for _, item := range slice {
+		occurenceMap[item] = struct{}{}
+	}
+	for item := range occurenceMap {
+		res = append(res, item)
+	}
+	return res
+}
