3588b3ace00fdb14e7c5f81e0d4a66d9d800c49d
Binaek Sarkar <binaek@turbot.com>
2022-01-18 15:16:23 +0530

    remove csv as a defined formatter

--- constants/output_format.go
+++ constants/output_format.go
@@ -8,13 +8,8 @@ const (
 	OutputFormatLine  = "line"
 
 	// check output format
-	CheckOutputFormatNone     = "none"
-	CheckOutputFormatText     = "text"
-	CheckOutputFormatBrief    = "brief"
-	CheckOutputFormatCSV      = "csv"
-	CheckOutputFormatJSON     = "json"
-	CheckOutputFormatHTML     = "html"
-	CheckOutputFormatMarkdown = "md"
-	CheckOutputFormatNUnit3   = "nunit3"
-	CheckOutputFormatAsffJson = "json-asff"
+	CheckOutputFormatNone  = "none"
+	CheckOutputFormatText  = "text"
+	CheckOutputFormatBrief = "brief"
+	CheckOutputFormatJSON  = "json"
 )
