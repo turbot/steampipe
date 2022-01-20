027caa58acddd39428b5d6c4b14ed61e251c941f
kai <kai@turbot.com>
2022-01-20 15:05:14 +0000

    fix rebase

--- cmd/report.go
+++ cmd/report.go
@@ -6,10 +6,27 @@ import (
 	"github.com/spf13/cobra"
 	"github.com/turbot/go-kit/helpers"
 	"github.com/turbot/steampipe-plugin-sdk/logging"
+	"github.com/turbot/steampipe/cmdconfig"
+	"github.com/turbot/steampipe/constants"
 	"github.com/turbot/steampipe/contexthelpers"
 	"github.com/turbot/steampipe/report/reportserver"
 	"github.com/turbot/steampipe/utils"
 )
+
+func reportCmd() *cobra.Command {
+	cmd := &cobra.Command{
+		Use:              "report [report]",
+		TraverseChildren: true,
+		Args:             cobra.ArbitraryArgs,
+		Run:              runReportCmd,
+		Short:            "Run a report",
+		Long:             `Run a report...TODO better description!`,
+	}
+
+	cmdconfig.OnCmd(cmd).
+		AddBoolFlag(constants.ArgHelp, "h", false, "Help for report")
+	return cmd
+}
 
 func runReportCmd(cmd *cobra.Command, args []string) {
 	ctx := cmd.Context()
