9c0c0ebf436e10211e2a04583f207bf8f700dfb7
Puskar Basu <puskar@turbot.com>
2022-01-20 20:44:20 +0530

    more documenting

--- control/controlexecute/result_group.go
+++ control/controlexecute/result_group.go
@@ -22,10 +22,11 @@ const RootResultGroupName = "root_result_group"
 // ResultGroup is a struct representing a grouping of control results
 // It may correspond to a Benchmark, or some other arbitrary grouping
 type ResultGroup struct {
-	GroupId     string                   `json:"group_id" csv:"group_id"`
-	Title       string                   `json:"title" csv:"title"`
-	Description string                   `json:"description" csv:"description"`
-	Tags        map[string]string        `json:"tags"`
+	GroupId     string            `json:"group_id" csv:"group_id"`
+	Title       string            `json:"title" csv:"title"`
+	Description string            `json:"description" csv:"description"`
+	Tags        map[string]string `json:"tags"`
+	// the overall summary of the group
 	Summary     *GroupSummary            `json:"summary"`
 	Groups      []*ResultGroup           `json:"groups"`
 	ControlRuns []*ControlRun            `json:"controls"`
