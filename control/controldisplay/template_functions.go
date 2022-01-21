7eae58800b72c30960cc38c9d6dd7ae05cbb4229
Binaek Sarkar <binaek@turbot.com>
2022-01-21 18:05:34 +0530

    template functions

--- control/controldisplay/template_functions.go
+++ control/controldisplay/template_functions.go
@@ -4,16 +4,60 @@ import (
 	"bytes"
 	"encoding/csv"
 	"fmt"
-	"reflect"
+	"strings"
+	"sync"
+	"text/template"
 	"time"
+
+	"github.com/Masterminds/sprig/v3"
+)
+
+// templateFuncs merges desired functions from sprig with custom functions that we
+// define in steampipe
+func templateFuncs() template.FuncMap {
+	useFromSprigMap := []string{"upper", "toJson", "quote", "dict", "add", "now"}
+
+	var funcs template.FuncMap = template.FuncMap{}
+	sprigMap := sprig.TxtFuncMap()
+	for _, use := range useFromSprigMap {
+		f, found := sprigMap[use]
+		if !found {
+			// guarantee that when a function is expected to be present
+			// it does not slip through any crack
+			panic(fmt.Sprintf("%s not found", use))
+		}
+		if found {
+			funcs[use] = f
+		}
+	}
+	for k, v := range formatterTemplateFuncMap {
+		funcs[k] = v
+	}
+
+	return funcs
+}
+
+// custom steampipe functions - ones we couldn't find in sprig
+var formatterTemplateFuncMap template.FuncMap = template.FuncMap{
+	"durationInSeconds": durationInSeconds,
+	"toCsvCell":         toCsvCell,
+}
+
+var (
+	csvWriterBuffer = bytes.NewBufferString("")
+	csvWriter       = csv.NewWriter(csvWriterBuffer)
+	csvBufferLock   = sync.Mutex{}
 )
 
 // toCsvCell escapes a value for csv
-func toCsvCell(v reflect.Value) string {
-	buffer := bytes.NewBufferString("")
-	csvWriter := csv.NewWriter(buffer)
-	csvWriter.Write([]string{fmt.Sprintf("\"%v\"", v)})
-	return buffer.String()
+func toCsvCell(v interface{}) string {
+	csvBufferLock.Lock()
+	defer csvBufferLock.Unlock()
+
+	csvWriterBuffer.Reset()
+	csvWriter.Write([]string{fmt.Sprintf("%v", v)})
+	csvWriter.Flush()
+	return strings.TrimSpace(csvWriterBuffer.String())
 }
 
 // durationInSeconds returns the passed in duration as seconds
