4ba4bb43c3119ebc36a50f65c01bfd52c117f6ed
kai <kai@turbot.com>
2022-01-26 22:06:44 +0000

    Add reportassets.Ensure

--- version/version.go
+++ version/version.go
@@ -19,12 +19,12 @@ Also https://www.digitalocean.com/community/tutorials/using-ldflags-to-set-versi
 **/
 
 // The main version number that is being run at the moment.
-var steampipeVersion = "0.12.1"
+var steampipeVersion = "0.13.0"
 
 // A pre-release marker for the version. If this is "" (empty string)
 // then it means that it is a final release. Otherwise, this is a pre-release
 // such as "dev" (in development), "beta", "rc1", etc.
-var prerelease = ""
+var prerelease = "alpha.0"
 
 // SteampipeVersion is an instance of semver.Version. This has the secondary
 // benefit of verifying during tests and init time that our version is a
