489637830378cc3b3df85cf9870b6fea486f1146
Mike Burgess <mike@turbot.com>
2022-01-26 20:58:40 +0000

    Update makefile to write out JSON versions file

---
+++ ui/report/scripts/version.go
@@ -0,0 +1,21 @@
+package main
+
+import (
+	"encoding/json"
+	"github.com/turbot/steampipe/version"
+	"io/ioutil"
+)
+
+type packageVersion struct {
+	Version string `json:"version"`
+}
+
+func main() {
+	spVersionString := version.SteampipeVersion.String()
+	spVersion := packageVersion{Version: spVersionString}
+	versionsFile, _ := json.MarshalIndent(spVersion, "", " ")
+	err := ioutil.WriteFile("build/versions.json", versionsFile, 0644)
+	if err != nil {
+		panic(err)
+	}
+}
