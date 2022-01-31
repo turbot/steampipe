aad50fcf922d600c06421c9445c0d3eaf1cbfc51
kai <kai@turbot.com>
2022-01-31 10:52:21 +0000

    disable assest Ensure() for now

--- report/reportassets/ensure.go
+++ report/reportassets/ensure.go
@@ -5,38 +5,36 @@ import (
 	"encoding/json"
 	"log"
 	"os"
-	"path/filepath"
 
 	"github.com/turbot/go-kit/helpers"
 	"github.com/turbot/steampipe/filepaths"
-	"github.com/turbot/steampipe/utils"
-	"github.com/turbot/steampipe/version"
 )
 
-//go:embed assets.zip
-var reportAssets []byte
-
-const assetsZipFileName = "assets.zip"
+//
+////go:embed assets.zip
+//var reportAssets []byte
+//
+//const assetsZipFileName = "assets.zip"
 
 func Ensure() error {
-	// load report assets versions.json
-	versionFile, err := LoadReportAssetVersionFile()
-	if err != nil {
-		return err
-	}
-	if versionFile.Version == version.SteampipeVersion.String() {
-		return nil
-	}
-
-	reportAssetsPath := filepaths.ReportAssetsPath()
-
-	zipPath := filepath.Join(os.TempDir(), assetsZipFileName)
-	err = os.WriteFile(zipPath, reportAssets, 0744)
-	defer os.RemoveAll(zipPath)
-
-	if _, err := utils.Unzip(zipPath, reportAssetsPath); err != nil {
-		return err
-	}
+	//// load report assets versions.json
+	//versionFile, err := LoadReportAssetVersionFile()
+	//if err != nil {
+	//	return err
+	//}
+	//if versionFile.Version == version.SteampipeVersion.String() {
+	//	return nil
+	//}
+	//
+	//reportAssetsPath := filepaths.ReportAssetsPath()
+	//
+	//zipPath := filepath.Join(os.TempDir(), assetsZipFileName)
+	//err = os.WriteFile(zipPath, reportAssets, 0744)
+	//defer os.RemoveAll(zipPath)
+	//
+	//if _, err := utils.Unzip(zipPath, reportAssetsPath); err != nil {
+	//	return err
+	//}
 
 	return nil
 }
