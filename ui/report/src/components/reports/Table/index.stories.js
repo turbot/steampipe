ee005c557521d1ff9980efaf7e7d042b1092de55
Mike Burgess <mike@turbot.com>
2022-02-03 15:16:42 +0000

    Add col-wrap support

--- ui/report/src/components/reports/Table/index.stories.js
+++ ui/report/src/components/reports/Table/index.stories.js
@@ -64,3 +64,80 @@ Nulls.args = {
     ],
   },
 };
+
+export const HideColumn = Template.bind({});
+HideColumn.args = {
+  data: {
+    columns: [
+      { name: "Region", data_type_name: "TEXT" },
+      { name: "Resource Count", data_type_name: "INT8" },
+      { name: "Encrypted", data_type_name: "BOOL" },
+      { name: "Info", data_type_name: "JSONB" },
+    ],
+    rows: [
+      ["us-east-1", 246, true, null],
+      ["us-east-2", 146, false, { foo: "bar" }],
+      ["us-west-1", 57, true, { bar: "foo" }],
+      ["eu-west-1", 290, false, { foobar: "barfoo" }],
+      ["eu-west-2", 198, true, null],
+    ],
+  },
+  properties: {
+    columns: {
+      Info: {
+        display: "none",
+      },
+    },
+  },
+};
+
+export const WrapColumn = Template.bind({});
+WrapColumn.args = {
+  data: {
+    columns: [
+      { name: "Region", data_type_name: "TEXT" },
+      { name: "Resource Count", data_type_name: "INT8" },
+      { name: "Encrypted", data_type_name: "BOOL" },
+      { name: "Long", data_type_name: "TEXT" },
+    ],
+    rows: [
+      [
+        "us-east-1",
+        246,
+        true,
+        "I am a really, really, really, really, really, really, really, really, really, really long value that I wish to wrap",
+      ],
+      [
+        "us-east-2",
+        146,
+        false,
+        "I am a really, really, really, really, really, really, really, really, really, really long value that I wish to wrap",
+      ],
+      [
+        "us-west-1",
+        57,
+        true,
+        "I am a really, really, really, really, really, really, really, really, really, really long value that I wish to wrap",
+      ],
+      [
+        "eu-west-1",
+        290,
+        false,
+        "I am a really, really, really, really, really, really, really, really, really, really long value that I wish to wrap",
+      ],
+      [
+        "eu-west-2",
+        198,
+        true,
+        "I am a really, really, really, really, really, really, really, really, really, really long value that I wish to wrap",
+      ],
+    ],
+  },
+  properties: {
+    columns: {
+      Long: {
+        wrap: "always",
+      },
+    },
+  },
+};
