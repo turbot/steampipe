2e60eeebc7089be8e8b218da0717f420cca40550
Mike Burgess <mike@turbot.com>
2022-02-04 12:34:44 +0000

    Add error boundary

--- ui/report/src/components/Modal/ErrorModal.js
+++ ui/report/src/components/Modal/ErrorModal.js
@@ -1,11 +1,12 @@
+import ErrorMessage from "../ErrorMessage";
 import Modal from "./index";
-import { ErrorIcon } from "../../../constants/icons";
+import { ErrorIcon } from "../../constants/icons";
 
 const ErrorModal = ({ error, title }) => {
   return (
     <Modal
       icon={<ErrorIcon className="h-6 w-6 text-red-600" aria-hidden="true" />}
-      message={error}
+      message={<ErrorMessage error={error} />}
       title={title}
     />
   );
--- ui/report/src/components/reports/Error/index.tsx
+++ ui/report/src/components/reports/Error/index.tsx
@@ -1,31 +1,16 @@
-const getErrorMessage = (error: any, fallbackMessage: string) => {
-  if (typeof error === "string") {
-    return error;
-  }
-  if (error.message) {
-    return error.message;
-  }
-  if (error.Message) {
-    return error.Message;
-  }
-  return fallbackMessage;
-};
+import ErrorMessage from "../../ErrorMessage";
 
 interface ErrorProps {
   error?: any;
-  fallbackMessage?: string;
 }
 
-const Error = ({
-  error,
-  fallbackMessage = "An unknown error occurred.",
-}: ErrorProps) => {
+const Error = ({ error }: ErrorProps) => {
   if (!error) {
     return null;
   }
   return (
     <div className="flex w-full h-full p-2 break-all bg-red-50 border-red-700 border text-red-700 justify-center items-center shadow rounded-md">
-      {getErrorMessage(error, fallbackMessage)}
+      <ErrorMessage error={error} />
     </div>
   );
 };
