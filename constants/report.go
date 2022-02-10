17bc2e8c1830ecf79057542aed9e82995f26ad7e
kai <kai@turbot.com>
2022-02-10 13:59:58 +0000

    Update version to 0.13.0-alpha.6

--- constants/report.go
+++ constants/report.go
@@ -2,6 +2,6 @@ package constants
 
 const (
 	ReportServerDefaultPort = 9194
-	AssetsVersion           = "0.13.0-alpha.5"
+	AssetsVersion           = "0.13.0-alpha.6"
 	AssetsImageRef          = "us-docker.pkg.dev/steampipe/steampipe/assets:" + AssetsVersion
 )
