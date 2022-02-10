faa414b4ef30e42ef2560a001ddab62f0207458c
Mike Burgess <mike@turbot.com>
2022-02-10 12:29:27 +0000

    Fix report UI chart tooltips being cut off when they exceed the boundaries of the chart container. Fixes #1419.

--- ui/report/src/components/reports/charts/Chart/index.tsx
+++ ui/report/src/components/reports/charts/Chart/index.tsx
@@ -58,7 +58,15 @@ const getCommonBaseOptions = () => ({
       fontSize: 11,
     },
   },
+  textStyle: {
+    fontFamily:
+      'ui-sans-serif, system-ui, -apple-system, BlinkMacSystemFont, "Segoe UI", Roboto, "Helvetica Neue", Arial, "Noto Sans", sans-serif, "Apple Color Emoji", "Segoe UI Emoji", "Segoe UI Symbol", "Noto Color Emoji"',
+  },
   tooltip: {
+    appendToBody: true,
+    textStyle: {
+      fontSize: 11,
+    },
     trigger: "item",
   },
 });
