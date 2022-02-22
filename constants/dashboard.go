edf11ad2e19229ff79d39265e42f5ffa09512c42
kai <kai@turbot.com>
2022-02-22 15:53:00 +0000

    Update version to 0.13.0-alpha.9

--- constants/dashboard.go
+++ constants/dashboard.go
@@ -2,6 +2,6 @@ package constants
 
 const (
 	DashboardServerDefaultPort = 9194
-	DashboardAssetsVersion     = "0.13.0-alpha.8"
+	DashboardAssetsVersion     = "0.13.0-alpha.9"
 	DashboardAssetsImageRef    = "us-docker.pkg.dev/steampipe/steampipe/assets:" + DashboardAssetsVersion
 )
