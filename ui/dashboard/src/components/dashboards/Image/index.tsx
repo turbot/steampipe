75358f71c0133d247b60f7da21dacc94ab0a0c5c
Mike Burgess <mike@turbot.com>
2022-02-23 20:22:14 +0000

    Dashboard image component should support getting src and alt from a query #1429

--- ui/dashboard/src/components/dashboards/Image/index.tsx
+++ ui/dashboard/src/components/dashboards/Image/index.tsx
@@ -2,8 +2,15 @@ import Img from "react-cool-img";
 import Table from "../Table";
 import { BasePrimitiveProps, ExecutablePrimitiveProps } from "../common";
 import { get } from "lodash";
+import { getColumnIndex } from "../../../utils/data";
+import { useEffect, useState } from "react";
 
 type ImageType = "image" | "table" | null;
+
+interface ImageState {
+  src: string | null;
+  alt: string | null;
+}
 
 export type ImageProps = BasePrimitiveProps &
   ExecutablePrimitiveProps & {
@@ -14,12 +21,55 @@ export type ImageProps = BasePrimitiveProps &
     };
   };
 
+const useImageState = ({ data, properties }: ImageProps) => {
+  const [calculatedProperties, setCalculatedProperties] = useState<ImageState>({
+    src: properties.src || null,
+    alt: properties.alt || null,
+  });
+
+  useEffect(() => {
+    if (!data) {
+      return;
+    }
+
+    if (
+      !data.columns ||
+      !data.rows ||
+      data.columns.length === 0 ||
+      data.rows.length === 0
+    ) {
+      setCalculatedProperties({
+        src: null,
+        alt: null,
+      });
+      return;
+    }
+
+    const srcColIndex = getColumnIndex(data.columns, "src");
+    const src = srcColIndex >= 0 ? get(data, `rows[0][${srcColIndex}]`) : null;
+    const altColIndex = getColumnIndex(data.columns, "alt");
+    const alt = altColIndex >= 0 ? get(data, `rows[0][${altColIndex}]`) : null;
+
+    setCalculatedProperties({
+      src,
+      alt,
+    });
+  }, [data, properties]);
+
+  return calculatedProperties;
+};
+
 const Image = (props: ImageProps) => {
+  const state = useImageState(props);
+  return <Img src={state.src} alt={state.alt} />;
+};
+
+const ImageWrapper = (props: ImageProps) => {
   if (get(props, "properties.type") === "table") {
     // @ts-ignore
     return <Table {...props} />;
   }
-  return <Img src={props.properties.src} alt={props.properties.alt} />;
+  return <Image {...props} />;
 };
 
-export default Image;
+export default ImageWrapper;
