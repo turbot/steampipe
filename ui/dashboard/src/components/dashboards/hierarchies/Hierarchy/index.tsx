92e65efafdd5da52baacc97a21c3aab94992354f
Mike Burgess <mike@turbot.com>
2022-02-25 17:02:36 +0000

    Improve sankey hierarchy tooltip to remove value #1429.

--- ui/dashboard/src/components/dashboards/hierarchies/Hierarchy/index.tsx
+++ ui/dashboard/src/components/dashboards/hierarchies/Hierarchy/index.tsx
@@ -69,6 +69,9 @@ const getSeriesForHierarchyType = (
           },
           data: sankeyData,
           links,
+          tooltip: {
+            formatter: "{b}",
+          },
         });
         break;
       }
