1ec9e318ea70f3cd7fda19eb793123564c3bda85
Binaek Sarkar <binaek@turbot.com>
2022-03-17 16:42:44 +0530

    tests

--- ociinstaller/plugin_test.go
+++ ociinstaller/plugin_test.go
@@ -7,23 +7,43 @@ import (
 
 type transformTest struct {
 	ref                                  *SteampipeImageRef
-	pluginLineContent                    string
-	expectedTransformedPluginLineContent string
+	pluginLineContent                    []byte
+	expectedTransformedPluginLineContent []byte
 }
 
 var transformTests = map[string]transformTest{
-	"test1": {
+	"empty": {
 		ref:                                  NewSteampipeImageRef("chaos"),
-		pluginLineContent:                    `plugin = "chaos"`,
-		expectedTransformedPluginLineContent: `plugin = "chaos@latest"`,
+		pluginLineContent:                    []byte(`plugin = "chaos"`),
+		expectedTransformedPluginLineContent: []byte(`plugin = "chaos"`),
+	},
+	"latest": {
+		ref:                                  NewSteampipeImageRef("chaos@latest"),
+		pluginLineContent:                    []byte(`plugin = "chaos"`),
+		expectedTransformedPluginLineContent: []byte(`plugin = "chaos"`),
+	},
+	"0": {
+		ref:                                  NewSteampipeImageRef("chaos@0"),
+		pluginLineContent:                    []byte(`plugin = "chaos"`),
+		expectedTransformedPluginLineContent: []byte(`plugin = "chaos@0"`),
+	},
+	"0.2": {
+		ref:                                  NewSteampipeImageRef("chaos@0.2"),
+		pluginLineContent:                    []byte(`plugin = "chaos"`),
+		expectedTransformedPluginLineContent: []byte(`plugin = "chaos@0.2"`),
+	},
+	"0.2.0": {
+		ref:                                  NewSteampipeImageRef("chaos@0.2.0"),
+		pluginLineContent:                    []byte(`plugin = "chaos"`),
+		expectedTransformedPluginLineContent: []byte(`plugin = "chaos@0.2.0"`),
 	},
 }
 
 func TestAddPluginName(t *testing.T) {
 	for name, test := range transformTests {
-		sourcebytes := bytes.NewBufferString(test.pluginLineContent).Bytes()
-		transformed := addPluginStreamToConfig(sourcebytes, test.ref)
-		expectedBytes := bytes.NewBufferString(test.expectedTransformedPluginLineContent).Bytes()
+		sourcebytes := test.pluginLineContent
+		expectedBytes := test.expectedTransformedPluginLineContent
+		transformed := bytes.TrimSpace(addPluginStreamToConfig(sourcebytes, test.ref, "latest"))
 
 		if !bytes.Equal(transformed, expectedBytes) {
 			t.Fatalf("%s failed - expected(%s) - got(%s)", name, test.expectedTransformedPluginLineContent, transformed)
