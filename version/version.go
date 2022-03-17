3aa2c18f7accd4b85c242c7efae6907d4eeaa070
Binaek Sarkar <binaek@turbot.com>
2022-03-17 22:33:43 +0530

    update version

--- version/version.go
+++ version/version.go
@@ -24,7 +24,7 @@ var steampipeVersion = "0.13.1"
 // A pre-release marker for the version. If this is "" (empty string)
 // then it means that it is a final release. Otherwise, this is a pre-release
 // such as "dev" (in development), "beta", "rc1", etc.
-var prerelease = "rc.1"
+var prerelease = ""
 
 // SteampipeVersion is an instance of semver.Version. This has the secondary
 // benefit of verifying during tests and init time that our version is a
