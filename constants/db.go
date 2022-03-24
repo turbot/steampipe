9a12bae0564d0cd439859ec2f560b7299c322bbd
Binaek Sarkar <binaek@turbot.com>
2022-03-24 16:25:11 +0530

    fdw alpha

--- constants/db.go
+++ constants/db.go
@@ -28,7 +28,7 @@ const (
 // constants for installing db and fdw images
 const (
 	DatabaseVersion = "14.2.0"
-	FdwVersion      = "0.4.0"
+	FdwVersion      = "1.0.0-alpha.1"
 
 	// PostgresImageRef is the OCI Image ref for the databse binaries
 	// The 12.1.0 image uses the older jar format 12.1.0-v2 is the same version of postgres,
