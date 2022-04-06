581178490a2ac0f42b0d830240213f644f166cff
Mike Burgess <mike@turbot.com>
2022-04-06 23:41:24 +0100

    WIP - improvements for benchmark layout

--- ui/dashboard/src/components/dashboards/check/Benchmark/index.tsx
+++ ui/dashboard/src/components/dashboards/check/Benchmark/index.tsx
@@ -4,7 +4,13 @@ import { CheckProps, CheckRunState } from "../common";
 import { classNames } from "../../../../utils/styles";
 import { default as BenchmarkType } from "../common/Benchmark";
 import { default as ControlType } from "../common/Control";
-import { ErrorIcon, OKIcon, UnknownIcon } from "../../../../constants/icons";
+import {
+  CollapseBenchmarkIcon,
+  ErrorIcon,
+  ExpandBenchmarkIcon,
+  OKIcon,
+  UnknownIcon,
+} from "../../../../constants/icons";
 import { useState } from "react";
 
 const getPadding = (depth) => {
@@ -38,7 +44,7 @@ interface BenchmarkNodeProps {
 
 const CheckSummary = ({ summary }) => {
   return (
-    <div className="col-span-8 space-x-4 tabular-nums">
+    <div className="flex tabular-nums space-x-4 justify-end group-hover:bg-black-scale-1 px-1">
       <pre
         className={classNames(
           "inline",
@@ -77,30 +83,54 @@ interface CheckNodeStatusProps {
   run_state: CheckRunState;
 }
 
+const BenchmarkIcon = ({ expanded, run_state }) => (
+  <div className="relative flex items-center">
+    {!expanded && (
+      <ExpandBenchmarkIcon className="flex-shrink-0 w-5 h-5 text-foreground-lighter" />
+    )}
+    {expanded && (
+      <CollapseBenchmarkIcon className="flex-shrink-0 w-5 h-5 text-foreground-lighter" />
+    )}
+    {(run_state === "ready" || run_state === "started") && (
+      <LoadingIndicator className="absolute flex-shrink-0 w-5 h-5 top-px left-0 text-foreground-lightest" />
+    )}
+  </div>
+);
+
 const CheckNodeStatus = ({ run_state }: CheckNodeStatusProps) => {
   return (
-    <span className="mr-2 text-foreground-lighter">
+    <>
       {(run_state === "ready" || run_state === "started") && (
-        <LoadingIndicator className="inline w-5 h-5" />
+        <LoadingIndicator className="flex-shrink-0 w-5 h-5 text-foreground-lighter" />
       )}
-      {run_state === "complete" && <OKIcon className="inline w-5 h-5" />}
-      {run_state === "error" && <ErrorIcon className="inline w-5 h-5" />}
-      {run_state === "unknown" && <UnknownIcon className="inline w-5 h-5" />}
-    </span>
+      {run_state === "complete" && (
+        <OKIcon className="flex-shrink-0 w-5 h-5 text-foreground-lighter" />
+      )}
+      {run_state === "error" && (
+        <ErrorIcon className="flex-shrink-0 w-5 h-5 text-foreground-lighter" />
+      )}
+      {run_state === "unknown" && (
+        <UnknownIcon className="flex-shrink-0 w-5 h-5 text-foreground-lighter" />
+      )}
+    </>
   );
 };
 
 const ControlNode = ({ depth = 0, control }: ControlNodeProps) => {
   return (
-    <>
-      <div className={classNames("col-span-4 font-medium truncate")}>
+    <div className="group flex rounded-sm">
+      <div
+        className={classNames(
+          "px-1 space-x-2 flex flex-grow group-hover:bg-black-scale-1"
+        )}
+      >
         <CheckNodeStatus run_state={control.run_state} />
-        <span className={getPadding(depth)}>
+        <p className={classNames("", getPadding(depth))}>
           {control.title || control.name}
-        </span>
+        </p>
       </div>
       <CheckSummary summary={control.summary} />
-    </>
+    </div>
   );
 };
 
@@ -108,32 +138,48 @@ const BenchmarkNode = ({ depth = 0, benchmark }: BenchmarkNodeProps) => {
   const [expanded, setExpanded] = useState(depth < 1);
   return (
     <>
-      <div
-        className={classNames(
-          "col-span-4 font-medium truncate",
-          benchmark.benchmarks || benchmark.controls ? "cursor-pointer " : null
-        )}
-        onClick={
-          benchmark.benchmarks || benchmark.controls
-            ? () => setExpanded(!expanded)
-            : undefined
-        }
-      >
-        <CheckNodeStatus run_state={benchmark.run_state} />
-        <span className={getPadding(depth)}>
-          {benchmark.title || benchmark.name}
-        </span>
+      <div className="group flex rounded-sm">
+        <div
+          className={classNames(
+            "px-1 space-x-2 flex flex-grow group-hover:bg-black-scale-1",
+            benchmark.benchmarks || benchmark.controls
+              ? "cursor-pointer "
+              : null
+          )}
+          onClick={
+            benchmark.benchmarks || benchmark.controls
+              ? () => setExpanded(!expanded)
+              : undefined
+          }
+        >
+          <BenchmarkIcon expanded={expanded} run_state={benchmark.run_state} />
+          {/*<div className="relative flex items-center">*/}
+          {/*  {!expanded && (*/}
+          {/*    <ExpandBenchmarkIcon className="mr-1 inline w-5 h-5 text-foreground-lighter" />*/}
+          {/*  )}*/}
+          {/*  {expanded && (*/}
+          {/*    <CollapseBenchmarkIcon className="mr-1 inline w-5 h-5 text-foreground-lighter" />*/}
+          {/*  )}*/}
+          {/*  {(benchmark.run_state === "ready" ||*/}
+          {/*    benchmark.run_state === "started") && (*/}
+          {/*    <LoadingIndicator className="absolute mr-1 w-5 h-5 top-px left-0 text-foreground-lightest" />*/}
+          {/*  )}*/}
+          {/*<LoadingIndicator className="absolute w-5 h-5 top-px left-0 text-foreground-lightest" />*/}
+          {/*<CheckNodeStatus run_state={benchmark.run_state} />*/}
+          <p className={classNames("", getPadding(depth))}>
+            {benchmark.title || benchmark.name}
+          </p>
+          {/*</div>*/}
+        </div>
+        <CheckSummary summary={benchmark.summary} />
       </div>
-      <CheckSummary summary={benchmark.summary} />
       {expanded && (
         <>
           {benchmark.benchmarks.map((b) => (
             <BenchmarkNode key={b.name} depth={depth + 1} benchmark={b} />
           ))}
           {benchmark.controls.map((c) => (
-            <div className="col-span-12 grid grid-cols-12 gap-x-4">
-              <ControlNode key={c.name} depth={depth + 1} control={c} />
-            </div>
+            <ControlNode key={c.name} depth={depth + 1} control={c} />
           ))}
         </>
       )}
@@ -156,9 +202,9 @@ const Benchmark = (props: CheckProps) => {
   );
 
   return (
-    <div className="p-4 grid grid-cols-12 gap-x-4 gap-y-1">
-      <div className="col-span-4"></div>
-      <div className="col-span-8 text-foreground-light space-x-4 tabular-nums">
+    <div className="p-4">
+      <div className="flex flex-grow"></div>
+      <div className="flex text-foreground-light space-x-4 tabular-nums justify-end px-1">
         <pre className="inline">{`${padStart("OK", 5)}`}</pre>
         <pre className="inline">{`${padStart("Skip", 5)}`}</pre>
         <pre className="inline">{`${padStart("Info", 5)}`}</pre>
