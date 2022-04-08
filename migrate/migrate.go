f96ffc3d57b54850e671c8ae467ecfacd65668f0
Puskar Basu <puskar@turbot.com>
2022-04-08 15:39:58 +0530

    update Migrate func

--- migrate/migrate.go
+++ migrate/migrate.go
@@ -2,7 +2,6 @@ package migrate
 
 import (
 	"encoding/json"
-	"fmt"
 	"os"
 
 	"github.com/turbot/steampipe/utils"
@@ -14,20 +13,18 @@ type Migrateable interface {
 	WriteOut() error
 }
 
-func Migrate(old interface{}, new Migrateable, oldPath string) error {
+func Migrate[O any, T Migrateable](old O, new T, oldPath string) error {
 	stateFileContent, err := os.ReadFile(oldPath)
 	if err != nil {
 		if os.IsNotExist(err) {
 			return nil
 		}
-		fmt.Println("Could not read file")
 		return err
 	}
 
-	// / deserialize into new
-	err = json.Unmarshal(stateFileContent, new)
+	// deserialize into new
+	err = json.Unmarshal(stateFileContent, &new)
 	if err != nil {
-		fmt.Println("Could not parse file")
 		return err
 	}
 
@@ -37,11 +34,10 @@ func Migrate(old interface{}, new Migrateable, oldPath string) error {
 	}
 
 	// if schemaVersion is not available, deserialize into old
-	err = json.Unmarshal(stateFileContent, old)
+	err = json.Unmarshal(stateFileContent, &old)
 	if err != nil {
-		fmt.Println("Could not parse file")
 		return err
 	}
-	new = new.MigrateFrom(old)
-	return utils.CombineErrors(os.Remove(oldPath), new.WriteOut())
+	x := new.MigrateFrom(old)
+	return utils.CombineErrors(os.Remove(oldPath), x.WriteOut())
 }
