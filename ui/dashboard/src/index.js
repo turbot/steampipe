3969cd53c498fdeaebcd07ca9b656a34814b6821
Mike Burgess <mike@turbot.com>
2022-04-11 10:04:17 +0100

    Update dashboard UI dependencies #1429

--- ui/dashboard/src/index.js
+++ ui/dashboard/src/index.js
@@ -1,26 +1,26 @@
 import App from "./App";
 import ErrorBoundary from "./components/ErrorBoundary";
 import React from "react";
-import ReactDOM from "react-dom";
 import { AnalyticsProvider } from "./hooks/useAnalytics";
 import { BreakpointProvider } from "./hooks/useBreakpoint";
 import { BrowserRouter as Router } from "react-router-dom";
+import { createRoot } from "react-dom/client";
 import { ThemeProvider } from "./hooks/useTheme";
 import "./styles/index.css";
 
-ReactDOM.render(
-  <React.StrictMode>
-    <Router>
-      <ThemeProvider>
-        <ErrorBoundary>
-          <BreakpointProvider>
-            <AnalyticsProvider>
-              <App />
-            </AnalyticsProvider>
-          </BreakpointProvider>
-        </ErrorBoundary>
-      </ThemeProvider>
-    </Router>
-  </React.StrictMode>,
-  document.getElementById("root")
+const container = document.getElementById("root");
+const root = createRoot(container);
+
+root.render(
+  <Router>
+    <ThemeProvider>
+      <ErrorBoundary>
+        <BreakpointProvider>
+          <AnalyticsProvider>
+            <App />
+          </AnalyticsProvider>
+        </BreakpointProvider>
+      </ErrorBoundary>
+    </ThemeProvider>
+  </Router>
 );
