269e5509610bccb9af3ec4835c7642d77eb26f21
Binaek Sarkar <binaek@turbot.com>
2022-04-19 21:33:43 +0530

    Fixes issue where plugin install crashes for non-existent plugins. Closes #1896

--- constants/plugin.go
+++ constants/plugin.go
@@ -4,4 +4,5 @@ const (
 	PluginAlreadyInstalled       = "Already installed"
 	PluginLatestAlreadyInstalled = "Latest already installed"
 	PluginNotInstalled           = "Not installed"
+	PluginNotFound               = "Not found"
 )
