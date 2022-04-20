66166041d6c2e6dfff3ccd6a4ac0f8e4ba791897
Mike Burgess <mike@turbot.com>
2022-04-21 00:06:09 +0100

    WIP

--- ui/dashboard/src/components/dashboards/check/Benchmark/index.tsx
+++ ui/dashboard/src/components/dashboards/check/Benchmark/index.tsx
@@ -4,7 +4,7 @@ import Error from "../../Error";
 import Panel from "../../layout/Panel";
 import padEnd from "lodash/padEnd";
 import Table from "../../Table";
-import { BenchmarkTreeProps, CheckProps, CheckSummary } from "../common";
+import { BenchmarkTreeProps, CheckProps } from "../common";
 import { classNames } from "../../../../utils/styles";
 import { default as BenchmarkType } from "../common/Benchmark";
 import { default as ControlType } from "../common/Control";
@@ -13,139 +13,20 @@ import { LeafNodeData } from "../../common";
 import { stringToColour } from "../../../../utils/color";
 import { useMemo, useState } from "react";
 
-const getPadding = (depth) => {
-  switch (depth) {
-    case 1:
-      return "pl-[6px]";
-    case 2:
-      return "pl-[12px]";
-    case 3:
-      return "pl-[18px]";
-    case 4:
-      return "pl-[24px]";
-    case 5:
-      return "pl-[30px]";
-    case 6:
-      return "pl-[36px]";
-    default:
-      return "pl-0";
-  }
-};
-
 interface ControlNodeProps {
   depth: number;
   control: ControlType;
-  rootSummary: CheckSummary;
 }
 
 interface BenchmarkNodeProps {
   depth: number;
   benchmark: BenchmarkType;
-  rootSummary: CheckSummary;
 }
 
 interface BenchmarkTableViewProps {
   benchmark: BenchmarkType;
   definition: CheckProps;
 }
-
-// const CheckSummary = ({ summary }) => {
-//   return (
-//     <div className="flex tabular-nums space-x-4 justify-end group-hover:bg-black-scale-1 px-1">
-//       <pre
-//         className={classNames(
-//           "inline",
-//           summary.ok > 0 ? "text-ok" : "text-foreground-lightest"
-//         )}
-//       >{`${padStart(summary.ok, 5)}`}</pre>
-//       <pre
-//         className={classNames(
-//           "inline",
-//           summary.alarm > 0 ? "text-alert" : "text-foreground-lightest"
-//         )}
-//       >{`${padStart(summary.alarm, 5)}`}</pre>
-//       <pre
-//         className={classNames(
-//           "inline",
-//           summary.error > 0 ? "text-alert" : "text-foreground-lightest"
-//         )}
-//       >{`${padStart(summary.error, 5)}`}</pre>
-//       <pre
-//         className={classNames(
-//           "inline",
-//           summary.info > 0 ? "text-info" : "text-foreground-lightest"
-//         )}
-//       >{`${padStart(summary.info, 5)}`}</pre>
-//       <pre
-//         className={classNames(
-//           "inline",
-//           summary.skip > 0 ? null : "text-foreground-lightest"
-//         )}
-//       >{`${padStart(summary.skip, 5)}`}</pre>
-//     </div>
-//   );
-// };
-
-// const BenchmarkNodeIcon = ({ expanded, run_state }) => (
-//   <div className="relative flex items-center">
-//     {!expanded && (
-//       <ExpandBenchmarkIcon className="flex-shrink-0 w-5 h-5 text-foreground-lighter" />
-//     )}
-//     {expanded && (
-//       <CollapseBenchmarkIcon className="flex-shrink-0 w-5 h-5 text-foreground-lighter" />
-//     )}
-//     {(run_state === "ready" || run_state === "started") && (
-//       <LoadingIndicator className="absolute flex-shrink-0 w-5 h-5 top-px left-0 text-foreground-lightest" />
-//     )}
-//   </div>
-// );
-//
-// const ControlNodeIcon = ({ expanded, run_state }) => {
-//   return (
-//     <div className="relative flex items-center">
-//       {!expanded && (
-//         <ExpandBenchmarkIcon className="flex-shrink-0 w-5 h-5 text-foreground-lighter" />
-//       )}
-//       {expanded && (
-//         <CollapseBenchmarkIcon className="flex-shrink-0 w-5 h-5 text-foreground-lighter" />
-//       )}
-//       {(run_state === "ready" || run_state === "started") && (
-//         <LoadingIndicator className="absolute flex-shrink-0 w-5 h-5 top-px left-0 text-foreground-lightest" />
-//       )}
-//     </div>
-//   );
-// };
-
-// const controlColumns = [
-//   {
-//     Header: "status",
-//     accessor: "status",
-//     name: "status",
-//     data_type_name: "CONTROL_STATUS",
-//     wrap: false,
-//   },
-//   {
-//     Header: "resource",
-//     accessor: "resource",
-//     name: "resource",
-//     data_type_name: "TEXT",
-//     wrap: false,
-//   },
-//   {
-//     Header: "reason",
-//     accessor: "reason",
-//     name: "reason",
-//     data_type_name: "TEXT",
-//     wrap: true,
-//   },
-//   {
-//     Header: "dimensions",
-//     accessor: "dimensions",
-//     name: "dimensions",
-//     data_type_name: "CONTROL_DIMENSIONS",
-//     wrap: true,
-//   },
-// ];
 
 const ControlDimension = ({ dimensionKey, dimensionValue }) => (
   <span
@@ -228,12 +109,12 @@ const ControlResults = ({ depth, results }) => {
   ));
 };
 
-const ControlNode = ({ depth = 0, control, rootSummary }: ControlNodeProps) => {
+const ControlNode = ({ depth = 0, control }: ControlNodeProps) => {
   const [showControls, setShowControls] = useState(false);
   return (
     <>
-      <div className="group flex rounded-sm py-2">
-        <NodeIndentMarker depth={depth} expanded={showControls} />
+      <div className="group flex rounded-sm py-2 items-center">
+        <NodeIndentMarker depth={depth} />
         <div
           className={classNames(
             "pr-1 space-x-2 flex flex-grow flex-nowrap truncate group-hover:bg-black-scale-1 items-center",
@@ -249,11 +130,7 @@ const ControlNode = ({ depth = 0, control, rootSummary }: ControlNodeProps) => {
         >
           <p>{control.title || control.name}</p>
         </div>
-        <CheckSummaryChart
-          summary={control.summary}
-          rootSummary={rootSummary}
-        />
-        {/*<CheckSummary summary={control.summary} />*/}
+        <CheckSummaryChart summary={control.summary} />
       </div>
       {showControls && (
         <>
@@ -269,9 +146,6 @@ const ControlNode = ({ depth = 0, control, rootSummary }: ControlNodeProps) => {
               <ControlIndentMarker depth={depth + 1} />
               <ControlError depth={depth + 1} error={control.run_error} />
             </>
-            // <div className="pl-4 py-4">
-            //   <Error error={control.run_error} />
-            // </div>
           )}
         </>
       )}
@@ -291,9 +165,8 @@ const ControlIndentMarker = ({ depth }) => {
   );
 };
 
-const NodeIndentMarker = ({ depth, expanded }) => {
+const NodeIndentMarker = ({ depth }) => {
   const barArray = Array(depth > 0 ? depth - 1 : 0).fill("|");
-  // const plusArray = Array(depth > 0 ? 1 : 0).fill(expanded ? "-" : "+");
   const plusArray = Array(depth > 0 ? 1 : 0).fill("+");
   const combinedArray = [...barArray, ...plusArray];
   const joined = combinedArray.join(" ");
@@ -306,24 +179,19 @@ const NodeIndentMarker = ({ depth, expanded }) => {
   );
 };
 
-const BenchmarkNode = ({
-  depth = 0,
-  benchmark,
-  rootSummary,
-}: BenchmarkNodeProps) => {
+const BenchmarkNode = ({ depth = 0, benchmark }: BenchmarkNodeProps) => {
   const [expanded, setExpanded] = useState(depth < 1);
 
   return (
     <>
-      <div className="group flex rounded-sm py-2">
-        <NodeIndentMarker depth={depth} expanded={expanded} />
+      <div className="group flex rounded-sm py-2 items-center">
+        <NodeIndentMarker depth={depth} />
         <div
           className={classNames(
             "pr-1 space-x-2 flex flex-grow group-hover:bg-black-scale-1 items-center",
             benchmark.benchmarks || benchmark.controls
               ? "cursor-pointer "
               : null
-            // getPadding(depth)
           )}
           onClick={
             benchmark.benchmarks || benchmark.controls
@@ -331,35 +199,17 @@ const BenchmarkNode = ({
               : undefined
           }
         >
-          {/*<BenchmarkNodeIcon*/}
-          {/*  expanded={expanded}*/}
-          {/*  run_state={benchmark.run_state}*/}
-          {/*/>*/}
           <p>{benchmark.title || benchmark.name}</p>
         </div>
-        <CheckSummaryChart
-          summary={benchmark.summary}
-          rootSummary={rootSummary}
-        />
-        {/*<CheckSummary summary={benchmark.summary} />*/}
+        <CheckSummaryChart summary={benchmark.summary} />
       </div>
       {expanded && (
         <>
           {benchmark.benchmarks.map((b) => (
-            <BenchmarkNode
-              key={b.name}
-              depth={depth + 1}
-              benchmark={b}
-              rootSummary={rootSummary}
-            />
+            <BenchmarkNode key={b.name} depth={depth + 1} benchmark={b} />
           ))}
           {benchmark.controls.map((c) => (
-            <ControlNode
-              key={c.name}
-              depth={depth + 1}
-              control={c}
-              rootSummary={rootSummary}
-            />
+            <ControlNode key={c.name} depth={depth + 1} control={c} />
           ))}
         </>
       )}
@@ -492,21 +342,7 @@ const BenchmarkTree = (props: BenchmarkTreeProps) => {
 
   return (
     <div className="p-4">
-      {/*// <div className="flex flex-grow"></div>*/}
-      {/*//{" "}*/}
-      {/*<div className="flex text-foreground-light space-x-4 tabular-nums justify-end px-1">*/}
-      {/*  // <pre className="inline">{`${padStart("OK", 5)}`}</pre>*/}
-      {/*  // <pre className="inline">{`${padStart("Alarm", 5)}`}</pre>*/}
-      {/*  // <pre className="inline">{`${padStart("Error", 5)}`}</pre>*/}
-      {/*  // <pre className="inline">{`${padStart("Info", 5)}`}</pre>*/}
-      {/*  // <pre className="inline">{`${padStart("Skip", 5)}`}</pre>*/}
-      {/*  //{" "}*/}
-      {/*</div>*/}
-      <BenchmarkNode
-        depth={0}
-        benchmark={props.properties.benchmark}
-        rootSummary={rootSummary}
-      />
+      <BenchmarkNode depth={0} benchmark={props.properties.benchmark} />
     </div>
   );
 };
@@ -570,49 +406,3 @@ const BenchmarkWrapper = (props: CheckProps) => {
 export default BenchmarkWrapper;
 
 export { BenchmarkTree, ControlDimension };
-
-// const res = {
-//   action: "leaf_node_complete",
-//   dashboard_node: {
-//     name: "mike.chart.dashboard_benchmarks_anonymous_chart_0",
-//     sql: "      select region, count(*) as total from aws_s3_bucket group by region order by total asc\n",
-//     data: {
-//       columns: [
-//         {
-//           name: "region",
-//           data_type_name: "TEXT",
-//         },
-//         {
-//           name: "total",
-//           data_type_name: "INT8",
-//         },
-//       ],
-//       rows: [
-//         ["us-west-2", 1],
-//         ["ap-northeast-2", 1],
-//         ["ap-south-1", 1],
-//         ["ap-southeast-1", 1],
-//         ["ap-southeast-2", 1],
-//         ["ca-central-1", 1],
-//         ["us-west-1", 1],
-//         ["ap-northeast-1", 1],
-//         ["eu-north-1", 1],
-//         ["eu-west-1", 1],
-//         ["eu-west-2", 1],
-//         ["eu-west-3", 1],
-//         ["sa-east-1", 1],
-//         ["us-east-2", 1],
-//         ["eu-central-1", 2],
-//         ["us-east-1", 15],
-//       ],
-//     },
-//     properties: {
-//       type: "column",
-//     },
-//     node_type: "chart",
-//     dashboard: "mike.dashboard.benchmarks",
-//     source_definition:
-//       '  chart {\n    type = "column"\n    sql = <<EOQ\n      select region, count(*) as total from aws_s3_bucket group by region order by total asc\n    EOQ\n  }',
-//   },
-//   execution_id: "0x14001696e10",
-// };
