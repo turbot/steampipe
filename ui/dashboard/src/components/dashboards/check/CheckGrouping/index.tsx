f9698c4eb68eda5e4bc5794293bb2ab3f20fa871
Mike Burgess <mike@turbot.com>
2022-04-22 11:31:26 +0100

    Use centered check summary chart

--- ui/dashboard/src/components/dashboards/check/CheckGrouping/index.tsx
+++ ui/dashboard/src/components/dashboards/check/CheckGrouping/index.tsx
@@ -1,15 +1,16 @@
 import CheckPanel from "../CheckPanel";
-import { CheckNode } from "../common";
+import { CheckNode, CheckSummary } from "../common";
 
 interface CheckGroupingProps {
   node: CheckNode;
+  rootSummary: CheckSummary;
 }
 
-const CheckGrouping = ({ node }: CheckGroupingProps) => {
+const CheckGrouping = ({ node, rootSummary }: CheckGroupingProps) => {
   return (
     <div className="space-y-8 col-span-12">
       {node.children?.map((child) => (
-        <CheckPanel key={child.name} node={child} />
+        <CheckPanel key={child.name} node={child} rootSummary={rootSummary} />
       ))}
     </div>
   );
