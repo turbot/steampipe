519dace185a864401f3f491b5e7e566b57e32980
Binaek Sarkar <binaek@turbot.com>
2022-04-25 17:18:05 +0530

    restore in steps

--- db/db_local/backup.go
+++ db/db_local/backup.go
@@ -1,6 +1,7 @@
 package db_local
 
 import (
+	"bufio"
 	"context"
 	"fmt"
 	"log"
@@ -13,10 +14,16 @@ import (
 	"github.com/turbot/go-kit/helpers"
 	"github.com/turbot/steampipe/constants"
 	"github.com/turbot/steampipe/filepaths"
+	"github.com/turbot/steampipe/utils"
 )
 
 var (
 	errDbInstanceRunning = fmt.Errorf("cannot start DB backup - an instance is still running. To stop running services, use %s ", constants.Bold("steampipe service stop"))
+)
+
+const (
+	NoMatViewRefreshListFileName   = "without_refresh.lst"
+	OnlyMatViewRefreshListFileName = "only_refresh.lst"
 )
 
 type oldDbRunningConfig struct {
@@ -49,7 +56,7 @@ func prepareBackup(ctx context.Context) (*string, error) {
 		return nil, err
 	}
 
-	return &config.dbName, os.RemoveAll(location)
+	return &config.dbName, nil
 }
 
 func errIfInstanceRunning(ctx context.Context, location string) error {
@@ -155,6 +162,7 @@ func stopDbByCmd(ctx context.Context, cmd *exec.Cmd) error {
 // needsBackup checks whether the `$STEAMPIPE_INSTALL_DIR/db` directory contains any database installation
 // other than desired version.
 // it's called as part of `prepareBackup` to decide whether `pg_dump` needs to run
+// it's also called as part of `restoreBackup` for removal of the installation once restoration successfully completes
 func needsBackup(ctx context.Context) (bool, string, error) {
 	dbBaseDirectory := filepaths.EnsureDatabaseDir()
 	entries, err := os.ReadDir(dbBaseDirectory)
@@ -194,13 +202,59 @@ func restoreBackup(ctx context.Context) error {
 	}
 
 	// load the db status
-	info, err := GetState()
+	runningInfo, err := GetState()
 	if err != nil {
 		return err
 	}
-	if info == nil {
+	if runningInfo == nil {
 		return fmt.Errorf("steampipe service is not running")
 	}
+
+	// get the location of the other instance which was backed up
+	_, location, err := needsBackup(ctx)
+	if err != nil {
+		return err
+	}
+
+	// extract the Table of Contents from the Backup Archive
+	toc, err := getTOCFromBackup(ctx)
+	if err != nil {
+		return err
+	}
+
+	// partition the Table of Contents into separate lists (no refresh and only refresh)
+	noRefreshListFile, onlyRefreshListFile, err := splitTocMatViewRefreshData(ctx, toc)
+	if err != nil {
+		return err
+	}
+	defer func() {
+		os.Remove(noRefreshListFile)
+		os.Remove(onlyRefreshListFile)
+	}()
+	err = runRestoreUsingList(ctx, runningInfo, noRefreshListFile)
+	if err != nil {
+		return err
+	}
+	err = runRestoreUsingList(ctx, runningInfo, onlyRefreshListFile)
+	if err != nil {
+		// we could not refresh the Materialized views
+		// this is probably because the Materialized views
+		// contain transitive references to unqualified table names
+		// WARN the user.
+		// TODO: We should try to refresh these separately using a Maintenance Client
+		// https://github.com/turbot/steampipe/issues/1934
+		log.Println("[WARN] Could not REFRESH MATERAILIZED VIEWS while restoring data.")
+	}
+
+	err = os.Remove(databaseBackupFilePath())
+	if err != nil {
+		log.Printf("[WARN] Could not remove Backup data at %s.", databaseBackupFilePath())
+	}
+
+	return os.RemoveAll(location)
+}
+
+func runRestoreUsingList(ctx context.Context, info *RunningDBInstanceInfo, listFile string) error {
 
 	cmd := exec.CommandContext(
 		ctx,
@@ -214,8 +268,8 @@ func restoreBackup(ctx context.Context) error {
 		// This ensures that either all the commands complete successfully, or no changes are applied.
 		// This option implies --exit-on-error.
 		"--single-transaction",
-		// immadiately Exit if an error is encountered while sending SQL commands to the database.
-		"--exit-on-error",
+		// Restore only those archive elements that are listed in list-file, and restore them in the order they appear in the file.
+		fmt.Sprintf("--use-list=%s", listFile),
 		// the database name
 		fmt.Sprintf("--dbname=%s", info.Database),
 		// connection parameters
@@ -230,5 +284,65 @@ func restoreBackup(ctx context.Context) error {
 		return err
 	}
 
-	return nil // os.Remove(getBackupFile())
+	return nil
+}
+
+// splitTocMatViewRefreshData writes back the TableOfContents into a two temporary files:
+// 	1. without REFRESH MATERIALIZED VIEWS
+//  2. only REFRESH MATERIALIZED VIEWS
+//
+// This needs to be done because REFRESHING cannot work in restore, since the BackUP will always set
+// a blank search path before commencing and the MATERIALIZED VIEWS may have functions with
+// unqualified table names
+func splitTocMatViewRefreshData(ctx context.Context, toc []string) (string, string, error) {
+	withoutRefresh, onlyRefresh := utils.Partition(toc, func(v string) bool {
+		return !strings.Contains(strings.ToUpper(v), "MATERIALIZED VIEW DATA")
+	})
+
+	withoutFile := filepath.Join(filepaths.EnsureDatabaseDir(), NoMatViewRefreshListFileName)
+	onlyFile := filepath.Join(filepaths.EnsureDatabaseDir(), OnlyMatViewRefreshListFileName)
+
+	err := utils.CombineErrors(
+		os.WriteFile(withoutFile, []byte(strings.Join(withoutRefresh, "\n")), 0644),
+		os.WriteFile(onlyFile, []byte(strings.Join(onlyRefresh, "\n")), 0644),
+	)
+
+	return withoutFile, onlyFile, err
+}
+
+// getTOCFromBackup uses pg_restore to read the TableOfContents from the
+// back archive
+func getTOCFromBackup(ctx context.Context) ([]string, error) {
+	cmd := exec.CommandContext(
+		ctx,
+		pgRestoreBinaryExecutablePath(),
+		databaseBackupFilePath(),
+		// as a tar format
+		"--format=tar",
+		// only the public schema is backed up
+		"--schema=public",
+		"--list",
+	)
+	log.Println("[TRACE]", cmd.String())
+
+	b, err := cmd.Output()
+	if err != nil {
+		return nil, err
+	}
+
+	scanner := bufio.NewScanner(strings.NewReader(string(b)))
+	scanner.Split(bufio.ScanLines)
+
+	lines := []string{";" /* start with an extra comment line */}
+	for scanner.Scan() {
+		line := scanner.Text()
+		if strings.HasPrefix(line, ";") {
+			// no use of comments
+			continue
+		}
+		lines = append(lines, scanner.Text())
+	}
+	lines = append(lines, ";" /* an extra comment line at the end */)
+
+	return lines, err
 }
