1e04af5c68a07f01cb2ada7e6cd461588228ed6d
Binaek Sarkar <binaek@turbot.com>
2022-04-25 17:17:57 +0530

    comment

--- utils/collection.go
+++ utils/collection.go
@@ -1,5 +1,10 @@
 package utils
 
+// Partition splits the array of elements into two groups:
+// the left partition contains elements that the predicate returns `true` for.
+// the right partition contains elements that the predicate returns `false` for.
+//
+// The predicate is invoked with each element
 func Partition[V any](elements []V, predicate func(V) bool) ([]V, []V) {
 	leftPartition := []V{}
 	rightPartition := []V{}
