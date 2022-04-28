4b4e22809a83f95ca5a20bee3c40830de6cc60af
kai <kai@turbot.com>
2022-04-28 13:03:09 +0100

    Update FDW version to 1.0.0-rc.1 - uncomment and Fix mutable function/self references check in qual code.

--- constants/db.go
+++ constants/db.go
@@ -28,14 +28,10 @@ const (
 // constants for installing db and fdw images
 const (
 	DatabaseVersion = "14.2.0"
-	FdwVersion      = "1.0.0-rc.0"
+	FdwVersion      = "1.0.0-rc.1"
 
 	// PostgresImageRef is the OCI Image ref for the databse binaries
-	// The 12.1.0 image uses the older jar format 12.1.0-v2 is the same version of postgres,
-	// just packaged as gzipped tar files (consistent with oras, faster to unzip).  Once everyone is
-	// on a newer build, we can delete the old image move the 12.1.0 tag to the new image, and
-	// change this back for consistency
-	PostgresImageRef = "us-docker.pkg.dev/steampipe/steampipe/db:14.2.0"
+	PostgresImageRef = "us-docker.pkg.dev/steampipe/steampipe/db:" + DatabaseVersion
 	FdwImageRef      = "us-docker.pkg.dev/steampipe/steampipe/fdw:" + FdwVersion
 )
 
