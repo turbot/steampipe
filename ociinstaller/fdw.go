efbaffebf3f569a55f2c8ac9b05a0ffeae4be1e5
kai <kai@turbot.com>
2022-05-25 17:22:27 +0100

    tidy

--- ociinstaller/fdw.go
+++ ociinstaller/fdw.go
@@ -3,12 +3,14 @@ package ociinstaller
 import (
 	"context"
 	"fmt"
+	"log"
+	"os/exec"
 	"path/filepath"
 	"time"
-	"log"
 
 	"github.com/turbot/steampipe/constants"
 	versionfile "github.com/turbot/steampipe/ociinstaller/versionfile"
+	"github.com/turbot/steampipe/utils"
 )
 
 // InstallFdw installs the Steampipe Postgres foreign data wrapper from an OCI image
@@ -59,11 +61,28 @@ func installFdwFiles(image *SteampipeImage, tempdir string, dest string) error {
 	hubBinPath := filepath.Join(dest, "lib", "postgresql")
 	hubControlPath := filepath.Join(dest, "share", "postgresql", "extension")
 	hubSQLPath := filepath.Join(dest, "share", "postgresql", "extension")
-
 	fileName := image.Fdw.BinaryFile
 	sourcePath := filepath.Join(tempdir, fileName)
-	if _, err := ungzip(sourcePath, hubBinPath); err != nil {
-		return fmt.Errorf("could not unzip %s to %s", sourcePath, hubBinPath)
+
+	isM1, err := utils.IsMacM1()
+	if err != nil {
+		return fmt.Errorf("failed to detect system architecture")
+	}
+	if isM1 {
+		// TACTICAL: when installing the FDW for Mac M1, it is necessary to do a shell copy of the unzipped file
+		if _, err := ungzip(sourcePath, tempdir); err != nil {
+			return fmt.Errorf("could not unzip %s to %s", sourcePath, tempdir)
+		}
+		unzippedSoPath := filepath.Join(tempdir)
+		var cpCmd = exec.Command("cp", unzippedSoPath, hubBinPath)
+		if _, err := cpCmd.Output(); err != nil {
+			return fmt.Errorf("could not copy extracted file %s to %s", unzippedSoPath, tempdir)
+		}
+	} else {
+		// for other platforms, unzip directly into the destination
+		if _, err := ungzip(sourcePath, hubBinPath); err != nil {
+			return fmt.Errorf("could not unzip %s to %s", sourcePath, hubBinPath)
+		}
 	}
 
 	fileName = image.Fdw.ControlFile
