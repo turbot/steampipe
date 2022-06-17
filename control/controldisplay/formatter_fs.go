e158aa1f09e40fdd443588ff8f7c62c2b295a949
Puskar Basu <puskar@turbot.com>
2022-06-17 17:17:49 +0530

    write templates always

--- control/controldisplay/formatter_fs.go
+++ control/controldisplay/formatter_fs.go
@@ -1,7 +1,6 @@
 package controldisplay
 
 import (
-	"bytes"
 	"embed"
 	"io/fs"
 	"log"
@@ -14,61 +13,26 @@ import (
 //go:embed templates/*
 var builtinTemplateFS embed.FS
 
-// EnsureTemplates scans the '$STEAMPIPE_INSTALL_DIR/templates' directory and
-// copies over any missing templates as defined in the 'templates' package,
-// also if there is a change in any of the templates, EnsureTemplates can detect
-// the change and copy over the new template file
+// WriteTemplates scans the '$STEAMPIPE_INSTALL_DIR/templates' directory and
+// copies over all the templates defined in the 'templates' package
 //
 // The name of the folder in the 'templates' package is used to identify
 // templates in '$STEAMPIPE_INSTALL_DIR/templates' - where it is expected
-// that a directory with the same name will exist. If said directory does
-// not exist, it is copied over from 'templates'
+// that a directory with the same name will exist.
 //
-func EnsureTemplates() error {
-	log.Println("[TRACE] ensuring check export/output templates")
+func WriteTemplates() error {
+	log.Println("[TRACE] writing check export/output templates")
 	dirs, err := fs.ReadDir(builtinTemplateFS, "templates")
 	if err != nil {
 		return err
 	}
 	for _, d := range dirs {
 		targetDirectory := filepath.Join(filepaths.EnsureTemplateDir(), d.Name())
-
-		// check if the target template directory exists, if not then create them
-		// and write the templates
-		if _, err := os.Stat(targetDirectory); os.IsNotExist(err) {
-			log.Println("[TRACE] target directory does not exist - copying template")
-			if err := writeTemplate(d.Name(), targetDirectory); err != nil {
-				log.Println("[ERROR] error copying template", err)
-				return err
-			}
-		} else if err != nil {
-			log.Println("[ERROR] error fetching directory information", err)
+		// write the templates for each directory
+		if err := writeTemplate(d.Name(), targetDirectory); err != nil {
+			log.Println("[ERROR] error copying template", err)
 			return err
 		}
-
-		// store the embedded template file info
-		embeddedFileInfo, err := fs.ReadFile(builtinTemplateFS, filepath.Join("templates", d.Name(), "output.tmpl"))
-		if err != nil {
-			log.Println("[ERROR] error fetching embedded template file info", err)
-			return err
-		}
-		// store the current template file info
-		templateFileInfo, err := os.ReadFile(filepath.Join(targetDirectory, "output.tmpl"))
-		if err != nil {
-			log.Println("[ERROR] error fetching current template file info", err)
-			return err
-		}
-
-		// if the target directory and templates exist, but there is a modification
-		// in the template, copy over the template
-		if !bytes.Equal(embeddedFileInfo, templateFileInfo) {
-			log.Println("[TRACE] there is a diff in the template file")
-			if err := writeTemplate(d.Name(), targetDirectory); err != nil {
-				log.Println("[ERROR] error copying template", err)
-				return err
-			}
-		}
-
 	}
 	return nil
 }
