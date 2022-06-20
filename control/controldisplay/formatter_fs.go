410f1123d2dc92e35734254bb9f4a3ef5e9e0c81
Puskar Basu <puskar@turbot.com>
2022-06-20 17:46:53 +0530

    update logic

--- control/controldisplay/formatter_fs.go
+++ control/controldisplay/formatter_fs.go
@@ -21,13 +21,13 @@ type TemplateVersionFile struct {
 }
 
 // WriteTemplates scans the '$STEAMPIPE_INSTALL_DIR/check/templates' directory and
-// copies over all the templates defined in the 'templates' package if needed.
+// copies over the templates defined in the 'templates' package if needed.
 //
 // The name of the folder in the 'templates' package is used to identify
 // templates in '$STEAMPIPE_INSTALL_DIR/templates' - where it is expected
 // that a directory with the same name will exist.
 //
-// We only re-write the templates, when there is a higher template version
+// We re-write the templates, when there is a higher template version
 // available in the 'templates' package.
 func WriteTemplates() error {
 	start := time.Now()
@@ -38,26 +38,14 @@ func WriteTemplates() error {
 	}
 	for _, d := range dirs {
 		targetDirectory := filepath.Join(filepaths.EnsureTemplateDir(), d.Name())
-		targetVersionsFilePath := filepath.Join(targetDirectory, "versions.json")
+		currentVersionsFilePath := filepath.Join(targetDirectory, "versions.json")
 		embeddedVersionsFilePath := filepath.Join("templates", d.Name(), "versions.json")
 
-		// check if versions file exist
-		if _, err := os.Stat(targetVersionsFilePath); os.IsNotExist(err) {
-			log.Println("[TRACE] target versions file does not exist - copying template")
+		// check if version in versions.json matches with embedded template version
+		if getCurrentTemplateVersion(currentVersionsFilePath) != getEmbeddedTemplateVersion(embeddedVersionsFilePath) {
+			log.Println("[TRACE] versions do not match - copying template")
 			if err := writeTemplate(d.Name(), targetDirectory); err != nil {
-				log.Println("[ERROR] error copying template", err)
-				return err
-			}
-		} else if err != nil {
-			log.Println("[ERROR] error fetching directory information", err)
-			return err
-		}
-
-		// now check if version in versions.json matches with current template version
-		if getTargetTemplateVersion(targetVersionsFilePath) != getEmbedTemplateVersion(embeddedVersionsFilePath) {
-			log.Println("[TRACE] target versions do not match - copying template")
-			if err := writeTemplate(d.Name(), targetDirectory); err != nil {
-				log.Println("[ERROR] error copying template", err)
+				log.Println("[TRACE] error copying template", err)
 				return err
 			}
 		}
@@ -67,31 +55,35 @@ func WriteTemplates() error {
 	return nil
 }
 
-func getTargetTemplateVersion(path string) string {
+func getCurrentTemplateVersion(path string) string {
 	data, err := ioutil.ReadFile(path)
 	if err != nil {
-		log.Println("[ERROR] error reading target version file", err)
+		if os.IsNotExist(err) {
+			log.Println("[TRACE] template version file does not exist - install the new template")
+		} else {
+			log.Println("[TRACE] error reading current version file - installing the new template")
+		}
 		return ""
 	}
 	var ver TemplateVersionFile
 	err = json.Unmarshal(data, &ver)
 	if err != nil {
-		log.Println("[ERROR] error while unmarshaling json", err)
+		log.Println("[TRACE] error while unmarshaling current versions.json file", err)
 		return ""
 	}
 	return ver.Version
 }
 
-func getEmbedTemplateVersion(path string) string {
+func getEmbeddedTemplateVersion(path string) string {
 	data, err := fs.ReadFile(builtinTemplateFS, path)
 	if err != nil {
-		log.Println("[ERROR] error reading embedded versionfile", err)
+		log.Println("[TRACE] error reading embedded version file - installing the new template")
 		return ""
 	}
 	var ver TemplateVersionFile
 	err = json.Unmarshal(data, &ver)
 	if err != nil {
-		log.Println("[ERROR] error while unmarshaling json", err)
+		log.Println("[TRACE] error while unmarshaling json", err)
 		return ""
 	}
 	return ver.Version
