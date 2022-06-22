f11c84635a3cd39f84ca0659256118fb70e43c07
Puskar Basu <puskar@turbot.com>
2022-06-22 19:25:37 +0530

    add dockerfiles

---
+++ dockerfiles/run-tests.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+
+chown steampipe:0 /home/steampipe/.steampipe/db/14.2.0/data/
+
+steampipe -v
+git clone https://github.com/turbot/steampipe.git
+cd steampipe
+git init
+git submodule update --init
+git submodule update --recursive
+
+./tests/acceptance/run.sh
