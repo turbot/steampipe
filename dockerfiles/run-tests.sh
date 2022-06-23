2a3bf4ae797f73d924496660d37502c4834de693
Puskar Basu <puskar@turbot.com>
2022-06-23 15:14:26 +0530

    chown steampipe:0 /workspace

--- dockerfiles/run-tests.sh
+++ dockerfiles/run-tests.sh
@@ -1,11 +1,12 @@
 #!/usr/bin/env bash
 
 # chown steampipe:0 /home/steampipe/.steampipe/db/14.2.0/data/
-
+chown steampipe:0 /workspace
 steampipe -v
 pwd
-cat /etc/sudoers
-sudo git clone https://github.com/turbot/steampipe.git
+whoami
+uname -a
+git clone https://github.com/turbot/steampipe.git
 ls -al
 cd steampipe
 git init
