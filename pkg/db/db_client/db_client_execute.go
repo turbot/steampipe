01ccfafe56bb638a202e334afbdb68c25ef22577
kaidaguerre <kai@turbot.com>
2022-07-13 11:04:44 +0100

    Steampipe sometimes hangs in interactive mod if timing is enabled. Closes #2237

--- pkg/db/db_client/db_client_execute.go
+++ pkg/db/db_client/db_client_execute.go
@@ -54,7 +54,7 @@ func (c *DbClient) ExecuteSyncInSession(ctx context.Context, session *db_common.
 			syncResult.Rows = append(syncResult.Rows, row)
 		}
 	}
-	if c.shouldShowTiming {
+	if c.shouldShowTiming() {
 		syncResult.TimingResult = <-result.TimingResult
 	}
 	return syncResult, nil
@@ -64,6 +64,9 @@ func (c *DbClient) ExecuteSyncInSession(ctx context.Context, session *db_common.
 // execute the query in the given Context
 // NOTE: The returned Result MUST be fully read - otherwise the connection will block and will prevent further communication
 func (c *DbClient) Execute(ctx context.Context, query string) (*queryresult.Result, error) {
+	// re-read shouldShowTiming from viper (in case the .timing command has been run)
+	c.setShouldShowTiming()
+
 	// acquire a session
 	sessionResult := c.AcquireSession(ctx)
 	if sessionResult.Error != nil {
@@ -91,8 +94,8 @@ func (c *DbClient) ExecuteInSession(ctx context.Context, session *db_common.Data
 	if session.Connection == nil {
 		return nil, fmt.Errorf("nil database connection passed to ExecuteInSession")
 	}
-
 	startTime := time.Now()
+
 	var tx *sql.Tx
 
 	defer func() {
@@ -103,7 +106,7 @@ func (c *DbClient) ExecuteInSession(ctx context.Context, session *db_common.Data
 			if tx != nil {
 				tx.Rollback()
 			}
-			// call the completion callback - if one was provided
+			// in case of error call the onComplete callback
 			if onComplete != nil {
 				onComplete()
 			}
@@ -133,10 +136,9 @@ func (c *DbClient) ExecuteInSession(ctx context.Context, session *db_common.Data
 		// read in the rows and stream to the query result object
 		c.readRows(ctx, startTime, rows, result)
 		// set the time that it took for this one to execute
-		if c.shouldShowTiming {
-			c.getQueryTimingAsync(ctx, startTime, session, result.TimingResult)
-		}
+		c.getQueryTiming(ctx, startTime, session, result.TimingResult)
 
+		// call the completion callback - if one was provided
 		if onComplete != nil {
 			onComplete()
 		}
@@ -145,16 +147,27 @@ func (c *DbClient) ExecuteInSession(ctx context.Context, session *db_common.Data
 	return result, nil
 }
 
-func (c *DbClient) getQueryTimingAsync(ctx context.Context, startTime time.Time, session *db_common.DatabaseSession, resultChannel chan *queryresult.TimingResult) {
+func (c *DbClient) getQueryTiming(ctx context.Context, startTime time.Time, session *db_common.DatabaseSession, resultChannel chan *queryresult.TimingResult) {
+	if !c.shouldShowTiming() {
+		// so we are not displaying timing - do not fetch the query timing
+		// however, we still need to update the updateScanMetadataMaxId
+		// (unless timing is disabled, i.e. this query is part of a timing fetch call)
+		if !c.disableTiming {
+			// even if not showing timing, we need to update ScanMetadataMaxId
+			c.updateScanMetadataMaxId(ctx, session)
+		}
+		return
+	}
+
 	var timingResult = &queryresult.TimingResult{
 		Duration: time.Since(startTime),
 	}
 	// disable fetching timing information to avoid recursion
-	c.shouldShowTiming = false
+	c.disableTiming = true
 
 	// whatever happens, we need to reenable timing, and send the result back with at least the duration
 	defer func() {
-		c.shouldShowTiming = true
+		c.disableTiming = false
 		resultChannel <- timingResult
 	}()
 
@@ -187,6 +200,25 @@ func (c *DbClient) getQueryTimingAsync(ctx context.Context, startTime time.Time,
 	session.ScanMetadataMaxId = id
 
 	return
+}
+
+func (c *DbClient) updateScanMetadataMaxId(ctx context.Context, session *db_common.DatabaseSession) error {
+	res, err := c.ExecuteSyncInSession(ctx, session, "select max(id) from steampipe_command.scan_metadata")
+	if err != nil {
+		return err
+	}
+
+	for _, r := range res.Rows {
+		rw := r.(*queryresult.RowResult)
+		id, ok := rw.Data[0].(int64)
+		if ok {
+			// update the max id for this session
+			session.ScanMetadataMaxId = id
+		}
+
+		break
+	}
+	return nil
 }
 
 // run query in a goroutine, so we can check for cancellation
