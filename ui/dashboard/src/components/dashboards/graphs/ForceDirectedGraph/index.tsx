612217dcf04f29a61b4fde78489eb068930d858c
Mike Burgess <mike@turbot.com>
2022-07-19 09:21:35 +0100

    Allow romaing in graphs (zoom + move)

--- ui/dashboard/src/components/dashboards/graphs/ForceDirectedGraph/index.tsx
+++ ui/dashboard/src/components/dashboards/graphs/ForceDirectedGraph/index.tsx
@@ -2,11 +2,11 @@ import Graph from "../Graph";
 import { GraphProps, IGraph } from "../types";
 import { registerGraphComponent } from "../index";
 
-const Sankey = (props: GraphProps) => <Graph {...props} />;
+const ForceDirectedGraph = (props: GraphProps) => <Graph {...props} />;
 
 const definition: IGraph = {
   type: "graph",
-  component: Sankey,
+  component: ForceDirectedGraph,
 };
 
 registerGraphComponent(definition.type, definition);
