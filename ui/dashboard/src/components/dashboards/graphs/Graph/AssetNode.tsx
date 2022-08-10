053d382f5cee6dbe2d0aea689e0fd99a1874259d
Mike Burgess <mike@turbot.com>
2022-08-10 21:57:41 +0100

    Fix z-index of select input menu in portal

--- ui/dashboard/src/components/dashboards/graphs/Graph/AssetNode.tsx
+++ ui/dashboard/src/components/dashboards/graphs/Graph/AssetNode.tsx
@@ -2,8 +2,7 @@ import { Handle } from "react-flow-renderer";
 import { memo } from "react";
 
 const AssetNode = ({ data }) => {
-  console.log(data);
-  const icon = data.icon ? data.icon : "aws_s3_bucket";
+  const icon = data.icon ? data.icon : null;
 
   // Notes:
   // * The Handle elements seem to be required to allow the connectors to work.
@@ -15,7 +14,7 @@ const AssetNode = ({ data }) => {
       <Handle type="source" />
       <div className="flex flex-col items-center cursor-auto">
         <div className="py-2 px-2 rounded-full w-[35px] h-[35px] text-sm leading-[35px] my-0 mx-auto border border-divide cursor-grab">
-          <img className="max-w-full" src={icon} />
+          {icon && <img className="max-w-full" src={icon} />}
         </div>
         <div className="text-[7px] mt-1 bg-dashboard-panel text-foreground min-w-[35px]">
           {data.label}
