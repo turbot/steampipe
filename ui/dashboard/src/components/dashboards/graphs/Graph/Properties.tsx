55e7f189fde034bd52cd7ea094d81bf277f62a0e
Mike Burgess <mike@turbot.com>
2022-08-11 14:09:57 +0100

    Improve rendering of property values

--- ui/dashboard/src/components/dashboards/graphs/Graph/Properties.tsx
+++ ui/dashboard/src/components/dashboards/graphs/Graph/Properties.tsx
@@ -1,17 +1,32 @@
 import { classNames } from "../../../../utils/styles";
 
+const PropertyItemValue = ({ value }) => {
+  if (value === null || value === undefined) {
+    return (
+      <span className="text-foreground-lightest">
+        <>null</>
+      </span>
+    );
+  }
+  let renderValue: string = "";
+  switch (typeof value) {
+    case "object":
+      renderValue = JSON.stringify(value, null, 2);
+      break;
+    default:
+      renderValue = value.toString();
+      break;
+  }
+  return (
+    <span className={classNames("block", "break-words")}>{renderValue}</span>
+  );
+};
+
 const PropertyItem = ({ name, value }) => {
   return (
     <div>
       <span className="block text-sm text-table-head truncate">{name}</span>
-      {value === null && (
-        <span className="text-foreground-lightest">
-          <>null</>
-        </span>
-      )}
-      {value !== null && (
-        <span className={classNames("block", "break-words")}>{value}</span>
-      )}
+      <PropertyItemValue value={value} />
     </div>
     // <div className="space-x-2">
     //   <span>{name}</span>
