1320f4c5896511044bbb478830f9d4fc02ba3c70
Mike Burgess <mike@turbot.com>
2022-08-18 11:19:44 +0100

    Support column-like "fields" with href rendering in properties tooltips

--- ui/dashboard/src/components/dashboards/Table/index.tsx
+++ ui/dashboard/src/components/dashboards/Table/index.tsx
@@ -33,8 +33,8 @@ import { ThemeNames } from "../../../hooks/useTheme";
 import { useSortBy, useTable } from "react-table";
 import { registerComponent } from "../index";
 
-type TableColumnDisplay = "all" | "none";
-type TableColumnWrap = "all" | "none";
+export type TableColumnDisplay = "all" | "none";
+export type TableColumnWrap = "all" | "none";
 
 interface TableColumnInfo {
   Header: string;
--- ui/dashboard/src/components/dashboards/common/types.ts
+++ ui/dashboard/src/components/dashboards/common/types.ts
@@ -1,3 +1,20 @@
+import { TableColumnDisplay, TableColumnWrap } from "../Table";
+
 export interface ChartTooltipFormatter {
   format(params: Object | any[]): string;
+}
+
+export interface CategoryFields {
+  [name: string]: CategoryField;
+}
+
+export interface CategoryField {
+  name: string;
+  href?: string | null;
+  display?: TableColumnDisplay;
+  wrap?: TableColumnWrap;
+}
+
+export interface KeyValuePairs {
+  [key: string]: any;
 }
