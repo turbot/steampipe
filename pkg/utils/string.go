e276c64e96b6450c03a5f8785ce0267cf5810f2f
Puskar Basu <puskar@turbot.com>
2022-09-05 11:50:51 +0530

    Fix the alignment of plugin update/install outputs. Closes #2417

--- pkg/utils/string.go
+++ pkg/utils/string.go
@@ -1,6 +1,7 @@
 package utils
 
 import (
+	"bytes"
 	"encoding/csv"
 	"strings"
 )
@@ -18,4 +19,37 @@ func SplitByRune(str string, r rune) []string {
 // SplitByWhitespace splits by the ' ' rune
 func SplitByWhitespace(str string) []string {
 	return SplitByRune(str, ' ')
+}
+
+// Resize resizes the string with the given length. It ellipses with '…' when the string's length exceeds
+// the desired length or pads spaces to the right of the string when length is smaller than desired
+func Resize(s string, length uint) string {
+	n := int(length)
+	if len(s) == n {
+		return s
+	}
+	// Pads only when length of the string smaller than len needed
+	s = PadRight(s, n, ' ')
+	if len(s) > n {
+		b := []byte(s)
+		var buf bytes.Buffer
+		for i := 0; i < n-3; i++ {
+			buf.WriteByte(b[i])
+		}
+		buf.WriteString("…  ")
+		s = buf.String()
+	}
+	return s
+}
+
+// PadRight returns a new string of a specified length in which the end of the current string is padded with spaces or with a specified Unicode character.
+func PadRight(str string, length int, pad byte) string {
+	if len(str) >= length {
+		return str
+	}
+	buf := bytes.NewBufferString(str)
+	for i := 0; i < length-len(str); i++ {
+		buf.WriteByte(pad)
+	}
+	return buf.String()
 }
