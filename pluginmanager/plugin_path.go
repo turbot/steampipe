208538f8ff7c08f1cac47df530468b9d9352d35b
kai <kai@turbot.com>
2022-09-22 14:16:03 +0800

    circular ref issue

--- pluginmanager/plugin_path.go
+++ pluginmanager/plugin_path.go
@@ -2,6 +2,7 @@ package pluginmanager
 
 import (
 	"fmt"
+	"github.com/turbot/steampipe/pkg/utils"
 	"log"
 	"os"
 	"path/filepath"
@@ -50,7 +51,7 @@ func FindPluginFolder(remoteSchema string) (string, error) {
 	pluginDir := filepaths.EnsurePluginDir()
 
 	// first try searching by prefix - trim the schema name
-	globPattern := filepath.Join(pluginDir, trimSchemaName(remoteSchema)) + "*"
+	globPattern := filepath.Join(pluginDir, utils.TrimSchemaName(remoteSchema)) + "*"
 	matches, err := filepath.Glob(globPattern)
 	if err != nil {
 		return "", err
@@ -65,7 +66,7 @@ func FindPluginFolder(remoteSchema string) (string, error) {
 			// do not fail on error here
 			continue
 		}
-		hashedName := PluginFQNToSchemaName(folderRelativePath)
+		hashedName := utils.PluginFQNToSchemaName(folderRelativePath)
 		if hashedName == remoteSchema {
 			return filepath.Join(pluginDir, folderRelativePath), nil
 		}
