6942fb705cc727afc770be256a20471f741dd936
Mike Burgess <mike@turbot.com>
2022-09-27 14:43:21 +0100

    Add tests for SnapshotRenderComplete

---
+++ ui/dashboard/src/components/snapshot/SnapshotRenderComplete/index.test.jsx
@@ -0,0 +1,29 @@
+import React from "react";
+import SnapshotRenderComplete from "./index.tsx";
+import { DashboardContext } from "../../../hooks/useDashboard";
+import { render } from "@testing-library/react";
+import "@testing-library/jest-dom";
+
+test("return null when should not render snapshot complete div", async () => {
+  // ARRANGE
+  const { container } = render(
+    <DashboardContext.Provider value={{ renderSnapshotCompleteDiv: false }}>
+      <SnapshotRenderComplete />
+    </DashboardContext.Provider>
+  );
+
+  // ASSERT
+  expect(container).toBeEmptyDOMElement();
+});
+
+test("return null when should not render snapshot complete div", async () => {
+  // ARRANGE
+  render(
+    <DashboardContext.Provider value={{ renderSnapshotCompleteDiv: true }}>
+      <SnapshotRenderComplete />
+    </DashboardContext.Provider>
+  );
+
+  // ASSERT
+  expect(document.querySelector("#snapshot-complete")).toBeTruthy();
+});
