264f5d63abfa233530dd6222946ab4317aaa9a9d
Mike Burgess <mike@turbot.com>
2022-10-01 11:31:42 +0100

    Validate grouping keys during parsing

--- ui/dashboard/src/hooks/useCheckGroupingConfig.ts
+++ ui/dashboard/src/hooks/useCheckGroupingConfig.ts
@@ -2,8 +2,20 @@ import {
   CheckDisplayGroup,
   CheckDisplayGroupType,
 } from "../components/dashboards/check/common";
-import { useSearchParams } from "react-router-dom";
 import { useMemo } from "react";
+import { useSearchParams } from "react-router-dom";
+
+const groupingKeys = [
+  "benchmark",
+  "control",
+  "dimension",
+  "reason",
+  "resource",
+  "result",
+  "severity",
+  "status",
+  "tag",
+];
 
 const useCheckGroupingConfig = () => {
   const [searchParams] = useSearchParams();
@@ -11,9 +23,17 @@ const useCheckGroupingConfig = () => {
     const rawGrouping = searchParams.get("grouping");
     if (rawGrouping) {
       const groupings: CheckDisplayGroup[] = [];
-      const groupingParts = rawGrouping.split(",");
+      const groupingParts = rawGrouping.split(",").filter((g) => !!g);
       for (const groupingPart of groupingParts) {
         const typeValueParts = groupingPart.split("|");
+        const groupingKey = typeValueParts[0];
+
+        // Is this a valid grouping key?
+        const isValid = groupingKeys.includes(groupingKey);
+        if (!isValid) {
+          throw new Error(`Unsupported grouping key ${groupingKey}`);
+        }
+
         if (typeValueParts.length > 1) {
           groupings.push({
             id: `${typeValueParts[0]}-${typeValueParts[1]}`,
