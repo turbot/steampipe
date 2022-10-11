e524fa68469101c38a506c23052ae569bad1b129
kai <kai@turbot.com>
2022-10-11 18:35:27 +0100

    fix control exporters

--- pkg/control/controldisplay/get_exporters.go
+++ pkg/control/controldisplay/get_exporters.go
@@ -1,12 +1,17 @@
 package controldisplay
 
-import "github.com/turbot/steampipe/pkg/export"
+import (
+	"github.com/turbot/steampipe/pkg/export"
+)
 
-func GetExporters() ([]export.Exporter, error) {
+// GetExporters returns 2 exporter maps, one keyed by name and one keyed by extension
+// this is needed because there is some complex logic to resolver control formatter which we do not want to replicate
+// so to avoid this we just build the maps and set these on the Export manager directly
+func GetExporters() (exportersByName, exportersByExtension map[string]export.Exporter, err error) {
 	formatResolver, err := NewFormatResolver()
 	if err != nil {
-		return nil, err
+		return nil, nil, err
 	}
-	exporters := formatResolver.controlExporters()
-	return exporters, nil
+	exportersByName, exportersByExtension = formatResolver.controlExporters()
+	return
 }
