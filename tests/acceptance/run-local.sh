0045d4fb5fe40134ac0f616c26793c282d08ce8e
Binaek Sarkar <binaek@turbot.com>
2022-10-19 17:05:58 +0530

    install steampipe before test scripts start

--- tests/acceptance/run-local.sh
+++ tests/acceptance/run-local.sh
@@ -6,7 +6,15 @@ MY_PATH="`( cd \"$MY_PATH\" && pwd )`"  # absolutized and normalized
 export STEAMPIPE_INSTALL_DIR=$(mktemp -d)
 export TIME_TO_QUERY=3                  # overriding since it takes more than 2secs to run locally
 export TZ=UTC
+export WD=$(mktemp -d)
 
-trap "code=$?;rm -rf $STEAMPIPE_INSTALL_DIR; exit $code" EXIT
+trap "cd -;code=$?;rm -rf $STEAMPIPE_INSTALL_DIR; exit $code" EXIT
+
+cd $WD
+echo "Working directory: $WD"
+# setup a steampipe installation
+echo "Install directory: $STEAMPIPE_INSTALL_DIR"
+steampipe query "select 1 as setup_complete"
+echo "Installation complete at $STEAMPIPE_INSTALL_DIR"
 
 source $MY_PATH/run.sh
