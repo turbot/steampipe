5e3e6e1c34929906cbfc8e75a4a5fd81cfdfab7c
kai <kai@turbot.com>
2022-10-26 12:19:17 +0100

    Update check query timeout to 240sec Update FDW

--- pkg/constants/db.go
+++ pkg/constants/db.go
@@ -20,20 +20,19 @@ const (
 var DatabaseListenAddresses = []string{"localhost", "127.0.0.1"}
 
 const (
-	DatabaseDefaultPort = 9193
-	// disabled
-	DatabaseDefaultQueryTimeout = 0
-	DatabaseSuperUser           = "root"
-	DatabaseUser                = "steampipe"
-	DatabaseName                = "steampipe"
-	DatabaseUsersRole           = "steampipe_users"
-	DefaultMaxConnections       = 5
+	DatabaseDefaultPort              = 9193
+	DatabaseDefaultCheckQueryTimeout = 0
+	DatabaseSuperUser                = "root"
+	DatabaseUser                     = "steampipe"
+	DatabaseName                     = "steampipe"
+	DatabaseUsersRole                = "steampipe_users"
+	DefaultMaxConnections            = 5
 )
 
 // constants for installing db and fdw images
 const (
 	DatabaseVersion = "14.2.0"
-	FdwVersion      = "1.4.2"
+	FdwVersion      = "1.4.3-rc.0"
 
 	// PostgresImageRef is the OCI Image ref for the database binaries
 	PostgresImageRef    = "us-docker.pkg.dev/steampipe/steampipe/db:14.2.0"
