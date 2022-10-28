eef615391d8c5033eabf796cae7166bb9c69e17c
Puskar Basu <puskar@turbot.com>
2022-10-28 13:20:07 +0530

    Update metaquery command .auto_complete to .autocomplete. Closes #2616

--- pkg/constants/metaquery_commands.go
+++ pkg/constants/metaquery_commands.go
@@ -20,7 +20,7 @@ const (
 	CmdSearchPath       = ".search_path"        // Set or show search-path
 	CmdSearchPathPrefix = ".search_path_prefix" // set search path prefix
 	CmdCache            = ".cache"              // cache control
-	CmdAutoComplete     = ".auto_complete"      // enable or disable auto complete
+	CmdAutoComplete     = ".autocomplete"       // enable or disable auto complete
 )
 
 // ArgFromMetaquery converts a metaquery of form '.header' into the config argument used to set the mode, i.e. 'header'
