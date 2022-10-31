55a3868f613d15fdd9d1193767e7233e5146cd2e
Binaek Sarkar <binaek@turbot.com>
2022-10-31 14:26:15 +0530

    moving

--- pkg/utils/wsl.go
+++ pkg/utils/wsl.go
@@ -12,11 +12,11 @@ var wslValue *bool = nil
 // IsWSL :: detects whether app is running in WSL environment
 // refer to: https://github.com/Microsoft/WSL/issues/423#issuecomment-679190758
 func IsWSL() (bool, error) {
-	if wslValue != nil {
-		return *wslValue, nil
-	}
 	if runtime.GOOS != "linux" {
 		return false, nil
+	}
+	if wslValue != nil {
+		return *wslValue, nil
 	}
 	// https://github.com/Microsoft/WSL/issues/2299#issuecomment-361366982
 	osReleaseContent, err := os.ReadFile("/proc/version")
