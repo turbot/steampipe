4b2aab61b3bfdbd137ca53528f9e17f27e8a3165
Puskar Basu <45908484+pskrbasu@users.noreply.github.com>
2022-11-03 15:46:12 +0530

    Fix and re-enable docker tests in release workflow

--- tests/dockertesting/debian/run-tests.sh
+++ tests/dockertesting/debian/run-tests.sh
@@ -15,13 +15,24 @@ git checkout $1
 git branch
 
 # declare the test file names
-declare -a arr=("migration" "service_and_plugin" "search_path" "chaos_and_query" "dynamic_schema" "cache" "mod_install" "mod" "check" "performance" "exit_codes" "force_stop")
+declare -a arr=("migration" "service_and_plugin" "search_path" "chaos_and_query" "dynamic_schema" "cache" "mod_install" "mod" "check" "performance" "exit_codes")
+declare -i failure_count=0
 
 # run test suite
 for i in "${arr[@]}"
 do
+  echo ""
   echo ">>>>> running $i.bats"
   ./tests/acceptance/run.sh $i.bats
+  failure_count+=$?
 done
 
-echo "test run complete"
+# check if all tests passed
+echo $failure_count
+if [[ $failure_count -eq 0 ]]; then
+  echo "test run successful"
+  exit 0
+else
+  echo "test run failed"
+  exit 1
+fi
