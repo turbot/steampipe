23fb4c7a9906af6603e02899a0e66b8dae87c07b
Mike Burgess <mike@turbot.com>
2022-11-07 10:07:41 +0000

    Remove search_path, and panel documentation/sql/source_definition from snapshots exported from the dashboard UI #2577

---
+++ ui/dashboard/src/utils/snapshot.ts
@@ -0,0 +1,32 @@
+import { PanelDefinition } from "../types";
+
+const stripSnapshotDataForExport = (snapshot) => {
+  if (!snapshot) {
+    return {};
+  }
+
+  switch (snapshot.schema_version) {
+    case "20220614":
+    case "20220929":
+      const { panels, search_path, ...rest } = snapshot;
+      const newPanels = {};
+      for (const [name, panel] of Object.entries(panels)) {
+        const { documentation, sql, source_definition, ...rest } =
+          panel as PanelDefinition;
+        newPanels[name] = {
+          ...rest,
+        };
+      }
+
+      return {
+        ...rest,
+        panels: newPanels,
+      };
+    default:
+      throw new Error(
+        `Unsupported dashboard event schema ${snapshot.schema_version}`
+      );
+  }
+};
+
+export { stripSnapshotDataForExport };
