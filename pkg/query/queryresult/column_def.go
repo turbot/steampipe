67396749898262ecf998ddc95d912c84fa9cbbe9
kai <kai@turbot.com>
2022-11-08 14:19:28 +0000

    Remove ScanType from Snapshot panel data.

--- pkg/query/queryresult/column_def.go
+++ pkg/query/queryresult/column_def.go
@@ -4,7 +4,7 @@ import "reflect"
 
 // ColumnDef is a struct used to store column information from query results
 type ColumnDef struct {
-	Name     string `json:"name"`
-	DataType string `json:"data_type"`
-	ScanType reflect.Type
+	Name     string       `json:"name"`
+	DataType string       `json:"data_type"`
+	ScanType reflect.Type `json:"-"`
 }
