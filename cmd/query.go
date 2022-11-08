69d95f38ce74aaefa8e6932950e5e5b8ca79f678
kai <kai@turbot.com>
2022-11-08 21:42:38 +0000

    Using query command --export flag raises an error that it cannot be used in interactive mode, even when not in interactive mode. Closes #2707

--- cmd/query.go
+++ cmd/query.go
@@ -153,7 +153,7 @@ func validateQueryArgs(ctx context.Context, args []string) error {
 	if interactiveMode && (viper.IsSet(constants.ArgSnapshot) || viper.IsSet(constants.ArgShare)) {
 		return fmt.Errorf("cannot share snapshots in interactive mode")
 	}
-	if len(viper.GetStringSlice(constants.ArgExport)) > 0 {
+	if interactiveMode && len(viper.GetStringSlice(constants.ArgExport)) > 0 {
 		return fmt.Errorf("cannot export query results in interactive mode")
 	}
 	// if share or snapshot args are set, there must be a query specified
