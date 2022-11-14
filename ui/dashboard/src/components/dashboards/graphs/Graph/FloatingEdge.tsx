fd1e7f208779fd704b35641b23cc72dc98ffa6c9
Mike Burgess <mike@turbot.com>
2022-11-14 22:49:46 +0000

    Tweak edge label underline spacing

--- ui/dashboard/src/components/dashboards/graphs/Graph/FloatingEdge.tsx
+++ ui/dashboard/src/components/dashboards/graphs/Graph/FloatingEdge.tsx
@@ -45,7 +45,7 @@ const FloatingEdge = ({
       className={classNames(
         "block p-px italic max-w-[70px] text-sm text-center text-wrap leading-tight line-clamp-2",
         row_data && row_data.properties
-          ? "-mt-1 underline decoration-dashed decoration-2 underline-offset-2 decoration-black-scale-3 cursor-pointer"
+          ? "-mt-1 underline decoration-dashed decoration-2 underline-offset-3 decoration-black-scale-3 cursor-pointer"
           : null
       )}
       style={{ color, opacity: labelOpacity }}
