6204553087b6ff764f31dc10bb0f273bb371cb55
Binaek Sarkar <binaek@turbot.com>
2022-11-23 22:53:38 +0530

    just the one

--- pkg/constants/postgresql_conf.go
+++ pkg/constants/postgresql_conf.go
@@ -59,17 +59,9 @@ log_disconnections=on
 # troubleshooting with users.
 log_min_duration_statement=5000
 
-# Increasing the memory settings helps PostgreSQL to not
+# Increasing the locks per transaction helps PostgreSQL to not
 # run out of available memory when working with large plugins
 # or aggregators with a large number of sub connections (or both)
-effective_cache_size=64kB
-full_page_writes=off
-maintenance_work_mem=1024kB
-random_page_cost=0.01
-seq_page_cost=0.01
-temp_buffers=800kB
-wal_buffers=32kB
-work_mem=64kB
 max_locks_per_transaction = 2048 
 
 `
