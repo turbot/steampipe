0037c49184b6d280251110d9d8308191087cf7df
kai <kai@turbot.com>
2022-11-24 22:42:34 +0000

    handle with array conversion

--- pkg/dashboard/dashboardexecute/resolved_runtime_dependency.go
+++ pkg/dashboard/dashboardexecute/resolved_runtime_dependency.go
@@ -2,6 +2,7 @@ package dashboardexecute
 
 import (
 	"fmt"
+	"github.com/turbot/steampipe/pkg/type_conversion"
 	"sync"
 
 	"github.com/turbot/go-kit/helpers"
@@ -42,6 +43,10 @@ func (d *ResolvedRuntimeDependency) Resolve() (bool, error) {
 	val, err := d.getValueFunc(d.dependency.SourceResource.GetUnqualifiedName())
 	if err != nil {
 		return false, err
+	}
+	// if this is an array, wrap in an array
+	if d.dependency.IsArray {
+		val = type_conversion.AnySliceToTypedSlice([]any{val})
 	}
 	d.value = val
 
