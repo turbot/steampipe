a5c74fd993e6f8102397c2262d62c9f4ad85114c
Binaek Sarkar <binaek@turbot.com>
2022-11-25 17:38:14 +0530

    update unit test

--- pkg/workspace/workspace_test.go
+++ pkg/workspace/workspace_test.go
@@ -516,7 +516,7 @@ func executeWorkspaceLoadTest(t *testing.T, name string, test loadWorkspaceTest,
 	if !actualWorkspace.Mod.Equals(expectedWorkspace.Mod) {
 		fmt.Printf("")
 
-		t.Errorf("Test: '%s'' FAILED", name)
+		t.Errorf("Test: '%s'' FAILED. Expected: %v - Actual %v", name, actualWorkspace.Mod, expectedWorkspace.Mod)
 	}
 }
 
