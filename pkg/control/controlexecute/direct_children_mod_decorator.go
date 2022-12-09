f790db2970ac2aa434a0f107812adb4892a5ca83
kai <kai@turbot.com>
2022-12-09 13:04:03 +0000

    Fix DirectChildrenModDecorator

--- pkg/control/controlexecute/direct_children_mod_decorator.go
+++ pkg/control/controlexecute/direct_children_mod_decorator.go
@@ -1,21 +1,17 @@
 package controlexecute
 
 import (
-	"github.com/hashicorp/hcl/v2"
 	"github.com/turbot/steampipe/pkg/steampipeconfig/modconfig"
-	"github.com/zclconf/go-cty/cty"
 )
-
-// TODO use mod as embedded struct
 
 // DirectChildrenModDecorator is a struct used to wrap a Mod but modify the results of GetChildren to only return
 // immediate mod children (as opposed to all resources in dependency mods as well)
 // This is needed when running 'check all' for a mod which has dependency mopds'
 type DirectChildrenModDecorator struct {
-	modconfig.ModTreeItemBase
-	Mod *modconfig.Mod
+	*modconfig.Mod
 }
 
+// override GetChildren
 func (r DirectChildrenModDecorator) GetChildren() []modconfig.ModTreeItem {
 	var res []modconfig.ModTreeItem
 	for _, child := range r.Mod.GetChildren() {
@@ -24,54 +20,4 @@ func (r DirectChildrenModDecorator) GetChildren() []modconfig.ModTreeItem {
 		}
 	}
 	return res
-}
-
-// GetMetadata implements ResourceWithMetadata
-func (r DirectChildrenModDecorator) GetMetadata() *modconfig.ResourceMetadata {
-	return r.Mod.GetMetadata()
-}
-
-// SetMetadata implements ResourceWithMetadata
-func (r DirectChildrenModDecorator) SetMetadata(metadata *modconfig.ResourceMetadata) {
-	r.Mod.SetMetadata(metadata)
-}
-
-// SetAnonymous implements SetAnonymous
-func (r DirectChildrenModDecorator) SetAnonymous(block *hcl.Block) {
-	r.Mod.SetAnonymous(block)
-}
-
-// IsAnonymous implements ResourceWithMetadata
-func (r DirectChildrenModDecorator) IsAnonymous() bool {
-	return r.Mod.IsAnonymous()
-}
-
-// AddReference implements ResourceWithMetadata
-func (r DirectChildrenModDecorator) AddReference(ref *modconfig.ResourceReference) {
-	r.Mod.AddReference(ref)
-}
-
-// GetReferences implements ResourceWithMetadata
-func (r DirectChildrenModDecorator) GetReferences() []*modconfig.ResourceReference {
-	return r.Mod.GetReferences()
-}
-
-// GetDisplay implements DashboardLeafNode
-func (r DirectChildrenModDecorator) GetDisplay() string {
-	return ""
-}
-
-// GetType implements DashboardLeafNode
-func (r DirectChildrenModDecorator) GetType() string {
-	return ""
-}
-
-// GetWidth implements DashboardLeafNode
-func (r DirectChildrenModDecorator) GetWidth() int {
-	return 0
-}
-
-// CtyValue implements CtyValueProvider
-func (r DirectChildrenModDecorator) CtyValue() (cty.Value, error) {
-	return r.Mod.CtyValue()
 }
