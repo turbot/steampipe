4daacff0a84babdbe20c79e0ef7d139cb9f73602
Puskar Basu <puskar@turbot.com>
2023-01-06 19:50:54 +0530

    fix

--- tests/acceptance/test_files/json_patch.sh
+++ tests/acceptance/test_files/json_patch.sh
@@ -10,9 +10,9 @@ for i in $patch_keys; do
   path=$(echo $patch_file | jq -c ".[${i}]" | jq ".path")
   value=$(echo $patch_file | jq -c ".[${i}]" | jq ".value")
 
-  # ignore the diff of paths 'end_time', 'start_time' and 'search_path',
+  # ignore the diff of paths 'end_time', 'start_time' and 'schema_version',
   # print the rest
-  if [[ $op != '"test"' ]] && [[ $path != '"/end_time"' ]] && [[ $path != '"/start_time"' ]] && [[ $path != *'"/search_path'* ]]; then
+  if [[ $op != '"test"' ]] && [[ $path != '"/end_time"' ]] && [[ $path != '"/start_time"' ]] && [[ $path != '"/schema_version"' ]]; then
     if [[ $op == '"remove"' ]]; then
       echo "key: $path"
       echo "expected: $value"
