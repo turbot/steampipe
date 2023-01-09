0c20707b46075ff1199b8da36c6540d212e2f3c0
kai <kai@turbot.com>
2023-01-09 11:12:29 +0000

    remove unneeded functions

--- pkg/control/controlexecute/direct_children_mod_decorator.go
+++ pkg/control/controlexecute/direct_children_mod_decorator.go
@@ -1,9 +1,7 @@
 package controlexecute
 
 import (
-	"github.com/hashicorp/hcl/v2"
 	"github.com/turbot/steampipe/pkg/steampipeconfig/modconfig"
-	"github.com/zclconf/go-cty/cty"
 )
 
 // DirectChildrenModDecorator is a struct used to wrap a Mod but modify the results of GetChildren to only return
@@ -13,7 +11,7 @@ type DirectChildrenModDecorator struct {
 	*modconfig.Mod
 }
 
-// override GetChildren
+// GetChildren is overridden
 func (r DirectChildrenModDecorator) GetChildren() []modconfig.ModTreeItem {
 	var res []modconfig.ModTreeItem
 	for _, child := range r.Mod.GetChildren() {
@@ -24,54 +22,9 @@ func (r DirectChildrenModDecorator) GetChildren() []modconfig.ModTreeItem {
 	return res
 }
 
-// GetDocumentation implements DashboardLeafNode, ModTreeItem
+// GetDocumentation implements DashboardLeafNode
 func (r DirectChildrenModDecorator) GetDocumentation() string {
 	return r.Mod.GetDocumentation()
-}
-
-func (r DirectChildrenModDecorator) GetMod() *modconfig.Mod {
-	return r.Mod
-}
-
-// BlockType implements HclResource
-func (r DirectChildrenModDecorator) BlockType() string {
-	return r.Mod.BlockType()
-
-}
-
-// CtyValue implements HclResource
-func (r DirectChildrenModDecorator) CtyValue() (cty.Value, error) {
-	return r.Mod.CtyValue()
-}
-
-// GetDeclRange implements HclResource
-func (r DirectChildrenModDecorator) GetDeclRange() *hcl.Range {
-	return &r.Mod.DeclRange
-}
-
-// OnDecoded implements HclResource
-func (r DirectChildrenModDecorator) OnDecoded(block *hcl.Block, resourceMapProvider modconfig.ResourceMapsProvider) hcl.Diagnostics {
-	return nil
-}
-
-// GetMetadata implements ResourceWithMetadata
-func (r DirectChildrenModDecorator) GetMetadata() *modconfig.ResourceMetadata {
-	return r.Mod.GetMetadata()
-}
-
-// SetMetadata implements ResourceWithMetadata
-func (r DirectChildrenModDecorator) SetMetadata(metadata *modconfig.ResourceMetadata) {
-	r.Mod.SetMetadata(metadata)
-}
-
-// SetAnonymous implements SetAnonymous
-func (r DirectChildrenModDecorator) SetAnonymous(block *hcl.Block) {
-	r.Mod.SetAnonymous(block)
-}
-
-// IsAnonymous implements ResourceWithMetadata
-func (r DirectChildrenModDecorator) IsAnonymous() bool {
-	return r.Mod.IsAnonymous()
 }
 
 // GetDisplay implements DashboardLeafNode
