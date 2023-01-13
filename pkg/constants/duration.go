2c7c6a3955dc459b2fd717ae761e824008a90dcf
Binaek Sarkar <binaek@turbot.com>
2023-01-13 20:04:10 +0530

    increase db connection timeout

--- pkg/constants/duration.go
+++ pkg/constants/duration.go
@@ -4,7 +4,7 @@ import "time"
 
 var (
 	DashboardServiceStartTimeout = 30 * time.Second
-	DBConnectionTimeout          = 5 * time.Second
+	DBConnectionTimeout          = 30 * time.Second
 	DBConnectionRetryBackoff     = 200 * time.Millisecond
 	ServicePingInterval          = 50 * time.Millisecond
 )
