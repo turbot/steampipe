24e5457b57c947dc4a6413bfa573292e866e6e17
Binaek Sarkar <binaek@turbot.com>
2023-01-17 14:34:37 +0530

    corrections

--- pkg/task/display.go
+++ pkg/task/display.go
@@ -5,14 +5,9 @@ import (
 	"fmt"
 	"log"
 	"os"
-	"sort"
 
-	SemVer "github.com/Masterminds/semver"
-	"github.com/fatih/color"
-	"github.com/olekukonko/tablewriter"
 	"github.com/spf13/cobra"
 	"github.com/turbot/go-kit/files"
-	"github.com/turbot/steampipe/pkg/constants"
 	"github.com/turbot/steampipe/pkg/error_helpers"
 	"github.com/turbot/steampipe/pkg/filepaths"
 	"github.com/turbot/steampipe/pkg/plugin"
@@ -22,12 +17,6 @@ import (
 const (
 	AvailableVersionsCacheStructVersion = 20221121
 )
-
-type AvailableVersionCache struct {
-	StructVersion uint32                               `json:"struct_version"`
-	CliCache      *CLIVersionCheckResponse             `json:"cli_version"`
-	PluginCache   map[string]plugin.VersionCheckReport `json:"plugin_version"`
-}
 
 func (r *Runner) saveAvailableVersions(cli *CLIVersionCheckResponse, plugin map[string]plugin.VersionCheckReport) error {
 	utils.LogTime("Runner.saveNotifications start")
@@ -59,7 +48,7 @@ func (r *Runner) hasAvailableVersion() bool {
 	return files.FileExists(filepaths.AvailableVersionsFilePath())
 }
 
-func (r *Runner) getAvailableVersions() (*AvailableVersionCache, error) {
+func (r *Runner) loadCachedVersions() (*AvailableVersionCache, error) {
 	utils.LogTime("Runner.getNotifications start")
 	defer utils.LogTime("Runner.getNotifications end")
 	f, err := os.Open(filepaths.AvailableVersionsFilePath())
@@ -97,136 +86,19 @@ func (r *Runner) displayNotifications(cmd *cobra.Command, cmdArgs []string) erro
 		return nil
 	}
 
-	availableVersions, err := r.getAvailableVersions()
+	cachedVersions, err := r.loadCachedVersions()
 	if err != nil {
 		return err
 	}
 
-	cliLines, err := cliNotificationMessage(availableVersions.CliCache)
+	table, err := cachedVersions.asTable()
 	if err != nil {
 		return err
 	}
-
-	pluginLines := pluginNotificationMessage(availableVersions.PluginCache)
-
-	// convert notificationLines into an array of arrays
-	// since that's what our table renderer expects
-	var notificationTable = utils.Map(append(cliLines, pluginLines...), func(line string) []string {
-		return []string{line}
-	})
-
-	table := tablewriter.NewWriter(os.Stdout)
-	table.SetHeader([]string{})                // no headers please
-	table.SetAlignment(tablewriter.ALIGN_LEFT) // we align to the left
-	table.SetAutoWrapText(false)               // let's not wrap the text
-	table.SetBorder(true)                      // there needs to be a border to provide the dialog feel
-	table.AppendBulk(notificationTable)        // Add Bulk Data
 
 	fmt.Println()
 	table.Render()
 	fmt.Println()
 
 	return nil
-}
-
-func cliNotificationMessage(info *CLIVersionCheckResponse) ([]string, error) {
-	if info == nil {
-		return nil, nil
-	}
-
-	if info.NewVersion == "" {
-		return nil, nil
-	}
-
-	newVersion, err := SemVer.NewVersion(info.NewVersion)
-	if err != nil {
-		return nil, err
-	}
-	currentVersion, err := SemVer.NewVersion(currentVersion)
-
-	if err != nil {
-		fmt.Println(fmt.Errorf("there's something wrong with the Current Version"))
-		fmt.Println(err)
-	}
-
-	if newVersion.GreaterThan(currentVersion) {
-		var downloadURLColor = color.New(color.FgYellow)
-		var notificationLines = []string{
-			"",
-			fmt.Sprintf("A new version of Steampipe is available! %s → %s", constants.Bold(currentVersion), constants.Bold(newVersion)),
-			fmt.Sprintf("You can update by downloading from %s", downloadURLColor.Sprint("https://steampipe.io/downloads")),
-			"",
-		}
-		return notificationLines, nil
-	}
-	return nil, nil
-}
-
-func pluginNotificationMessage(updateReport map[string]plugin.VersionCheckReport) []string {
-	var pluginsToUpdate []plugin.VersionCheckReport
-
-	for _, r := range updateReport {
-		if skip, _ := plugin.SkipUpdate(r); !skip {
-			pluginsToUpdate = append(pluginsToUpdate, r)
-		}
-	}
-	notificationLines := []string{}
-	if len(pluginsToUpdate) > 0 {
-		notificationLines = getPluginNotificationLines(pluginsToUpdate)
-	}
-	return notificationLines
-}
-
-func getPluginNotificationLines(reports []plugin.VersionCheckReport) []string {
-	var notificationLines = []string{
-		"",
-		"Updated versions of the following plugins are available:",
-		"",
-	}
-	longestNameLength := 0
-	for _, report := range reports {
-		thisName := report.ShortName()
-		if len(thisName) > longestNameLength {
-			longestNameLength = len(thisName)
-		}
-	}
-
-	// sort alphabetically
-	sort.Slice(reports, func(i, j int) bool {
-		return reports[i].ShortName() < reports[j].ShortName()
-	})
-
-	for _, report := range reports {
-		thisName := report.ShortName()
-		line := ""
-		if len(report.Plugin.Version) == 0 {
-			format := fmt.Sprintf("  %%-%ds @ %%-10s  →  %%10s", longestNameLength)
-			line = fmt.Sprintf(
-				format,
-				thisName,
-				report.CheckResponse.Stream,
-				constants.Bold(report.CheckResponse.Version),
-			)
-		} else {
-			version := report.CheckResponse.Version
-			format := fmt.Sprintf("  %%-%ds @ %%-10s       %%10s → %%-10s", longestNameLength)
-			// an arm64 binary of the plugin might exist for the same version
-			if report.Plugin.Version == report.CheckResponse.Version {
-				version = fmt.Sprintf("%s (arm64)", version)
-			}
-			line = fmt.Sprintf(
-				format,
-				thisName,
-				report.CheckResponse.Stream,
-				constants.Bold(report.Plugin.Version),
-				constants.Bold(version),
-			)
-		}
-		notificationLines = append(notificationLines, line)
-	}
-	notificationLines = append(notificationLines, "")
-	notificationLines = append(notificationLines, fmt.Sprintf("You can update by running %s", constants.Bold("steampipe plugin update --all")))
-	notificationLines = append(notificationLines, "")
-
-	return notificationLines
 }
