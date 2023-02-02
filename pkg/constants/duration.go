32189ee0ad0daf9c02f29a83e3f1349a83089ea1
Binaek Sarkar <binaek@turbot.com>
2023-02-02 16:26:30 +0530

    adding constants for timeout

--- pkg/constants/duration.go
+++ pkg/constants/duration.go
@@ -5,7 +5,8 @@ import "time"
 var (
 	DashboardServiceStartTimeout = 30 * time.Second
 	DBConnectionTimeout          = 30 * time.Second
-	DBRecoveryWaitTimeout        = 5 * time.Minute
 	DBConnectionRetryBackoff     = 200 * time.Millisecond
+	DBRecoveryTimeout            = 5 * time.Minute
+	DBRecoveryRetryBackoff       = 200 * time.Millisecond
 	ServicePingInterval          = 50 * time.Millisecond
 )
