1704f3339a768a7f1fe50c867bb5a1b6160b376b
Puskar Basu <puskar@turbot.com>
2023-02-28 18:40:08 +0530

    exclude query column for steampipe_query table

--- pkg/utils/string_slice.go
+++ pkg/utils/string_slice.go
@@ -17,3 +17,9 @@ func UnquoteStringArray(stringArray []string) []string {
 func StringSlicesEqual(l, r []string) bool {
 	return strings.Join(l, ",") == strings.Join(r, ",")
 }
+
+// RemoveElementFromSlice takes a slice of strings and an index to remove,
+// and returns a new slice with the specified element removed.
+func RemoveElementFromSlice(slice []string, s int) []string {
+	return append(slice[:s], slice[s+1:]...)
+}
