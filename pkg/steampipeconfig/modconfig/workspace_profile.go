7741f856dde5117746f7ddf3c967ad7e70af86dd
Binaek Sarkar <binaek@turbot.com>
2023-03-15 19:18:31 +0530

    add to workspace profile

--- pkg/steampipeconfig/modconfig/workspace_profile.go
+++ pkg/steampipeconfig/modconfig/workspace_profile.go
@@ -32,6 +32,10 @@ type WorkspaceProfile struct {
 	Cache             *bool             `hcl:"cache" cty:"cache"`
 	CacheTTL          *int              `hcl:"cache_ttl" cty:"cache_ttl"`
 	Base              *WorkspaceProfile `hcl:"base"`
+	SearchPath        *string           `hcl:"search_path" cty:"search_path"`
+	SearchPathPrefix  *string           `hcl:"search_path_prefix" cty:"search_path_prefix"`
+	Watch             *bool             `hcl:"watch" cty:"watch"`
+	MaxParallel       *int              `hcl:"max_parallel" cty:"max-parallel"`
 
 	// options
 	QueryOptions     *options.Query                     `cty:"query-options"`
@@ -211,6 +215,9 @@ func (p *WorkspaceProfile) ConfigMap(cmd *cobra.Command) map[string]interface{} 
 	}
 	if cmd.Name() == constants.CmdNameDashboard && p.DashboardOptions != nil {
 		res.PopulateConfigMapForOptions(p.DashboardOptions)
+	}
+	if commandName == "query" && p.QueryOptions != nil {
+		res.PopulateConfigMapForOptions(p.QueryOptions)
 	}
 
 	return res
