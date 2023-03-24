453ac8a9f297ba0546489dba24c762088a591124
Binaek Sarkar <binaek@turbot.com>
2023-03-24 17:16:43 +0530

    using stringer

--- pkg/cmdconfig/env_var_type.go
+++ pkg/cmdconfig/env_var_type.go
@@ -8,16 +8,4 @@ const (
 	Bool
 )
 
-func (t EnvVarType) String() string {
-	switch t {
-	case String:
-		return "string"
-	case Bool:
-		return "boolean"
-	case Int:
-		return "int"
-	}
-	return "unknown"
-}
-
 //go:generate go run golang.org/x/tools/cmd/stringer -type=EnvVarType
