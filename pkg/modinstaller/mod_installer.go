a59120e8bb322d9cbd830b92dd75f7a0b3c5ba0c
Binaek Sarkar <binaek@turbot.com>
2023-05-09 17:37:34 +0530

    splitting out stuff

--- pkg/modinstaller/mod_installer.go
+++ pkg/modinstaller/mod_installer.go
@@ -1,7 +1,6 @@
 package modinstaller
 
 import (
-	"bytes"
 	"context"
 	"fmt"
 	"log"
@@ -11,7 +10,6 @@ import (
 
 	"github.com/Masterminds/semver/v3"
 	git "github.com/go-git/go-git/v5"
-	"github.com/hashicorp/hcl/v2/hclwrite"
 	"github.com/otiai10/copy"
 	"github.com/spf13/viper"
 	"github.com/turbot/steampipe/pkg/constants"
@@ -23,7 +21,6 @@ import (
 	"github.com/turbot/steampipe/pkg/steampipeconfig/versionmap"
 	"github.com/turbot/steampipe/pkg/utils"
 	"github.com/turbot/steampipe/sperr"
-	"github.com/zclconf/go-cty/cty"
 )
 
 type ModInstaller struct {
@@ -228,188 +225,15 @@ func (i *ModInstaller) InstallWorkspaceDependencies(ctx context.Context) (err er
 	}
 
 	//  now safe to save the mod file
-	if len(i.mods) > 0 {
-		if err := i.updateModFile(); err != nil {
-			return err
-		}
+	if err := i.updateModFile(); err != nil {
+		return err
 	}
+
 	if !workspaceMod.HasDependentMods() {
 		// there are no dependencies - delete the cache
 		i.installData.Lock.Delete()
 	}
 	return nil
-}
-
-// calculates changes required in mod.sp to reflect uninstalls
-func (i *ModInstaller) calcChangesForUninstall(oldRequire *modconfig.Require, newRequire *modconfig.Require) ChangeSet {
-	changes := ChangeSet{}
-	for _, requiredMod := range oldRequire.Mods {
-		// check if this mod is still a dependency
-		if modInNew := newRequire.GetModDependency(requiredMod.Name); modInNew == nil {
-			changes = append(changes, &Change{
-				Operation:   Delete,
-				OffsetStart: requiredMod.DefRange.Start.Byte,
-				OffsetEnd:   requiredMod.BodyRange.End.Byte,
-			})
-		}
-	}
-	return changes
-}
-
-// calculates changes required in mod.sp to reflect new installs
-func (i *ModInstaller) calcChangesForInstall(oldRequire *modconfig.Require, newRequire *modconfig.Require) ChangeSet {
-	modsToAdd := []*modconfig.ModVersionConstraint{}
-	for _, requiredMod := range newRequire.Mods {
-		if modInOld := oldRequire.GetModDependency(requiredMod.Name); modInOld == nil {
-			modsToAdd = append(modsToAdd, requiredMod)
-		}
-	}
-
-	if len(modsToAdd) == 0 {
-		// an empty changeset
-		return ChangeSet{}
-	}
-
-	// create the HCL serialization for the mod blocks which needs to be placed
-	// in the require block
-	f := hclwrite.NewEmptyFile()
-	rootBody := f.Body()
-	for _, modToAdd := range modsToAdd {
-		rootBody.AppendBlock(i.createNewModRequireBlock(modToAdd))
-	}
-
-	return ChangeSet{
-		&Change{
-			Operation:   Insert,
-			OffsetStart: oldRequire.BodyRange.End.Byte - 1,
-			Content:     f.Bytes(),
-		},
-	}
-}
-
-// calculates the changes required in mod.sp to reflect updates
-func (i *ModInstaller) calcChangesForUpdate(oldRequire *modconfig.Require, newRequire *modconfig.Require) ChangeSet {
-	changes := ChangeSet{}
-	for _, requiredMod := range oldRequire.Mods {
-		modInUpdated := newRequire.GetModDependency(requiredMod.Name)
-		if modInUpdated == nil {
-			continue
-		}
-		if modInUpdated.VersionString != requiredMod.VersionString {
-			changes = append(changes, &Change{
-				Operation:   Replace,
-				OffsetStart: requiredMod.VersionRange.Start.Byte,
-				OffsetEnd:   requiredMod.VersionRange.End.Byte,
-				Content:     []byte(fmt.Sprintf("version = \"%s\"", modInUpdated.VersionString)),
-			})
-		}
-	}
-	return changes
-}
-
-// loads the contents of the mod.sp file and wraps it with a thin wrapper
-// to assist in byte sequence manipulation
-func (i *ModInstaller) loadModFileBytes() (*ByteSequence, error) {
-	modFileBytes, err := os.ReadFile(filepaths.ModFilePath(i.workspaceMod.ModPath))
-	if err != nil {
-		return nil, err
-	}
-	return NewByteSequence(modFileBytes), nil
-}
-
-// creates a new "mod" block which can be written as part of the "require" block in mod.sp
-func (i *ModInstaller) createNewModRequireBlock(modVersion *modconfig.ModVersionConstraint) *hclwrite.Block {
-	modRequireBlock := hclwrite.NewBlock("mod", []string{modVersion.Name})
-	modRequireBlock.Body().SetAttributeValue("version", cty.StringVal(modVersion.VersionString))
-	return modRequireBlock
-}
-
-// updates the 'require' block in 'mod.sp'
-func (i *ModInstaller) updateModFile() error {
-	contents, err := i.loadModFileBytes()
-	if err != nil {
-		return err
-	}
-
-	oldRequire := i.oldRequire
-	newRequire := i.workspaceMod.Require
-
-	// fill these requires in with empty requires
-	// so that we don't have to do nil checks everywhere
-	// from here on out - if it's empty - it's nil
-
-	if oldRequire == nil {
-		// use an empty require as the old requirements
-		oldRequire = modconfig.NewRequire()
-	}
-	if newRequire == nil {
-		// use a stub require instance
-		newRequire = modconfig.NewRequire()
-	}
-
-	changes := MergedChangeSet()
-	if newRequire.Empty() && !oldRequire.Empty() {
-		// if the new set of requires is empty - just remove the require block
-		changes = NewChangeSet(&Change{
-			Operation:   Delete,
-			OffsetStart: oldRequire.DeclRange.Start.Byte,
-			OffsetEnd:   oldRequire.BodyRange.End.Byte,
-		})
-	} else if !newRequire.Empty() && oldRequire.Empty() {
-		// if the new require is not empty, but the old one is
-		// add a new require block with the new stuff
-		// by generating the HCL string that goes in
-		f := hclwrite.NewEmptyFile()
-
-		var body *hclwrite.Body
-		var insertOffset int
-
-		if oldRequire.BodyRange.Start.Byte != 0 {
-			// this means that there is a require block
-			// but is probably empty
-			body = f.Body()
-			insertOffset = oldRequire.BodyRange.End.Byte - 1
-		} else {
-			// we don't have a require block at all
-			// let's create one to append to
-			body = f.Body().AppendNewBlock("require", nil).Body()
-			insertOffset = i.workspaceMod.DeclRange.End.Byte - 1
-		}
-
-		for _, mvc := range newRequire.Mods {
-			newBlock := i.createNewModRequireBlock(mvc)
-			body.AppendBlock(newBlock)
-		}
-
-		// prefix and suffix with new lines
-		// this is so that we can handle empty blocks
-		// which do not have newlines
-		buffer := bytes.NewBuffer([]byte{'\n'})
-		buffer.Write(f.Bytes())
-		buffer.WriteByte('\n')
-
-		changes = NewChangeSet(&Change{
-			Operation:   Insert,
-			OffsetStart: insertOffset,
-			Content:     buffer.Bytes(),
-		})
-	} else if !newRequire.Empty() && !oldRequire.Empty() {
-		// calculate the changes
-		uninstallChanges := i.calcChangesForUninstall(oldRequire, newRequire)
-		installChanges := i.calcChangesForInstall(oldRequire, newRequire)
-		updateChanges := i.calcChangesForUpdate(oldRequire, newRequire)
-
-		changes = MergedChangeSet(
-			uninstallChanges,
-			installChanges,
-			updateChanges,
-		)
-	}
-
-	contents.ApplyChanges(changes)
-	contents.Apply(hclwrite.Format)
-
-	return os.WriteFile(filepaths.ModFilePath(i.workspaceMod.ModPath), contents.Bytes(), 0644)
 }
 
 func (i *ModInstaller) GetModList() string {
