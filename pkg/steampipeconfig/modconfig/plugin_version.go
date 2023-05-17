9844bb2d572c409f79e98bfe7ca6884a0e00a670
Binaek Sarkar <binaek@turbot.com>
2023-05-17 18:41:05 +0530

    disable plugin min_version warning

--- pkg/steampipeconfig/modconfig/plugin_version.go
+++ pkg/steampipeconfig/modconfig/plugin_version.go
@@ -53,11 +53,14 @@ func (p *PluginVersion) Initialise(block *hcl.Block) hcl.Diagnostics {
 			return diags
 		}
 		// raise deprecation warning
-		diags = append(diags, &hcl.Diagnostic{
-			Severity: hcl.DiagWarning,
-			Summary:  fmt.Sprintf("Property 'version' is deprecated - use 'min_version' instead, in plugin '%s' require block", p.RawName),
-			Subject:  &p.DeclRange,
-		})
+		// disable raising this for now, since for the
+		// v0.20.x we should support both usage (but not together)
+		//
+		// diags = append(diags, &hcl.Diagnostic{
+		// 	Severity: hcl.DiagWarning,
+		// 	Summary:  fmt.Sprintf("Property 'version' is deprecated - use 'min_version' instead, in plugin '%s' require block", p.RawName),
+		// 	Subject:  &p.DeclRange,
+		// })
 		// copy into new property
 		p.MinVersionString = p.VersionString
 	}
