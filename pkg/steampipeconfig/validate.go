f09997da80a62c923a13103fde6942ebd480be5d
kai <kai@turbot.com>
2023-05-17 15:23:40 +0100

    Remove formatting from connection validaiton errors. #3432

--- pkg/steampipeconfig/validate.go
+++ pkg/steampipeconfig/validate.go
@@ -9,10 +9,10 @@ import (
 
 func ValidateConnectionName(connectionName string) error {
 	if helpers.StringSliceContains(constants.ReservedConnectionNames, connectionName) {
-		return fmt.Errorf("'%s' is a reserved connection name", constants.Bold(connectionName))
+		return fmt.Errorf("'%s' is a reserved connection name", connectionName)
 	}
 	if strings.HasPrefix(connectionName, constants.ReservedConnectionNamePrefix) {
-		return fmt.Errorf("invalid connection name '%s' - connection names cannot start with '%s'", constants.Bold(connectionName), constants.Bold(constants.ReservedConnectionNamePrefix))
+		return fmt.Errorf("invalid connection name '%s' - connection names cannot start with '%s'", connectionName, constants.ReservedConnectionNamePrefix)
 	}
 	return nil
 }
