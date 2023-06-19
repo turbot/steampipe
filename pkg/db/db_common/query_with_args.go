757ade44ae213d7a6e4430be658f0733eeea27e2
Binaek Sarkar <binaek@turbot.com>
2023-06-19 16:40:20 +0530

    data type

--- pkg/db/db_common/query_with_args.go
+++ pkg/db/db_common/query_with_args.go
@@ -4,3 +4,10 @@ type QueryWithArgs struct {
 	Query string
 	Args  []any
 }
+
+type QueriesWithArgs []QueryWithArgs
+
+// Add returns a new instance of QueriesWithArgs with the given QueryWithArgs appended
+func (q QueriesWithArgs) Add(another ...QueryWithArgs) QueriesWithArgs {
+	return append(q, another...)
+}
