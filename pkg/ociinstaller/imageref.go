b0e777c9f93aff37a77ed0f30b116246b2acc146
Patrick Decat <pdecat@gmail.com>
2023-06-27 10:49:47 +0200

    Return the full path of the plugin if it does not come from hub.steampipe.io

--- pkg/ociinstaller/imageref.go
+++ pkg/ociinstaller/imageref.go
@@ -87,11 +87,11 @@ func sanitizeRefStream(ref string) string {
 func (r *SteampipeImageRef) GetOrgNameAndStream() (string, string, string) {
 	// plugin.Name looks like `hub.steampipe.io/plugins/turbot/aws@latest`
 	split := strings.Split(r.DisplayImageRef(), "/")
-
-	org := split[len(split)-2]
 	pluginNameAndStream := strings.Split(split[len(split)-1], "@")
-
-	return org, pluginNameAndStream[0], pluginNameAndStream[1]
+	if strings.HasPrefix(r.DisplayImageRef(), "hub.steampipe.io/") {
+		return split[len(split)-2], pluginNameAndStream[0], pluginNameAndStream[1]
+	}
+	return strings.Join(split[0:len(split)-2], "/"), pluginNameAndStream[0], pluginNameAndStream[1]
 }
 
 // possible formats include
