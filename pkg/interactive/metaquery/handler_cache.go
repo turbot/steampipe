f792de6b1bba8ab03fc07095214957430b99f80f
Binaek Sarkar <binaek@turbot.com>
2023-06-27 18:22:09 +0530

    cache command

--- pkg/interactive/metaquery/handler_cache.go
+++ pkg/interactive/metaquery/handler_cache.go
@@ -12,8 +12,35 @@ import (
 	"github.com/turbot/steampipe/sperr"
 )
 
+func showCache(_ context.Context, input *HandlerInput) error {
+	if input.Client.ServerSettings() != nil && !input.Client.ServerSettings().CacheEnabled {
+		fmt.Println("Caching is disabled on the server")
+		return nil
+	}
+
+	currentStatusString := "off"
+	action := "on"
+	if viper.GetBool(constants.ArgClientCacheEnabled) {
+		currentStatusString = "on"
+		action = "off"
+	}
+
+	fmt.Printf(
+		`Caching is %s. To turn it %s, type %s\n`,
+		constants.Bold(currentStatusString),
+		constants.Bold(action),
+		constants.Bold(fmt.Sprintf(".cache %s", action)),
+	)
+
+	return nil
+}
+
 // controls the cache in the connected FDW
 func cacheControl(ctx context.Context, input *HandlerInput) error {
+	if len(input.args()) == 0 {
+		return showCache(ctx, input)
+	}
+
 	command := input.args()[0]
 	// just get the active session from the connection pool
 	// and set the cache parameters on it.
@@ -23,19 +50,24 @@ func cacheControl(ctx context.Context, input *HandlerInput) error {
 	if sessionResult.Error != nil {
 		return sessionResult.Error
 	}
-	conn := sessionResult.Session.Connection.Conn()
 	defer func() {
 		// we need to do this in a closure, otherwise the ctx will be evaluated immediately
 		// and not in call-time
 		sessionResult.Session.Close(false)
 	}()
+
+	conn := sessionResult.Session.Connection.Conn()
 	switch command {
 	case constants.ArgOn:
+		serverSettings := input.Client.ServerSettings()
+		if serverSettings != nil && !serverSettings.CacheEnabled {
+			fmt.Println("Cannot turn on cache - caching is disabled on the server")
+		}
 		viper.Set(constants.ArgClientCacheEnabled, true)
-		return db_common.SetCacheEnabled(ctx, true, sessionResult.Session.Connection.Conn())
+		return db_common.SetCacheEnabled(ctx, true, conn)
 	case constants.ArgOff:
 		viper.Set(constants.ArgClientCacheEnabled, false)
-		return db_common.SetCacheEnabled(ctx, false, sessionResult.Session.Connection.Conn())
+		return db_common.SetCacheEnabled(ctx, false, conn)
 	case constants.ArgClear:
 		return db_common.CacheClear(ctx, conn)
 	}
