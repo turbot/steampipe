da623b0125f7815c39551b4c29f01099cfe25620
Puskar Basu <puskar@turbot.com>
2023-07-10 21:01:45 +0530

    fix

--- pkg/steampipeconfig/modconfig/require.go
+++ pkg/steampipeconfig/modconfig/require.go
@@ -157,6 +157,10 @@ func (r *Require) searchInstalledPluginForRequirement(modName string, requiremen
 			// no point check - different plugin
 			continue
 		}
+		// if org and name matches but the plugin is built locally, return
+		if org == requirement.Org && name == requirement.Name && installed.Version == "local" {
+			return nil
+		}
 		if requirement.Constraint.Check(installed.Semver()) {
 			// constraint is satisfied
 			return nil
