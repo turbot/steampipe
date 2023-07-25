23587f7284dabb04756656a047f1e87105854f4e
Binaek Sarkar <binaek@turbot.com>
2023-07-25 19:28:58 +0530

    create a system client pool

--- pkg/db/db_client/db_client_connect.go
+++ pkg/db/db_client/db_client_connect.go
@@ -37,6 +37,7 @@ func (c *DbClient) establishConnectionPool(ctx context.Context) error {
 		"localhost",
 	}
 
+	// this will yield a false negative when connecting to a local service using a network IP
 	if helpers.StringSliceContains(locals, config.ConnConfig.Host) {
 		c.isLocalService = true
 	}
@@ -55,7 +56,7 @@ func (c *DbClient) establishConnectionPool(ctx context.Context) error {
 	// set an app name so that we can track database connections from this Steampipe execution
 	// this is used to determine whether the database can safely be closed
 	config.ConnConfig.Config.RuntimeParams = map[string]string{
-		"application_name": runtime.PgClientAppName,
+		"application_name": runtime.ClientConnectionAppName,
 	}
 
 	// this returns connection pool
@@ -74,5 +75,29 @@ func (c *DbClient) establishConnectionPool(ctx context.Context) error {
 		return err
 	}
 	c.pool = dbPool
+
+	return c.establishSystemConnectionPool(ctx, config)
+}
+
+// establishSystemConnectionPool creates a connection pool that can be used to support execution of user-initiated
+// queries (loading of connection state etc.)
+// unlike the other connection pool, this doesn't wait for the pool to completely start
+// this is because, the other pool will have established and verified a connection with the service
+func (c *DbClient) establishSystemConnectionPool(ctx context.Context, config *pgxpool.Config) error {
+	utils.LogTime("db_client.establishSystemConnectionPool start")
+	defer utils.LogTime("db_client.establishSystemConnectionPool end")
+
+	// create a copy of the config
+	copiedConfig := config.Copy()
+	copiedConfig.ConnConfig.Config.RuntimeParams = map[string]string{
+		"application_name": runtime.ClientSystemConnectionAppName,
+	}
+
+	// this returns connection pool
+	dbPool, err := pgxpool.NewWithConfig(context.Background(), config)
+	if err != nil {
+		return err
+	}
+	c.sysPool = dbPool
 	return nil
 }
