4bdd0acdbcce3536514327c4bdbbacbc80c51feb
kai <kai@turbot.com>
2023-07-28 19:09:36 +0100

    tactical - ensure steampipe_internal is created before creating steampipe_rate_limiter

--- pkg/pluginmanager_service/rate_limiter.go
+++ pkg/pluginmanager_service/rate_limiter.go
@@ -2,6 +2,7 @@ package pluginmanager_service
 
 import (
 	"context"
+	"fmt"
 	"github.com/turbot/steampipe/pkg/constants"
 	"github.com/turbot/steampipe/pkg/db/db_common"
 	"github.com/turbot/steampipe/pkg/db/db_local"
@@ -10,6 +11,8 @@ import (
 
 func (m *PluginManager) refreshRateLimiterTable(ctx context.Context) error {
 	queries := []db_common.QueryWithArgs{
+		// TACTICAL - look at startup order
+		{Query: fmt.Sprintf(`CREATE SCHEMA IF NOT EXISTS %s;`, constants.InternalSchema)},
 		rate_limiters.DropRateLimiterTable(),
 		rate_limiters.CreateRateLimiterTable(),
 		rate_limiters.GrantsOnRateLimiterTable(),
