b652dca99d73d89dd5f96d04e106e496da2433e8
Chandru <chandru89new@gmail.com>
2023-08-10 15:45:01 +0530

    Include MarkLineComponent in echarts build. Closes #3740.

--- ui/dashboard/src/components/dashboards/charts/Chart/echarts.ts
+++ ui/dashboard/src/components/dashboards/charts/Chart/echarts.ts
@@ -14,6 +14,7 @@ import {
   LegendComponent,
   TitleComponent,
   TooltipComponent,
+  MarkLineComponent,
 } from "echarts/components";
 import { LabelLayout } from "echarts/features";
 
@@ -26,6 +27,7 @@ echarts.use([
   LabelLayout,
   LegendComponent,
   LineChart,
+  MarkLineComponent,
   PieChart,
   SankeyChart,
   TitleComponent,
