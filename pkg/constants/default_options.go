2e55b25c7e446cf512618cd83cd47815d8f9a6b1
Puskar Basu <puskar@turbot.com>
2023-08-17 15:51:34 +0530

    Fix indentation

--- pkg/constants/default_options.go
+++ pkg/constants/default_options.go
@@ -9,19 +9,19 @@ const DefaultConnectionConfigContent = `
 #
 
 # options "database" {
-#   port                = 9193                  # any valid, open port number
-#   listen              = "local"               # local (alias for localhost), network (alias for *), or a comma separated list of hosts and/or IP addresses , or any valid combination of hosts and/or IP addresses
-#   search_path         = "aws,aws2,gcp,gcp2"   # comma-separated string; an exact search_path
-#   search_path_prefix  = "aws"                 # comma-separated string; a search_path prefix
-#   start_timeout       = 30                    # maximum time (in seconds) to wait for the database to start up
-#   cache               = true                  # true, false
-#   cache_max_ttl       = 900                   # max expiration (TTL) in seconds
-#   cache_max_size_mb   = 1024                  # max total size of cache across all plugins
+#   port               = 9193                  # any valid, open port number
+#   listen             = "local"               # local (alias for localhost), network (alias for *), or a comma separated list of hosts and/or IP addresses , or any valid combination of hosts and/or IP addresses
+#   search_path        = "aws,aws2,gcp,gcp2"   # comma-separated string; an exact search_path
+#   search_path_prefix = "aws"                 # comma-separated string; a search_path prefix
+#   start_timeout      = 30                    # maximum time (in seconds) to wait for the database to start up
+#   cache              = true                  # true, false
+#   cache_max_ttl      = 900                   # max expiration (TTL) in seconds
+#   cache_max_size_mb  = 1024                  # max total size of cache across all plugins
 # }
 
 # options "dashboard" {
-#   port          = 9193    # any valid, open port number
-#   listen        = "local" # local, network
+#   port   = 9193    # any valid, open port number
+#   listen = "local" # local, network
 # }
 
 # options "general" {
