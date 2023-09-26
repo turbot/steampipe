d4bc1fa95a734695bb920579a143b81d1649b326
Binaek Sarkar <binaek@turbot.com>
2023-09-26 15:27:45 +0530

    try assigning when empty

--- pkg/steampipeconfig/modconfig/var_config/named_values.go
+++ pkg/steampipeconfig/modconfig/var_config/named_values.go
@@ -3,16 +3,15 @@ package var_config
 // github.com/hashicorp/terraform/configs/parser_config.go
 import (
 	"fmt"
-	"github.com/turbot/steampipe/pkg/steampipeconfig/hclhelpers"
 	"unicode"
 
 	"github.com/hashicorp/hcl/v2"
 	"github.com/hashicorp/hcl/v2/gohcl"
 	"github.com/hashicorp/hcl/v2/hclsyntax"
+	"github.com/turbot/steampipe/pkg/steampipeconfig/hclhelpers"
 	"github.com/turbot/steampipe/pkg/steampipeconfig/inputvars/typeexpr"
 	"github.com/zclconf/go-cty/cty"
 	"github.com/zclconf/go-cty/cty/convert"
-	//"github.com/hashicorp/terraform/internal/typeexpr"
 )
 
 // A consistent detail message for all "not a valid identifier" diagnostics.
@@ -220,6 +219,9 @@ func (m VariableParsingMode) Parse(name, value string) (cty.Value, hcl.Diagnosti
 			return cty.DynamicVal, diags
 		}
 		val, valDiags := expr.Value(nil)
+		if tupleExpr, ok := expr.(*hclsyntax.TupleConsExpr); ok && /*is val zero?*/ len(tupleExpr.Exprs) == 0 {
+			val = cty.ListValEmpty(cty.String)
+		}
 		diags = append(diags, valDiags...)
 		return val, diags
 	default:
