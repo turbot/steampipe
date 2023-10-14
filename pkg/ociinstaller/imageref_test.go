62ac937b5cd65c1809c618e9fddd729a30f10181
François de Metz <francois@2metz.fr>
2023-10-14 12:42:32 +0200

    Add tests on TestGetOrgNameAndStream

--- pkg/ociinstaller/imageref_test.go
+++ pkg/ociinstaller/imageref_test.go
@@ -118,3 +118,30 @@ func TestDisplayImageRef(t *testing.T) {
 	}
 
 }
+
+func TestGetOrgNameAndStream(t *testing.T) {
+	cases := map[string][3]string{
+		"hub.steampipe.io/plugins/turbot/aws@latest":   {"turbot", "aws", "latest"},
+		"turbot/aws@latest":                            {"turbot", "aws", "latest"},
+		"aws@latest":                                   {"turbot", "aws", "latest"},
+		"hub.steampipe.io/plugins/turbot/aws@1.0.0":    {"turbot", "aws", "1.0.0"},
+		"hub.steampipe.io/plugins/otherOrg/aws@latest": {"otherOrg", "aws", "latest"},
+		"otherOrg/aws@latest":                          {"otherOrg", "aws", "latest"},
+		"hub.steampipe.io/plugins/otherOrg/aws@1.0.0":  {"otherOrg", "aws", "1.0.0"},
+		"otherOrg/aws@1.0.0":                           {"otherOrg", "aws", "1.0.0"},
+		"differentRegistry.com/otherOrg/aws@latest":    {"differentRegistry.com", "aws", "latest"},
+		"differentRegistry.com/otherOrg/aws@1.0.0":     {"differentRegistry.com", "aws", "1.0.0"},
+	}
+
+	for testCase, want := range cases {
+		t.Run(testCase, func(t *testing.T) {
+			r := NewSteampipeImageRef(testCase)
+			org, name, stream := r.GetOrgNameAndStream()
+			got := [3]string{org, name, stream}
+			if got != want {
+				t.Errorf("TestGetOrgNameAndStream failed for case '%s': expected %s, got %s", testCase, want, got)
+			}
+		})
+	}
+
+}
