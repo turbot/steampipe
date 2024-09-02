e7393ef465d15adc69f52717c1c98e68e67f7b23
kai <kai@turbot.com>
2024-09-02 10:23:59 +0100

    working on it

--- pkg/ociinstaller/imageconfig.go
+++ pkg/ociinstaller/imageconfig.go
@@ -10,44 +10,67 @@ type OciImageConfig interface {
 	Name()
 }
 
-type config struct {
-	SchemaVersion string        `json:"schemaVersion"`
-	Plugin        *configPlugin `json:"plugin,omitempty"`
-	Database      *configDb     `json:"db,omitempty"`
-	Fdw           *configFdw    `json:"fdw,omitempty"`
+type OciConfig interface {
+	GetSchemaVersion() string
+	SetSchemaVersion(string)
+}
+
+type OciConfigBase struct {
+	SchemaVersion string `json:"schemaVersion"`
+}
+
+func (c *OciConfigBase) GetSchemaVersion() string {
+	return c.SchemaVersion
+}
+func (c *OciConfigBase) SetSchemaVersion(version string) {
+	c.SchemaVersion = version
 }
 
 type configPlugin struct {
-	Name         string `json:"name,omitempty"`
-	Organization string `json:"organization,omitempty"`
-	Version      string `json:"version"`
+	OciConfigBase
+	Plugin struct {
+		Name         string `json:"name,omitempty"`
+		Organization string `json:"organization,omitempty"`
+		Version      string `json:"version"`
+	}
 }
 
 type configDb struct {
-	Name         string `json:"name,omitempty"`
-	Organization string `json:"organization,omitempty"`
-	Version      string `json:"version"`
-	DBVersion    string `json:"dbVersion,omitempty"`
+	OciConfigBase
+	Database struct {
+		Name         string `json:"name,omitempty"`
+		Organization string `json:"organization,omitempty"`
+		Version      string `json:"version"`
+		DBVersion    string `json:"dbVersion,omitempty"`
+	}
 }
 
 type configFdw struct {
-	Name         string `json:"name,omitempty"`
-	Organization string `json:"organization,omitempty"`
-	Version      string `json:"version"`
+	OciConfigBase
+	Fdw struct {
+		Name         string `json:"name,omitempty"`
+		Organization string `json:"organization,omitempty"`
+		Version      string `json:"version"`
+	}
 }
 
-func newSteampipeImageConfig(configBytes []byte) (*config, error) {
-	configData := &config{
-		Plugin:   &configPlugin{},
-		Database: &configDb{},
-		Fdw:      &configFdw{},
+func newSteampipeImageConfig(configBytes []byte, imageType ImageType) (OciConfig, error) {
+	var target OciConfig
+	switch imageType {
+	case ImageTypeDatabase:
+		target = &configDb{}
+	case ImageTypeFdw:
+		target = &configFdw{}
+	case ImageTypePlugin:
+		target = &configPlugin{}
 	}
-	if err := json.Unmarshal(configBytes, configData); err != nil {
+
+	if err := json.Unmarshal(configBytes, target); err != nil {
 		return nil, err
 	}
 
-	if configData.SchemaVersion == "" {
-		configData.SchemaVersion = DefaultConfigSchema
+	if target.GetSchemaVersion() == "" {
+		target.SetSchemaVersion(DefaultConfigSchema)
 	}
-	return configData, nil
+	return target, nil
 }
