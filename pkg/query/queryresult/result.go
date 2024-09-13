973939e4a260a506102b41c0d329c0ccd07a7a6f
kai <kai@turbot.com>
2024-09-13 16:11:28 +0100

    steampipe timing working

--- pkg/query/queryresult/result.go
+++ pkg/query/queryresult/result.go
@@ -25,14 +25,17 @@ func (r *TimingResult) Initialise(summary *QueryRowSummary, scans []*ScanMetadat
 	r.Scans = scans
 }
 
+// Result is a type alias for queryresult.Result[TimingResultStream]
 type Result = queryresult.Result[TimingResultStream]
 
 func NewResult(cols []*queryresult.ColumnDef) *Result {
-	return queryresult.NewResult[TimingResultStream](cols)
+	return queryresult.NewResult[TimingResultStream](cols, make(TimingResultStream, 1))
 }
 
+// SyncQueryResult is a type alias for queryresult.SyncQueryResult[TimingResult]
 type SyncQueryResult = queryresult.SyncQueryResult[*TimingResult]
 
+// ResultStreamer is a type alias for queryresult.ResultStreamer[TimingResultStream]
 type ResultStreamer = queryresult.ResultStreamer[TimingResultStream]
 
 func NewResultStreamer() *ResultStreamer {
