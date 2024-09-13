8acac0e269d22972b832eb46bcc0529aa9a04508
Puskar Basu <puskar@turbot.com>
2024-09-13 17:31:38 +0530

    comment

--- pkg/constants/image.go
+++ pkg/constants/image.go
@@ -1,5 +1,9 @@
 package constants
 
 const (
+	// The BaseImageRef is the common prefix for all turbot managed steampipe images
+	// Embedded PG: ghcr.io/turbot/steampipe/db
+	// FDW: ghcr.io/turbot/steampipe/fdw
+	// Plugins: ghcr.io/turbot/steampipe/plugins/turbot/plugin
 	BaseImageRef = "ghcr.io/turbot/steampipe"
 )
