ea732d5dd0025616a7077e8961f60128da705709
Puskar Basu <puskar@turbot.com>
2025-02-28 12:49:04 +0530

    update default_options

--- pkg/constants/default_options.go
+++ pkg/constants/default_options.go
@@ -28,6 +28,6 @@ const DefaultConnectionConfigContent = `
 
 # options "plugin" {
 #   memory_max_mb    = "1024"	# the default maximum memory to allow a plugin process - used if there is not max memory specified in the 'plugin' block' for that plugin
-#   start_timeout    = 180    # the default maximum time (in seconds) to wait for a plugin to start up
+#   start_timeout    = 30       # maximum time (in seconds) to wait for a plugin to start up
 # }
 `
