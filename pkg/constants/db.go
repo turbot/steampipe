fb26601fa33894fc9b958604b66e39c6fe278ebc
Puskar Basu <puskar@turbot.com>
2025-08-28 19:26:52 +0530

    fdw v2.1.3-rc.0

--- pkg/constants/db.go
+++ pkg/constants/db.go
@@ -28,7 +28,7 @@ const (
 // constants for installing db and fdw images
 const (
 	DatabaseVersion = "14.17.0"
-	FdwVersion      = "2.1.0"
+	FdwVersion      = "2.1.3-rc.0"
 
 	// PostgresImageRef is the OCI Image ref for the database binaries
 	PostgresImageRef    = "ghcr.io/turbot/steampipe/db:14.17.0"
