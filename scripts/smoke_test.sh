ec72e6d86d2757692c9d230bc2213f40d3b2dab4
Puskar Basu <puskar@turbot.com>
2025-11-03 07:07:51 +0530

    Fix smoke tests

--- scripts/smoke_test.sh
+++ scripts/smoke_test.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
 # This is a script with set of commands to smoke test a steampipe build.
 # The plan is to gradually add more tests to this script.
+set -e
 
 /usr/local/bin/steampipe --version # check version
 /usr/local/bin/steampipe query "select 1 as installed" # verify installation
@@ -17,10 +18,10 @@
 # the file path is different for darwin and linux
 if [ "$(uname -s)" = "Darwin" ]; then
   /usr/local/bin/steampipe query "select name from steampipe_registry_plugin limit 1;" --export /Users/runner/query.sps # verify file export
-  cat /Users/runner/query.sps | jq '.end_time' # verify file created is readable
+  jq '.end_time' /Users/runner/query.sps # verify file created is readable
 else
   /usr/local/bin/steampipe query "select name from steampipe_registry_plugin limit 1;" --export /home/steampipe/query.sps # verify file export
-  cat /home/steampipe/query.sps | jq '.end_time' # verify file created is readable
+  jq '.end_time' /home/steampipe/query.sps # verify file created is readable
 fi
 
 # Ensure the log file path exists before trying to read it
@@ -34,7 +35,7 @@ STEAMPIPE_LOG=info /usr/local/bin/steampipe query "select name from steampipe_re
 
 # Check if log file exists before attempting to cat it
 if ls $LOG_PATH 1> /dev/null 2>&1; then
-  bash -c "cat $LOG_PATH | grep '\[INFO\]'"
+  grep '\[INFO\]' $LOG_PATH
 else
   echo "Log file not found: $LOG_PATH"
   exit 1
